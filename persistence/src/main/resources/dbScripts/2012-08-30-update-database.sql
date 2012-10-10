116f78d5468d6fcea19b5db70fa15c5b2225ed26
bastiaan <bastiaan.verhoef.ape@gmail.com>
2012-10-10 12:51:48 +0000

    fix for database script
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@101 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/resources/dbScripts/2012-08-30-update-database.sql
+++ persistence/src/main/resources/dbScripts/2012-08-30-update-database.sql
@@ -1,14 +1,14 @@
-﻿DROP TABLE item;
-DROP TABLE level_type;
-DROP TABLE searches;
-DROP TABLE normal_user;
+﻿DROP TABLE item CASCADE;
+DROP TABLE level_type CASCADE;
+DROP TABLE searches CASCADE;
+DROP TABLE normal_user CASCADE;
 
 ALTER TABLE dashboard_user DROP CONSTRAINT partner_us_id_fkey;
 ALTER TABLE dashboard_user DROP COLUMN user_state_id;
 ALTER TABLE dashboard_user ADD COLUMN is_active boolean DEFAULT true;
 ALTER TABLE dashboard_user ALTER COLUMN is_active SET NOT NULL;
 
-DROP TABLE user_state;
+DROP TABLE user_state CASCADE;
 
 ALTER TABLE up_file DROP COLUMN ufs_id;
-DROP TABLE up_file_state;
+DROP TABLE up_file_state CASCADE;
