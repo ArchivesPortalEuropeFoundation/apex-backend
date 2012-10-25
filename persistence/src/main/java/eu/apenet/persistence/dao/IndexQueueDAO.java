2ba65ceee2d369fe1d8e012f848a91006abd89e5
bastiaan <bastiaan.verhoef.ape@gmail.com>
2012-10-25 12:50:46 +0000

    IssueID #44 : update queing
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@125 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/java/eu/apenet/persistence/dao/IndexQueueDAO.java
+++ persistence/src/main/java/eu/apenet/persistence/dao/IndexQueueDAO.java
@@ -1,6 +1,5 @@
 package eu.apenet.persistence.dao;
 
-import java.util.Date;
 import java.util.List;
 
 import eu.apenet.persistence.vo.IndexQueue;
@@ -12,23 +11,7 @@ import eu.apenet.persistence.vo.IndexQueue;
  */
 
 public interface IndexQueueDAO extends GenericDAO<IndexQueue, Integer> {
-	public IndexQueue getIndexQueueByHg(Integer hgId);
-	public IndexQueue getIndexQueueByFa(Integer faId);
-	public IndexQueue getIndexQueueBySg(Integer sgId);
-	public Integer getMaxIqId();
-	public List<IndexQueue> getAllOrderedbyId();
-	public List<IndexQueue> getFilesOrderedbyId(String type);
-	public Integer getMaxPosition();
-	public List<IndexQueue> getFilesbeforeDate(Date date, String sortValue, boolean ascending);
-	public Integer getMaxPositionofHgs();
-	public Integer getMaxPositionofFas();
-    public Integer getLastPositionOfHgs(int aiId);
-    public Integer getLastPositionOfFas(int aiId);
-	public IndexQueue getFilebyPosition(Integer position);
-	public List<IndexQueue> getHgs();
-	public List<IndexQueue> getFas();
-	public List<IndexQueue> getFilesFromPosition(Integer position);
 	public List<IndexQueue> getFilesWithErrors();
-	List<IndexQueue> getFirstItems(int limit);
+	List<IndexQueue> getFirstItems();
 }
 
