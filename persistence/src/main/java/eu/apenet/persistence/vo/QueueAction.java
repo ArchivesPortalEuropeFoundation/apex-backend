72ca6f2ef1c74b263693a17463b1247cee720cd9
bastiaan <bastiaan.verhoef.ape@gmail.com>
2012-11-01 08:28:07 +0000

    updating database module for queuing
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@148 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/java/eu/apenet/persistence/vo/QueueAction.java
+++ persistence/src/main/java/eu/apenet/persistence/vo/QueueAction.java
@@ -1,5 +1,42 @@
 package eu.apenet.persistence.vo;
 
 public enum QueueAction {
-	VALIDATE, CONVERT, INDEX, VALIDATE_CONVERT_INDEX, CONVERT_TO_EUROPEANA, DELIVER_TO_EUROPEANA
+	VALIDATE(true, false, false, false, false), CONVERT(false, true, false, false, false), INDEX(false, false, true,
+			false, false), VALIDATE_CONVERT_INDEX(true, true, true, false, false), CONVERT_TO_ESE_EDM(false, false,
+			false, true, false), DELIVER_TO_EUROPEANA(false, false, false, false, true);
+	private boolean validateAction = false;
+	private boolean convertAction = false;
+	private boolean indexAction = false;
+	private boolean eseEdmAction = false;
+	private boolean europeanaAction = false;
+
+	private QueueAction(boolean validateAction, boolean convertAction, boolean indexAction, boolean eseEdmAction,
+			boolean europeanaAction) {
+		this.validateAction = validateAction;
+		this.convertAction = convertAction;
+		this.indexAction = indexAction;
+		this.eseEdmAction = eseEdmAction;
+		this.europeanaAction = europeanaAction;
+	}
+
+	public boolean isValidateAction() {
+		return validateAction;
+	}
+
+	public boolean isConvertAction() {
+		return convertAction;
+	}
+
+	public boolean isIndexAction() {
+		return indexAction;
+	}
+
+	public boolean isEseEdmAction() {
+		return eseEdmAction;
+	}
+
+	public boolean isEuropeanaAction() {
+		return europeanaAction;
+	}
+
 }
