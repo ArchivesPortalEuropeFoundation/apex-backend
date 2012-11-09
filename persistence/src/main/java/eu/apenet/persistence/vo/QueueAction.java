32f34061850017f876150bff92349947587442b4
bastiaan <bastiaan.verhoef.ape@gmail.com>
2012-11-09 11:13:06 +0000

    update filestate
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@188 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/java/eu/apenet/persistence/vo/QueueAction.java
+++ persistence/src/main/java/eu/apenet/persistence/vo/QueueAction.java
@@ -1,7 +1,7 @@
 package eu.apenet.persistence.vo;
 
 public enum QueueAction {
-	VALIDATE("content.message.validate"), CONVERT("content.message.convert"), PUBLISH("content.message.publish"), CONVERT_VALIDATE_PUBLISH("content.message.doitall"), UNPUBLISH("content.message.unpublish"), DELETE("content.message.delete"), CONVERT_TO_ESE_EDM("content.message.convert.ese"),DELETE_ESE_EDM("content.message.delete.ese"), DELIVER_TO_EUROPEANA("content.message.deliver.europeana"), DELETE_FROM_EUROPEANA("content.message.delete.europeana" );
+	VALIDATE("content.message.validate"), CONVERT("content.message.convert"), PUBLISH("content.message.publish"), CONVERT_VALIDATE_PUBLISH("content.message.doitall"), UNPUBLISH("content.message.unpublish"), DELETE("content.message.delete"), CONVERT_TO_ESE_EDM("content.message.convert.ese"),DELETE_ESE_EDM("content.message.delete.ese"), DELIVER_TO_EUROPEANA("content.message.deliver.europeana"), DELETE_FROM_EUROPEANA("content.message.delete.europeana" ), UNPUBLISH_ALL("content.message.unpublish.all");
 
 	private String resourceName;
 	private QueueAction(String resourceName){
@@ -25,19 +25,19 @@ public enum QueueAction {
 		return CONVERT_TO_ESE_EDM.equals(this);
 	}
 	public boolean isDeleteEseEdmAction() {
-		return DELETE_ESE_EDM.equals(this);
+		return DELETE_ESE_EDM.equals(this) || UNPUBLISH_ALL.equals(this);
 	}
 	public boolean isDeliverToEuropeanaAction() {
 		return DELIVER_TO_EUROPEANA.equals(this);
 	}
 	public boolean isUnpublishAction() {
-		return UNPUBLISH.equals(this);
+		return UNPUBLISH.equals(this) || UNPUBLISH_ALL.equals(this);
 	}
 	public boolean isDeleteAction() {
 		return DELETE.equals(this);
 	}
 	public boolean isDeleteFromEuropeanaAction() {
-		return DELETE_FROM_EUROPEANA.equals(this);
+		return DELETE_FROM_EUROPEANA.equals(this) || UNPUBLISH_ALL.equals(this);
 	}
 
 
