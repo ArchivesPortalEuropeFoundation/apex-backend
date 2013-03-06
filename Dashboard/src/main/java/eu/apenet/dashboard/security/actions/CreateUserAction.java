9e4f560adb7a81680ea09c8e3471f5743dbf7b0c
yoannmoranville <yoann@archivesportaleurope.net>
2013-03-06 16:31:25 +0000

    Changes about regexp for ticket #206
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@403 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/security/actions/CreateUserAction.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/security/actions/CreateUserAction.java
@@ -121,14 +121,12 @@ public class CreateUserAction extends AbstractAction {
 						addFieldError("email", getText("email.notAvailable"));
 					}
 					else {
-						/*Email validation required to allow to create a new user*/
-						String  expression="^[\\w\\-]([\\.\\w])+[\\w]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
-			               CharSequence inputStr = email;
-			               Pattern pattern = Pattern.compile(expression,Pattern.CASE_INSENSITIVE);
-			               Matcher matcher = pattern.matcher(inputStr);
-			               /*A new method has been created to add the error message for an invalid email format*/
-			               if(!matcher.matches())
-			            	   addFieldError("email", getText("email.valid"));				
+                        //RFC regexp for emails
+                        String expression = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+			            Pattern pattern = Pattern.compile(expression,Pattern.CASE_INSENSITIVE);
+			            Matcher matcher = pattern.matcher(email);
+			            if(!matcher.matches())
+			                addFieldError("email", getText("email.valid"));
 					}
 				}
 			}
