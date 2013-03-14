2511db20839d14bb31c02b1c24e2502a556a28a9
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-03-14 13:34:40 +0000

    remvoe direct indexing
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/portal-project-1.1.1@445 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/listener/DashboardConfigListener.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/listener/DashboardConfigListener.java
@@ -1,7 +1,5 @@
 package eu.apenet.dashboard.listener;
 
-import java.util.Date;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 
@@ -12,17 +10,14 @@ import eu.apenet.commons.utils.APEnetUtilities;
 import eu.archivesportaleurope.commons.config.DashboardConfig;
 
 public class DashboardConfigListener extends ApePortalAndDashboardConfigListener {
-	private static final String TRUE = "true";
 	private static final String EMAIL_DASHBOARD_FEEDBACK_DESTINY = "EMAIL_DASHBOARD_FEEDBACK_DESTINY";
 	private static final String EUROPEANA_DIR_PATH = "EUROPEANA_DIR_PATH";
 	private static final String EUROPEANA_DIR_PATH_DEFAULT = "/mnt/europeana/";
 	private static final String SOLR_INDEX_URL = "SOLR_INDEX_URL";
-	private static final String DIRECT_INDEXING = "DIRECT_INDEXING";
 	private static final String XSL_DIR_PATH = "XSL_DIR_PATH";
 	private static final String XSL_DIR_PATH_DEFAULT = "/mnt/xsl/";
 	private static final String TMP_DIR_PATH = "TMP_DIR_PATH";
 	private static final String TMP_DIR_PATH_DEFAULT = "/mnt/tmp/";
-	private static final String BATCH_PROCESSING_ENABLED = "BATCH_PROCESSING_ENABLED";
 
 	private static final String DOMAIN_NAME_MAIN_SERVER = "DOMAIN_NAME_MAIN_SERVER";
 	private static final String DOMAIN_NAME_MAIN_SERVER_DEFAULT = "localhost:8443";
@@ -34,9 +29,7 @@ public class DashboardConfigListener extends ApePortalAndDashboardConfigListener
 			init(servletContext, apeConfig);
 			apeConfig.finalizeConfigPhase();
 			APEnetUtilities.setConfig(apeConfig);
-			if (!apeConfig.isDirectIndexing()){
-				QueueDaemon.start();
-			}
+			QueueDaemon.start();
 		} catch (RuntimeException e) {
 			log.fatal("Fatal error while initializing: " + e.getMessage(), e);
 			throw e;
@@ -71,16 +64,6 @@ public class DashboardConfigListener extends ApePortalAndDashboardConfigListener
 			log.info(SOLR_INDEX_URL + ": " + solrIndexUrl);
 		}
 		config.setSolrIndexUrl(solrIndexUrl);
-		/*
-		 * direct indexing
-		 */
-		boolean directIndexing = TRUE.equalsIgnoreCase(servletContext.getInitParameter(DIRECT_INDEXING));
-		config.setDirectIndexing(directIndexing);
-		String batchProcessingEnabled = servletContext.getInitParameter(BATCH_PROCESSING_ENABLED);
-		if (!StringUtils.isBlank(batchProcessingEnabled)) {
-			config.setBatchProcessingEnabled(TRUE.equalsIgnoreCase(batchProcessingEnabled));
-		}
-
 		
 		String xslDirPath = servletContext.getInitParameter(XSL_DIR_PATH);
 		if (StringUtils.isBlank(xslDirPath)) {
