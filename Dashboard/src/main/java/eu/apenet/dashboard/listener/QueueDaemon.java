8042b68dbd2194b684470246f23242e9f9f1ffb7
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-03-14 13:10:59 +0000

    merge of IssueID: 250
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/portal-project-1.1.1@433 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/listener/QueueDaemon.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/listener/QueueDaemon.java
@@ -1,32 +1,49 @@
 package eu.apenet.dashboard.listener;
 
-import java.util.Date;
-import java.util.Timer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
 
 public class QueueDaemon {
 	private static final Logger LOGGER = Logger.getLogger(QueueDaemon.class);
-	private static Timer timer ;
-	public static synchronized void  init (){
-		if (timer == null){
-			timer = new Timer(true);
+	private static ScheduledExecutorService scheduler;
+	private static boolean queueProcessing = false;
+	public static synchronized void start(){
+		if (scheduler == null){
+			scheduler = Executors.newScheduledThreadPool(1);
+			addTask(new Duration(0, 0, 0),new Duration(0, 10, 0) , new Duration(0, 2, 0));
 			LOGGER.info("Queue daemon started");
 		}
 		
 	}
-	public static void addTask(Date startDate, Duration maxDuration, Duration delay){
-		if (timer != null){
+	public static void addTask(Duration startDelay, Duration maxDuration, Duration delay){
+		if (scheduler != null){
 			LOGGER.info("Add queue task");
-			QueueTask queueTask = new QueueTask(maxDuration.getMilliseconds());
-			timer.scheduleAtFixedRate(queueTask, startDate, delay.getMilliseconds());
+			QueueTask queueTask = new QueueTask(scheduler, maxDuration, delay);
+			scheduler.schedule(queueTask, startDelay.getMilliseconds(), TimeUnit.MILLISECONDS);
 		}
 	}
-	public static void cancel(){
-		if (timer != null){
-			timer.cancel();
-			timer = null;
+	public static synchronized void stop(){
+		if (scheduler != null){
+			scheduler.shutdownNow();
+			scheduler = null;
 			LOGGER.info("Queue daemon stopped");
 		}
 	}
+	public static boolean isActive(){
+		if (scheduler != null){
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public static boolean isQueueProcessing() {
+		return queueProcessing;
+	}
+	protected static void setQueueProcessing(boolean queueProcessing) {
+		QueueDaemon.queueProcessing = queueProcessing;
+	}
+	
 }
