d2269d33e19d84324607fca14a49c73fb7c95695
yoannmoranville <yoann@archivesportaleurope.net>
2013-03-27 15:46:21 +0000

    #353: Validate files when converting them into EAG 2012
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/portal-project-1.1.1@519 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/services/eag/ParseEAGAction.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/services/eag/ParseEAGAction.java
@@ -5,9 +5,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import eu.apenet.dpt.utils.service.DocumentValidation;
+import eu.apenet.dpt.utils.util.Xsd_enum;
 import net.sf.saxon.s9api.SaxonApiException;
 
 import org.apache.commons.io.FileUtils;
@@ -22,10 +25,11 @@ import eu.apenet.dpt.utils.service.TransformationTool;
 import eu.apenet.persistence.dao.ArchivalInstitutionDAO;
 import eu.apenet.persistence.factory.DAOFactory;
 import eu.apenet.persistence.vo.ArchivalInstitution;
+import org.xml.sax.SAXParseException;
 
 public class ParseEAGAction extends ActionSupport {
 	
-	private final Logger logger = Logger.getLogger(ParseEAGAction.class);; //TODO instance
+	private final Logger logger = Logger.getLogger(ParseEAGAction.class); //TODO instance
 
 	@Override
 	public String execute() throws Exception {
@@ -35,6 +39,7 @@ public class ParseEAGAction extends ActionSupport {
 	}
 	
 	private void parseEAGs() throws SAXException, IOException{
+        List<String> eagsNotConverted = new ArrayList<String>();
 		ArchivalInstitutionDAO aiDao = DAOFactory.instance().getArchivalInstitutionDAO();
 		List<ArchivalInstitution> institutions = aiDao.findAll();
 		if(institutions!=null && institutions.size()>0){
@@ -55,16 +60,39 @@ public class ParseEAGAction extends ActionSupport {
 					in  = new FileInputStream(new File(tempDirOutputPath));
 					TransformationTool.createTransformation(in, tempOutputFile, new File(xslFilePath), null, true, true, null, true, null);
 
-					File file = new File(tempDirOutputPath);
-					try {
-						FileUtils.moveFile(file , new File(tempDirOutputPath+".old")); //backup old EAGs, if it's not needed comment this line
-						FileUtils.moveFile(new File(tempDirOutputPath+".new"), file);
-					} catch (IOException e) {
-						logger.error("problem moving file "+file.getAbsolutePath());
-					}
+                    List<SAXParseException> exceptions = DocumentValidation.xmlValidation(tempOutputFile, Xsd_enum.XSD_EAG_2012_SCHEMA);
+                    String eagDirPath = tempDirOutputPath.substring(0, tempDirOutputPath.lastIndexOf("/") + 1);
+                    String validationErrorFilePath = eagDirPath + "validation_errors.txt";
+                    if(exceptions == null) {
+                        File file = new File(tempDirOutputPath);
+                        try {
+                            FileUtils.moveFile(file , new File(tempDirOutputPath+".old")); //backup old EAGs, if it's not needed comment this line
+                            FileUtils.moveFile(new File(tempDirOutputPath+".new"), file);
+                        } catch (IOException e) {
+                            logger.error("problem moving file "+file.getAbsolutePath());
+                        }
+                        if(new File(validationErrorFilePath).exists())
+                            new File(validationErrorFilePath).delete();
+                    } else {
+                        eagsNotConverted.add(eagDirPath);
+                        logger.error("The converted EAG 2012 file is not valid, we will not use this file for now, it has to be checked manually. The list of errors can be found in the file validation_errors.txt");
+                        StringBuilder warn = new StringBuilder();
+                        for (SAXParseException exception : exceptions) {
+                            warn.append("l.").append(exception.getLineNumber()).append(" c.").append(exception.getColumnNumber()).append(": ").append(exception.getMessage()).append("\n");
+                        }
+                        File validationErrorFile = new File(validationErrorFilePath);
+                        FileUtils.writeStringToFile(validationErrorFile, warn.toString(), "UTF-8");
+                    }
 				}
 			}
 		}
+        if(eagsNotConverted.size() > 0) {
+            logger.warn("---------------");
+            logger.warn("The following EAGs (in total: " + eagsNotConverted.size() + ") have not been correctly validated against EAG 2012, please take care of them manually:");
+        }
+        for(String eagNotConverted : eagsNotConverted) {
+            logger.warn(eagNotConverted);
+        }
 	}
 
 	private boolean parseAllExistingEAGToEAG2012() throws FileNotFoundException {
