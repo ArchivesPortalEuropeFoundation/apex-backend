7e1f1829bb54c0b2865543715b26104f0cb4479a
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-03-29 13:47:59 +0000

    IssueID 331: convert to valid repository code
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/portal-project-1.1.1@544 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/services/eag/ParseEAGAction.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/services/eag/ParseEAGAction.java
@@ -6,13 +6,26 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
 
 import net.sf.saxon.s9api.SaxonApiException;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
@@ -30,7 +43,8 @@ import eu.apenet.persistence.vo.ArchivalInstitution;
 public class ParseEAGAction extends ActionSupport {
 
 	private final Logger logger = Logger.getLogger(ParseEAGAction.class); // TODO
-																			// instance
+	private final static XPath XPATH = APEnetUtilities.getDashboardConfig().getXpathFactory().newXPath(); // instance
+	List<String> eagsNotConverted = new ArrayList<String>();
 
 	@Override
 	public String execute() throws Exception {
@@ -39,8 +53,8 @@ public class ParseEAGAction extends ActionSupport {
 		return SUCCESS;
 	}
 
-	private void parseEAGs() throws SAXException, IOException {
-		List<String> eagsNotConverted = new ArrayList<String>();
+	private void parseEAGs() throws SAXException, IOException, ParserConfigurationException, XPathExpressionException {
+
 		ArchivalInstitutionDAO aiDao = DAOFactory.instance().getArchivalInstitutionDAO();
 		List<ArchivalInstitution> institutions = aiDao.findAll();
 		if (institutions != null && institutions.size() > 0) {
@@ -49,66 +63,63 @@ public class ParseEAGAction extends ActionSupport {
 				ArchivalInstitution institution = institutionsIterator.next();
 				String tempDirOutputPath = institution.getEagPath();
 				if (tempDirOutputPath != null && !tempDirOutputPath.isEmpty()) {
-					tempDirOutputPath = APEnetUtilities.getDashboardConfig().getRepoDirPath() + tempDirOutputPath;
+					String prefixPath = APEnetUtilities.getDashboardConfig().getRepoDirPath();
+					tempDirOutputPath = prefixPath + tempDirOutputPath;
 					if (!new File(tempDirOutputPath).exists()) {
-						tempDirOutputPath = APEnetUtilities.getDashboardConfig().getTempAndUpDirPath()
-								+ institution.getEagPath();
+						prefixPath = APEnetUtilities.getDashboardConfig().getRepoDirPath();
+						tempDirOutputPath = prefixPath + institution.getEagPath();
 					}
-					File tempDirOutputFile = new File(tempDirOutputPath);
-					if (tempDirOutputFile.exists()) {
-						FileInputStream in = null;
-						String tempOutputFilePath = tempDirOutputPath + ".new";
-						File tempOutputFile = new File(tempOutputFilePath);
-						String xslFilePath = APEnetUtilities.getDashboardConfig().getSystemXslDirPath()
-								+ APEnetUtilities.FILESEPARATOR + "eag2eag2012.xsl";
-						logger.info("'" + institution.getAiname() + "' is parsing file: '" + tempDirOutputPath);
-						in = new FileInputStream(new File(tempDirOutputPath));
-						if (DocumentValidation.xmlValidation(new File(tempDirOutputPath), Xsd_enum.XSD_EAG_2012_SCHEMA) != null) {
-							String eagDirPath = tempDirOutputPath.substring(0, tempDirOutputPath.lastIndexOf("/") + 1);
-							String validationErrorFilePath = eagDirPath + "validation_errors.txt";
+					File oldEagFile = new File(tempDirOutputPath);
+					if (oldEagFile.exists()) {
+						String pattern = institution.getCountry().getIsoname() + "-[a-zA-Z0-9:/\\-]{1,11}";
+						String repositoryCode = institution.getRepositorycode();
+						boolean validRepositoryCode = Pattern.matches(pattern, repositoryCode);
+						if (validRepositoryCode) {
 
-							TransformationTool.createTransformation(in, tempOutputFile, new File(xslFilePath), null,
-									true, true, null, true, null, APEnetUtilities.getDashboardConfig()
-											.getSystemXslDirPath());
-
-							List<SAXParseException> exceptions = DocumentValidation.xmlValidation(tempOutputFile,
-									Xsd_enum.XSD_EAG_2012_SCHEMA);
-							if (exceptions == null) {
-								File file = new File(tempDirOutputPath);
-								try {
-									FileUtils.moveFile(file, new File(tempDirOutputPath + ".old")); // backup
-																									// old
-																									// EAGs,
-																									// if
-																									// it's
-																									// not
-																									// needed
-																									// comment
-																									// this
-																									// line
-									FileUtils.moveFile(new File(tempDirOutputPath + ".new"), file);
-								} catch (IOException e) {
-									logger.error("problem moving file " + file.getAbsolutePath());
-								}
-								if (new File(validationErrorFilePath).exists())
-									new File(validationErrorFilePath).delete();
+						} else {
+							logger.warn(institution.getRepositorycode() + " does not match pattern: " + pattern);
+							DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+							factory.setNamespaceAware(false);
+							DocumentBuilder builder = factory.newDocumentBuilder();
+							Document doc = builder.parse(new InputSource(tempDirOutputPath));
+							doc.getDocumentElement().normalize();
+							XPathExpression eagIdExpression = XPATH.compile("/eag/eagheader/eagid");
+							String xmlEagid = (String) eagIdExpression.evaluate(doc, XPathConstants.STRING);
+							XPathExpression repoCodeExpression = XPATH
+									.compile("/eag/archguide/identity/repositorid/@repositorycode");
+							String repoCode = (String) repoCodeExpression.evaluate(doc, XPathConstants.STRING);
+							if (Pattern.matches(pattern, xmlEagid)) {
+								repositoryCode = xmlEagid;
+								logger.info("Use eagid: " + repositoryCode);
+							} else if (Pattern.matches(pattern, repoCode)) {
+								repositoryCode = repoCode;
+								logger.info("Use repositorycode: " + repositoryCode);
+							} else if (Pattern.matches(pattern, institution.getCountry().getIsoname() + "-" + repoCode)) {
+								repositoryCode = institution.getCountry().getIsoname() + "-" + repoCode;
+								logger.info("Use country + repositorycode: " + repositoryCode);
 							} else {
-								eagsNotConverted.add(eagDirPath);
-								logger.error("The converted EAG 2012 file is not valid, we will not use this file for now, it has to be checked manually. The list of errors can be found in the file validation_errors.txt");
-								StringBuilder warn = new StringBuilder();
-								for (SAXParseException exception : exceptions) {
-									warn.append("l.").append(exception.getLineNumber()).append(" c.")
-											.append(exception.getColumnNumber()).append(": ")
-											.append(exception.getMessage()).append("\n");
-								}
-								File validationErrorFile = new File(validationErrorFilePath);
-								FileUtils.writeStringToFile(validationErrorFile, warn.toString(), "UTF-8");
+								repositoryCode = institution.getCountry().getIsoname() + "-D" + institution.getAiId();
+								logger.info("Use db: " + repositoryCode);
 							}
+
 						}
-					}else {
-						logger.warn("'File does not exist: '" + tempDirOutputFile.getAbsolutePath());
+						String eagFile = repositoryCode.replaceAll("[:/\\\\]", "_") + ".xml";
+						String eagPath = "/" + institution.getCountry().getIsoname() + "/" + institution.getAiId()
+								+ "/EAG/";
+						logger.info(repositoryCode + " : " + eagPath + eagFile);
+						File newEagFile = new File(prefixPath + eagPath + eagFile);
+						boolean converted = convert(institution.getAiname(), repositoryCode, oldEagFile, newEagFile);
+						if (converted){
+							institution.setRepositorycode(repositoryCode);
+							institution.setEagPath(eagPath + eagFile);
+							aiDao.store(institution);
+						}
+					} else {
+						eagsNotConverted.add("NOT EXIST: " + oldEagFile.getAbsolutePath());
+						logger.warn("'File does not exist: '" + institution.getEagPath());
 					}
 				}
+
 			}
 		}
 		if (eagsNotConverted.size() > 0) {
@@ -119,6 +130,67 @@ public class ParseEAGAction extends ActionSupport {
 		for (String eagNotConverted : eagsNotConverted) {
 			logger.warn(eagNotConverted);
 		}
+	}
+
+	private boolean convert(String aiName, String repositoryCode, File oldEagFile, File newEagFile)
+			throws SAXException, IOException {
+
+		FileInputStream in = null;
+		File newTempOutputFile = new File(newEagFile.getAbsolutePath() + ".new");
+		File oldTempOutputFile = new File(newEagFile.getAbsolutePath() + ".old");
+		String xslFilePath = APEnetUtilities.getDashboardConfig().getSystemXslDirPath() + APEnetUtilities.FILESEPARATOR
+				+ "eag2eag2012.xsl";
+		logger.info("'" + aiName + "' is parsing file: '" + oldEagFile.getAbsolutePath());
+		in = new FileInputStream(oldEagFile);
+		if (DocumentValidation.xmlValidation(oldEagFile, Xsd_enum.XSD_EAG_2012_SCHEMA) != null) {
+			File validationErrorFile = new File(newTempOutputFile.getParentFile(), "validation_errors.txt");
+			Map<String, String> parameters = new HashMap<String, String>();
+			parameters.put("repositoryCode", repositoryCode);
+			TransformationTool.createTransformation(in, newTempOutputFile, new File(xslFilePath), parameters, true,
+					true, null, true, null, APEnetUtilities.getDashboardConfig().getSystemXslDirPath());
+
+			List<SAXParseException> exceptions = DocumentValidation.xmlValidation(newTempOutputFile,
+					Xsd_enum.XSD_EAG_2012_SCHEMA);
+			if (exceptions == null) {
+				try {
+					FileUtils.moveFile(oldEagFile, oldTempOutputFile); // backup
+																		// old
+																		// EAGs,
+																		// if
+																		// it's
+																		// not
+																		// needed
+																		// comment
+																		// this
+																		// line
+					FileUtils.moveFile(newTempOutputFile, newEagFile);
+				} catch (IOException e) {
+					logger.error("problem moving file " + newEagFile.getAbsolutePath());
+				}
+				if (validationErrorFile.exists())
+					validationErrorFile.delete();
+				File oldFile = oldEagFile;
+				while (!oldFile.getParentFile().getName().equals("EAG") && !oldFile.getName().equals("EAG"))
+				{
+					oldFile = oldFile.getParentFile();
+					logger.info("Delete old file:" + oldFile.getAbsolutePath());
+					oldFile.delete();
+				}
+				return true;
+			} else {
+				eagsNotConverted.add(oldEagFile.getParentFile().getAbsolutePath());
+				logger.error("The converted EAG 2012 file is not valid, we will not use this file for now, it has to be checked manually. The list of errors can be found in the file validation_errors.txt");
+				StringBuilder warn = new StringBuilder();
+				for (SAXParseException exception : exceptions) {
+					warn.append("l.").append(exception.getLineNumber()).append(" c.")
+							.append(exception.getColumnNumber()).append(": ").append(exception.getMessage())
+							.append("\n");
+				}
+				FileUtils.writeStringToFile(validationErrorFile, warn.toString(), "UTF-8");
+
+			}
+		}
+		return false;
 	}
 
 	private boolean parseAllExistingEAGToEAG2012() throws FileNotFoundException {
