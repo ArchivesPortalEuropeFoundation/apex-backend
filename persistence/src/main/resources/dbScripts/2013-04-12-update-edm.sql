0a68ce0559f689d4827750e3039bdc4580df1876
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-04-12 14:00:03 +0000

    IssueID 397: to many records counted
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/portal-project-1.1.1@656 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/resources/dbScripts/2013-04-12-update-edm.sql
+++ persistence/src/main/resources/dbScripts/2013-04-12-update-edm.sql
@@ -1,3 +1,3 @@
-ALTER TABLE finding_aid ADD COLUMN totalnumberofcho bigint default 0;
-UPDATE finding_aid SET totalnumberofcho  = 0;
-UPDATE finding_aid SET totalnumberofcho  = (totalnumberofdaos + 1) WHERE totalnumberofdaos > 0;
+ALTER TABLE finding_aid ADD COLUMN totalnumberofchos bigint default 0;
+UPDATE finding_aid SET totalnumberofchos  = 0;
+UPDATE finding_aid SET totalnumberofchos  = (totalnumberofdaos + 1) WHERE totalnumberofdaos > 0;
