95dfdaaf5e88a2c00a1cc68e20156678176182c7
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-04-16 04:58:56 +0000

    OAI-PMH issues
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/portal-project-1.1.1@685 9fd842fd-667d-44da-918d-ff91420b3ae1

--- OAIServer/src/main/java/eu/apenet/oaiserver/response/ListSetsResponse.java
+++ OAIServer/src/main/java/eu/apenet/oaiserver/response/ListSetsResponse.java
@@ -1,10 +1,8 @@
 package eu.apenet.oaiserver.response;
 
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import javax.xml.stream.XMLStreamException;
 
@@ -16,63 +14,76 @@ import eu.apenet.persistence.vo.ResumptionToken;
 
 public class ListSetsResponse extends AbstractResponse {
 
-
-	private static final String SEPARATOR = "-";
-	private List<String> eSets;
 	private ResumptionToken resumptionToken;
 
-	public ListSetsResponse(List<String> eSets, ResumptionToken resumptionToken) {
-		this.eSets = eSets;
+	public ListSetsResponse(ResumptionToken resumptionToken) {
 		this.resumptionToken = resumptionToken;
 	}
+
 	@Override
 	protected void generateResponseInternal(XMLStreamWriterHolder writer, Map<String, String> params)
 			throws XMLStreamException, IOException {
 		writer.writeStartElement(getVerb());
-		Set<String> writedSets = new HashSet<String>();
-		for (String eSet: eSets){
+		appendCountries(writer);
+		List<ArchivalInstitution> archivalInstitutions = DAOFactory.instance().getArchivalInstitutionDAO()
+				.getArchivalInstitutionsWithRepositoryCode();
+		for (ArchivalInstitution archivalInstitution : archivalInstitutions) {
 			writer.writeStartElement("set");
-			writer.writeTextElement("setSpec", eSet);
-			String institutionName = null;
-			String countryName = null;
-			String couIsoname = null;
-			if(!eSet.isEmpty() && eSet.contains(SEPARATOR)){
-				ArchivalInstitution example = new ArchivalInstitution();
-				example.setRepositorycode(eSet);
-				List<ArchivalInstitution> archivalInstitutions = DAOFactory.instance().getArchivalInstitutionDAO().findByExample(example);
-				if (archivalInstitutions.size() > 0) {
-					institutionName = archivalInstitutions.get(0).getAiname();
-				}
-				couIsoname = eSet.substring(0,eSet.indexOf(SEPARATOR));
-				Country country = DAOFactory.instance().getCountryDAO().getCountries(couIsoname).get(0); 
-				if (country != null) {
-					countryName = country.getCname();
-				}
-				writer.writeTextElement("setName", institutionName);
-			}
-			if(institutionName!=null && countryName!=null){
-				writer.writeStartElement("setDescription");
-				writer.writeStartElementNS(OAI_DC_PREFIX, DC_PREFIX, OAIDC_NAMESPACE);
-				writer.writeAttributeNS(XMLStreamWriterHolder.XMLNS, OAI_DC_PREFIX, "", OAIDC_NAMESPACE);
-				writer.writeAttributeNS(XMLStreamWriterHolder.XMLNS, DC_PREFIX, "", DC_METADATA_NAMESPACE);
-				writer.writeAttributeNS(XMLStreamWriterHolder.XSI_PREFIX, XMLStreamWriterHolder.SCHEMA_LOCATION, XMLStreamWriterHolder.XSI_NAMESPACE, OAIDC_NAMESPACE + " "  + OAIDC_SCHEMA_LOCATION_FILE);
-				writer.writeTextElementNS(DC_PREFIX, "title", DC_METADATA_NAMESPACE, "EDM Records for "+institutionName+", "+countryName);
-				writer.writeTextElementNS(DC_PREFIX, "creator", DC_METADATA_NAMESPACE, ARCHIVES_PORTAL_EUROPE);
-				writer.writeTextElementNS(DC_PREFIX, "description", DC_METADATA_NAMESPACE, "This set contains the EDM records created via "+ARCHIVES_PORTAL_EUROPE+" by "+institutionName+", "+countryName);
-				writer.writeTextElementNS(DC_PREFIX, "contributor", DC_METADATA_NAMESPACE, institutionName);
-				writer.closeElement();
-				writer.closeElement();
-			}
+			writer.writeTextElement("setSpec", archivalInstitution.getRepositorycode());
+			String institutionName = archivalInstitution.getAiname();
+			String countryName = archivalInstitution.getCountry().getCname();
+			writer.writeTextElement("setName", institutionName);
+			writer.writeStartElement("setDescription");
+			writer.writeStartElementNS(OAI_DC_PREFIX, DC_PREFIX, OAIDC_NAMESPACE);
+			writer.writeAttributeNS(XMLStreamWriterHolder.XMLNS, OAI_DC_PREFIX, "", OAIDC_NAMESPACE);
+			writer.writeAttributeNS(XMLStreamWriterHolder.XMLNS, DC_PREFIX, "", DC_METADATA_NAMESPACE);
+			writer.writeAttributeNS(XMLStreamWriterHolder.XSI_PREFIX, XMLStreamWriterHolder.SCHEMA_LOCATION,
+					XMLStreamWriterHolder.XSI_NAMESPACE, OAIDC_NAMESPACE + " " + OAIDC_SCHEMA_LOCATION_FILE);
+			writer.writeTextElementNS(DC_PREFIX, "title", DC_METADATA_NAMESPACE, "EDM Records for " + institutionName
+					+ ", " + countryName);
+			writer.writeTextElementNS(DC_PREFIX, "creator", DC_METADATA_NAMESPACE, ARCHIVES_PORTAL_EUROPE);
+			writer.writeTextElementNS(DC_PREFIX, "description", DC_METADATA_NAMESPACE,
+					"This set contains the EDM records created via " + ARCHIVES_PORTAL_EUROPE + " by "
+							+ institutionName + ", " + countryName);
+			writer.writeTextElementNS(DC_PREFIX, "contributor", DC_METADATA_NAMESPACE, institutionName);
 			writer.closeElement();
-			writedSets.add(eSet);
+			writer.closeElement();
+			writer.closeElement();
 		}
+
 		writeResumptionToken(writer, resumptionToken);
 		writer.closeElement();
 	}
 
+	private static void appendCountries(XMLStreamWriterHolder writer) throws XMLStreamException {
+		List<Country> countries = DAOFactory.instance().getCountryDAO().findAll();
+		for (Country country : countries) {
+			writer.writeStartElement("set");
+			writer.writeTextElement("setSpec", country.getIsoname());
+			writer.writeTextElement("setName", country.getCname());
+			writer.writeStartElement("setDescription");
+			writer.writeStartElementNS(OAI_DC_PREFIX, DC_PREFIX, OAIDC_NAMESPACE);
+			writer.writeAttributeNS(XMLStreamWriterHolder.XMLNS, OAI_DC_PREFIX, "", OAIDC_NAMESPACE);
+			writer.writeAttributeNS(XMLStreamWriterHolder.XMLNS, DC_PREFIX, "", DC_METADATA_NAMESPACE);
+			writer.writeAttributeNS(XMLStreamWriterHolder.XSI_PREFIX, XMLStreamWriterHolder.SCHEMA_LOCATION,
+					XMLStreamWriterHolder.XSI_NAMESPACE, OAIDC_NAMESPACE + " " + OAIDC_SCHEMA_LOCATION_FILE);
+			writer.writeTextElementNS(DC_PREFIX, "title", DC_METADATA_NAMESPACE,
+					"EDM Records for " + country.getCname());
+			writer.writeTextElementNS(DC_PREFIX, "creator", DC_METADATA_NAMESPACE, ARCHIVES_PORTAL_EUROPE);
+			writer.writeTextElementNS(
+					DC_PREFIX,
+					"description",
+					DC_METADATA_NAMESPACE,
+					"This set contains the EDM records created via " + ARCHIVES_PORTAL_EUROPE + " by "
+							+ country.getCname());
+			writer.writeTextElementNS(DC_PREFIX, "contributor", DC_METADATA_NAMESPACE, country.getCname());
+			writer.closeElement();
+			writer.closeElement();
+			writer.closeElement();
+		}
+	}
 
-
-	protected String getVerb(){
+	protected String getVerb() {
 		return RequestProcessor.VERB_LIST_SETS;
 	}
 
