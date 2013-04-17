e5af622121bdd233547bb5fc553c4d9c1da4301b
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-04-17 12:14:11 +0000

    merge release 1.2
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@739 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/ead2ese/ConvertAction.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/ead2ese/ConvertAction.java
@@ -1,6 +1,7 @@
 package eu.apenet.dashboard.ead2ese;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -8,11 +9,15 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
+import org.xml.sax.SAXException;
 
 import eu.apenet.commons.types.XmlType;
+import eu.apenet.commons.utils.APEnetUtilities;
 import eu.apenet.commons.view.jsp.SelectItem;
 import eu.apenet.dashboard.AbstractInstitutionAction;
 import eu.apenet.dashboard.actions.ajax.AjaxControllerAbstractAction;
@@ -20,7 +25,12 @@ import eu.apenet.dashboard.actions.content.BatchEadActions;
 import eu.apenet.dashboard.actions.content.ContentManagerAction;
 import eu.apenet.dashboard.services.ead.EadService;
 import eu.apenet.dpt.utils.ead2ese.EseConfig;
+import eu.apenet.dpt.utils.ead2ese.EseFileUtils;
+import eu.apenet.dpt.utils.util.Ead2EseInformation;
 import eu.apenet.persistence.dao.EadSearchOptions;
+import eu.apenet.persistence.factory.DAOFactory;
+import eu.apenet.persistence.vo.Ead;
+import eu.apenet.persistence.vo.FindingAid;
 import eu.apenet.persistence.vo.QueueAction;
 
 public class ConvertAction extends AbstractInstitutionAction  implements ServletRequestAware{
@@ -28,9 +38,13 @@ public class ConvertAction extends AbstractInstitutionAction  implements Servlet
 	private static final String CREATIVECOMMONS_CPDM = "cpdm";
 	private static final String CREATIVECOMMONS_CC0 = "cc0";
 	private static final String CREATIVECOMMONS = "creativecommons";
-	private static final String DATA_PROVIDER_CUSTOM = "custom";
 	private static final String INHERITLANGUAGE_PROVIDE = "provide";
 	private static final String EUROPEANA = "europeana";
+	private static final String TYPE_TEXT = "TEXT";	// Constant for type "text".
+	private static final String OPTION_YES = "yes";	// Constant for value "yes".
+	private static final String OPTION_NO = "no";		// Constant for value "no".
+
+	protected final Logger log = Logger.getLogger(getClass());
 	/**
 	 * 
 	 */
@@ -43,7 +57,6 @@ public class ConvertAction extends AbstractInstitutionAction  implements Servlet
     private Set<SelectItem> yesNoSet = new TreeSet<SelectItem>();
     private Set<SelectItem> inheritLanguageSet = new TreeSet<SelectItem>();
     private Set<SelectItem> providerSet = new TreeSet<SelectItem>();
-    private Set<SelectItem> dataProviderSet = new TreeSet<SelectItem>();
     private Set<SelectItem> licenseSet = new TreeSet<SelectItem>();
     private Set<SelectItem> europeanaLicenseSet = new TreeSet<SelectItem>();
     private String license;
@@ -53,18 +66,20 @@ public class ConvertAction extends AbstractInstitutionAction  implements Servlet
     private String licenseAdditionalInformation;
     private Map<String, String> dateMappings;
     private String filename;
-    private String validateLinks = "no";
-    private String inheritLanguage = "no";
+    private String validateLinks = ConvertAction.OPTION_NO;
+    private String inheritLanguage = ConvertAction.OPTION_NO;
     //private List<LabelValueBean> languages = new ArrayList<SelectItem>();
     private String hierarchyPrefix;
-    private String inheritOrigination= "no";
-    private String inheritFileParent= "no";
-    private String dataProvider = "mapping";
+    private String inheritOrigination= ConvertAction.OPTION_NO;
+    private String inheritFileParent= ConvertAction.OPTION_NO;
     private String customDataProvider;
     private String mappingsFileFileName; 		//The uploaded file name
     private File mappingsFile;					//The uploaded file
     private String mappingsFileContentType;		//The content type of the file uploaded
-    
+    private String textDataProvider;			//Text for the data provider from element "<repository>".
+    private boolean showDataProviderCheck;		//Show or not the check for the data provider
+    private boolean daoTypeCheck = true;
+    private boolean noLanguageOnClevel = true;
     private Set<SelectItem> languages = new TreeSet<SelectItem>();
 	private HttpServletRequest httpServletRequest;
 	
@@ -79,21 +94,34 @@ public class ConvertAction extends AbstractInstitutionAction  implements Servlet
 			if (StringUtils.isBlank(language)){
 				addFieldError("language", getText("errors.required"));
 			}
-		}
-		if (DATA_PROVIDER_CUSTOM.equals(dataProvider)){
-			if (StringUtils.isBlank(customDataProvider)){
-				addFieldError("customDataProvider",getText("errors.required"));
+		} else  if (ConvertAction.OPTION_NO.equals(inheritLanguage)) {
+			if (ConvertAction.TYPE_TEXT.equals(daoType)) {
+				if (noLanguageOnClevel) {
+					addFieldError("inheritLanguage", getText("errors.required")
+						+ getText("errors.clevel.without.langmaterial"));
+				}
 			}
 		}
+
 		if (EUROPEANA.equals(license)){
 			if (StringUtils.isBlank(europeanaLicense)){
 				addFieldError("europeanaLicense", getText("errors.required"));
 			}
-		}	
+		}
 		if (StringUtils.isBlank(daoType)){
 			addFieldError("daoType",getText("errors.required"));
-		} 
+		}
+		if (provider.isEmpty()) {
+			addFieldError("provider", getText("errors.required"));
+		}
+
+		if (textDataProvider.isEmpty() && !this.isShowDataProviderCheck()) {
+			addFieldError("textDataProvider", getText("errors.required"));
+		}
 	}
+
+
+
 	@Override
 	protected void buildBreadcrumbs() {
 		addBreadcrumb("content.action", getText("breadcrumb.section.contentmanager"));
@@ -101,7 +129,13 @@ public class ConvertAction extends AbstractInstitutionAction  implements Servlet
 	}
 
 
-    public ConvertAction() {
+    @Override
+	public void prepare() throws Exception {
+		super.prepare();
+
+    }
+
+	public ConvertAction() {
 		String[] isoLanguages = Locale.getISOLanguages();
 		languages.add(new SelectItem("", getText("ead2ese.content.selectone")));
 		for (String language : isoLanguages) {
@@ -112,17 +146,16 @@ public class ConvertAction extends AbstractInstitutionAction  implements Servlet
 		}  
 		//list="#{'IMAGE':'Image', 'TEXT':'Text', 'SOUND':'Sound', 'VIDEO':'Video'}"
 		typeSet.add(new SelectItem("", getText("ead2ese.content.selectone")));
+		typeSet.add(new SelectItem("3D", getText("ead2ese.content.type.3D")));
 		typeSet.add(new SelectItem("IMAGE", getText("ead2ese.content.type.image")));
-		typeSet.add(new SelectItem("TEXT", getText("ead2ese.content.type.text")));
+		typeSet.add(new SelectItem(ConvertAction.TYPE_TEXT, getText("ead2ese.content.type.text")));
 		typeSet.add(new SelectItem("SOUND", getText("ead2ese.content.type.sound")));
 		typeSet.add(new SelectItem("VIDEO", getText("ead2ese.content.type.video")));
-		yesNoSet.add(new SelectItem("yes", getText("ead2ese.content.yes")));
-		yesNoSet.add(new SelectItem("no", getText("ead2ese.content.no")));
-		inheritLanguageSet.add(new SelectItem("yes", getText("ead2ese.content.yes")));
-		inheritLanguageSet.add(new SelectItem("no", getText("ead2ese.content.no")));
-		inheritLanguageSet.add(new SelectItem(INHERITLANGUAGE_PROVIDE, getText("ead2ese.label.language.select")));	
-		dataProviderSet.add(new SelectItem(DATA_PROVIDER_CUSTOM, getText("ead2ese.content.dataprovider.custom")));
-		dataProviderSet.add(new SelectItem("mapping", getText("ead2ese.content.dataprovider.mapping")));
+		yesNoSet.add(new SelectItem(ConvertAction.OPTION_YES, getText("ead2ese.content.yes")));
+		yesNoSet.add(new SelectItem(ConvertAction.OPTION_NO, getText("ead2ese.content.no")));
+		inheritLanguageSet.add(new SelectItem(ConvertAction.OPTION_YES, getText("ead2ese.content.yes")));
+		inheritLanguageSet.add(new SelectItem(ConvertAction.OPTION_NO, getText("ead2ese.content.no")));
+		inheritLanguageSet.add(new SelectItem(INHERITLANGUAGE_PROVIDE, getText("ead2ese.label.language.select")));
 		licenseSet.add(new SelectItem(EUROPEANA, getText("ead2ese.content.license.europeana")));
 		licenseSet.add(new SelectItem(CREATIVECOMMONS, getText("ead2ese.content.license.creativecommons")));
 		licenseSet.add(new SelectItem(CREATIVECOMMONS_CC0, getText("ead2ese.content.license.creativecommons.cc0")));
@@ -134,11 +167,34 @@ public class ConvertAction extends AbstractInstitutionAction  implements Servlet
 		europeanaLicenseSet.add(new SelectItem("http://www.europeana.eu/rights/rr-r/", getText("ead2ese.content.license.europeana.access.restricted")));
 		europeanaLicenseSet.add(new SelectItem("http://www.europeana.eu/rights/unknown/", getText("ead2ese.content.license.europeana.access.unknown")));
 		this.setHierarchyPrefix(getText("ead2ese.content.hierarchy.prefix" ));
-
     }
-	public String input(){
+
+	public String input() throws IOException, SAXException, ParserConfigurationException{
+		if (StringUtils.isNotBlank(id)){
+			Ead ead = DAOFactory.instance().getEadDAO().findById(Integer.parseInt(id), FindingAid.class);
+			File file = EseFileUtils.getRepoFile(APEnetUtilities.getConfig().getRepoDirPath(),
+					ead.getPathApenetead());
+			Ead2EseInformation ead2EseInformation = new Ead2EseInformation(file, "", getAiname());
+			textDataProvider = ead2EseInformation.getRepository();
+			daoType = ead2EseInformation.getRoleType();
+			if (StringUtils.isBlank(textDataProvider)){
+				Ead2EseInformation ead2EseInformationParent = new Ead2EseInformation(file, "", null);
+				if (ead2EseInformationParent.getArchdescRepository() != null
+						&& !ead2EseInformationParent.getArchdescRepository().isEmpty()) {
+					textDataProvider = ead2EseInformationParent.getArchdescRepository();
+				}
+				this.setShowDataProviderCheck(true);
+			}
+			if (StringUtils.isNotBlank(ead2EseInformation.getLanguageCode())){
+				noLanguageOnClevel = false;			
+			}
+			
+		}else {
+			this.setShowDataProviderCheck(true);			
+		}
 		return SUCCESS;
 	}
+
 	
     public String getId() {
 		return id;
@@ -150,7 +206,7 @@ public class ConvertAction extends AbstractInstitutionAction  implements Servlet
 	}
 
 	@SuppressWarnings("unchecked")
-	public String execute() throws Exception{
+	public String execute() throws Exception{		
 		EseConfig config = fillConfig();
 		if (StringUtils.isBlank(batchItems)){
 			EadService.convertToEseEdm(Integer.parseInt(id), config.getProperties());
@@ -181,15 +237,19 @@ public class ConvertAction extends AbstractInstitutionAction  implements Servlet
 	protected EseConfig fillConfig(){
     	EseConfig config = new EseConfig();  	
     	config.setContextInformationPrefix(hierarchyPrefix);
-    	config.setInheritElementsFromFileLevel("yes".equals(inheritFileParent));
-    	config.setInheritOrigination("yes".equals(inheritOrigination));
-    	config.setInheritLanguage("yes".equals(inheritLanguage));
+    	config.setInheritElementsFromFileLevel(ConvertAction.OPTION_YES.equals(inheritFileParent));
+    	config.setInheritOrigination(ConvertAction.OPTION_YES.equals(inheritOrigination));
+    	config.setInheritLanguage(ConvertAction.OPTION_YES.equals(inheritLanguage));
     	if (INHERITLANGUAGE_PROVIDE.equals(inheritLanguage)){
     		config.setLanguage(language);
     	}
     	config.setType(daoType);
     	config.setProvider(provider);
-    	config.setDataProvider(customDataProvider);
+    	if (customDataProvider != null && !customDataProvider.isEmpty()) {
+    		config.setDataProvider(customDataProvider);
+    	} else if (textDataProvider != null && !textDataProvider.isEmpty()) {
+    		config.setDataProvider(textDataProvider);
+    	}
     	if (EUROPEANA.equals(license)){
     		config.setRights(europeanaLicense);
     	}else if(CREATIVECOMMONS_CC0.equals(license)){
@@ -199,6 +259,7 @@ public class ConvertAction extends AbstractInstitutionAction  implements Servlet
     	}else {
     		config.setRights(cc_js_result_uri);
     	}
+    	config.setUseExistingDaoRole(daoTypeCheck);
     	config.setRightsAdditionalInformation(licenseAdditionalInformation);
     	return config;
 	}
@@ -355,22 +416,6 @@ public class ConvertAction extends AbstractInstitutionAction  implements Servlet
 		this.providerSet = providerSet;
 	}
 
-	public Set<SelectItem> getDataProviderSet() {
-		return dataProviderSet;
-	}
-
-	public void setDataProviderSet(Set<SelectItem> dataProviderSet) {
-		this.dataProviderSet = dataProviderSet;
-	}
-
-	public String getDataProvider() {
-		return dataProvider;
-	}
-
-	public void setDataProvider(String dataProvider) {
-		this.dataProvider = dataProvider;
-	}
-
 	public Set<SelectItem> getLicenseSet() {
 		return licenseSet;
 	}
@@ -427,6 +472,36 @@ public class ConvertAction extends AbstractInstitutionAction  implements Servlet
 		this.batchItems = batchItems;
 	}
 
+	public String getTextDataProvider() {
+		return textDataProvider;
+	}
 
+	public void setTextDataProvider(String textDataProvider) {
+		this.textDataProvider = textDataProvider;
+	}
+
+	public boolean isShowDataProviderCheck() {
+		return showDataProviderCheck;
+	}
+
+	public void setShowDataProviderCheck(boolean showDataProviderCheck) {
+		this.showDataProviderCheck = showDataProviderCheck;
+	}
+
+	public boolean isDaoTypeCheck() {
+		return daoTypeCheck;
+	}
+
+	public void setDaoTypeCheck(boolean daoTypeCheck) {
+		this.daoTypeCheck = daoTypeCheck;
+	}
+
+	public boolean isNoLanguageOnClevel() {
+		return noLanguageOnClevel;
+	}
+
+	public void setNoLanguageOnClevel(boolean noLanguageOnClevel) {
+		this.noLanguageOnClevel = noLanguageOnClevel;
+	}
 	
 }
