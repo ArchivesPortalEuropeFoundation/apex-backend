435568d9180439269a361f244d9735cd9e732a15
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-04-17 11:48:41 +0000

    merge release 1.2
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@723 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/java/eu/apenet/persistence/dao/ArchivalInstitutionDAO.java
+++ persistence/src/main/java/eu/apenet/persistence/dao/ArchivalInstitutionDAO.java
@@ -3,6 +3,10 @@ package eu.apenet.persistence.dao;
 import java.util.Collection;
 import java.util.List;
 
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
 import eu.apenet.persistence.vo.ArchivalInstitution;
 import eu.apenet.persistence.vo.User;
 
@@ -26,6 +30,8 @@ public interface ArchivalInstitutionDAO extends GenericDAO<ArchivalInstitution, 
 	public Long countTotalArchivalInstitutions();
 	//public List<ArchivalInstitution> getGroupsAndArchivalInstitutions(Integer pId, String sortValue, boolean ascending);
 	public List<ArchivalInstitution> getArchivalInstitutionsByRepositorycode(String repositorycode);
+	public List<ArchivalInstitution> getArchivalInstitutionsWithRepositoryCode();
+	public boolean isRepositoryCodeAvailable(String repositorycode, Integer aiId);
 	public List<ArchivalInstitution> getArchivalInstitutionsByAutform(String autform);
 	//public List<ArchivalInstitution> getGroupsAndArchivalInstitutions(List<Partner> listpartners, String sortValue,boolean ascending);
 	//public ArchivalInstitution getArchivalInstitutionbyPartnerandAlIdentifier(List<Partner> listpartners, String internalAlId);
