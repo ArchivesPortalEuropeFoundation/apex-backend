bfe092561bb460eabd68451b658288dc71121dec
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-05-01 06:42:10 +0000

    IssueID 24: Flexible translations
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@837 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/test/java/eu/apenet/dashboard/manual/TestProperties.java
+++ Dashboard/src/test/java/eu/apenet/dashboard/manual/TestProperties.java
@@ -17,12 +17,13 @@ import java.util.Vector;
 
 public class TestProperties {
 
+	private static final String SUFFIX = ".properties";
 	/**
 	 * @param args
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		generateXxxPropertiesFile();
+		generateNewDashboardPropertiesFiles();
 
 	}
 	private static void generateXxxPropertiesFile()throws IOException{
@@ -46,23 +47,89 @@ public class TestProperties {
 				outputStreamWriter.close();
 
 	}
-	private static void sortPropertiesFile()throws IOException{
+	private static void generateNewPortalPropertiesFiles()throws IOException{
 		System.setProperty("file.encoding", "UTF-8");
+		String shared ="shared-resources";
+		String portal ="portal-resources";
 		File propertiesDir = new File(
-				"/home/bastiaan/apex/apexv1/workspace-dashboard/portal-project/Dashboard/src/main/resources/i18n");
+				"/home/bastiaan/apex/apexv1/workspace-new/frontend-project/ape-portal/src/main/resources/i18n");
+		Properties sharedProperties = getProperties(new File(propertiesDir, shared +SUFFIX));
+		Properties portalProperties = getProperties(new File(propertiesDir, portal +SUFFIX));
 		for (File file : propertiesDir.listFiles()) {
 			if (file.getName().startsWith("ApplicationResources_")) {
-				Properties properties = new OwnProperties();
-				InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(file), "UTF-8");
-				properties.load(inputStreamReader);
-				//inputStreamReader.close();
-				File newFile = new File(propertiesDir, "" + file.getName());
-				OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(newFile), "UTF-8");
-				properties.store(outputStreamWriter, "");
-				outputStreamWriter.flush();
-				outputStreamWriter.close();
+				String langSuffix = file.getName().replaceFirst("ApplicationResources", "");
+				Properties sharedLocaleProperties = new OwnProperties();
+				Properties portalLocaleProperties = new OwnProperties();
+				Properties properties = getProperties(file);
+				for (Entry<Object, Object> entry : properties.entrySet()){
+					Object key = entry.getKey();
+					boolean processed = putEntry(sharedProperties, sharedLocaleProperties, entry);
+					if (!processed){
+						processed = putEntry(portalProperties, portalLocaleProperties, entry);
+					}
+					if (!processed){
+						System.out.println(key + " dropped");
+					}
+				}
+				writeProperties(sharedLocaleProperties, new File(propertiesDir, shared +langSuffix));
+				writeProperties(portalLocaleProperties, new File(propertiesDir, portal +langSuffix));
 			}
 		}
+	}
+	private static void generateNewDashboardPropertiesFiles()throws IOException{
+		System.setProperty("file.encoding", "UTF-8");
+		String shared ="shared-resources";
+		String portal ="dashboard-resources";
+		File propertiesDir = new File(
+				"/home/bastiaan/apex/apexv1/workspace-new/backend-project/Dashboard/src/main/resources/i18n");
+		Properties sharedProperties = getProperties(new File(propertiesDir, shared +SUFFIX));
+		//Properties portalProperties = getProperties(new File(propertiesDir, portal +SUFFIX));
+		for (File file : propertiesDir.listFiles()) {
+			if (file.getName().startsWith("ApplicationResources")) {
+				String langSuffix = file.getName().replaceFirst("ApplicationResources", "");
+				Properties sharedLocaleProperties = new OwnProperties();
+				Properties portalLocaleProperties = new OwnProperties();
+				Properties properties = getProperties(file);
+				for (Entry<Object, Object> entry : properties.entrySet()){
+					Object key = entry.getKey();
+					boolean processed = putEntry(sharedProperties, sharedLocaleProperties, entry);
+					if (!processed){
+						portalLocaleProperties.put(key, entry.getValue());
+					}else{
+						System.out.println(key + " dropped");
+					}
+				}
+				//writeProperties(sharedLocaleProperties, new File(propertiesDir, shared +langSuffix));
+				writeProperties(portalLocaleProperties,file);
+			}
+		}
+	}
+	private static boolean putEntry(Properties globalProperties, Properties localProperties,Entry<Object, Object> entry){
+		Object key = entry.getKey();
+		Object value = entry.getValue();
+		if (globalProperties.containsKey(key)){
+//			Object notTranslatedValue = globalProperties.get(key);
+//			if (notTranslatedValue.equals(value)){
+//				System.out.println(key + " has fake translation");
+//			}else {
+				localProperties.put(key, value);
+				return true;
+//			}
+			
+		}
+		return false;
+	}
+	private static Properties getProperties(File file) throws IOException{
+		Properties properties = new OwnProperties();
+		InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(file), "UTF-8");
+		properties.load(inputStreamReader);
+		return properties;
+	}
+	private static void writeProperties(Properties properties, File file) throws IOException{
+		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
+		properties.store(outputStreamWriter, "");
+		outputStreamWriter.flush();
+		outputStreamWriter.close();
 	}
 	static class OwnProperties extends Properties {
 		@Override
