91d3a38e92f62d6430846a13256766ce7d65bdfc
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-05-23 10:24:50 +0000

    update xml downloading and remove old classes
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@1005 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/actions/DownloadGeneralALAction.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/actions/DownloadGeneralALAction.java
@@ -1,10 +1,12 @@
  package eu.apenet.dashboard.actions;
 
-import com.opensymphony.xwork2.ActionSupport;
+import java.io.File;
+
+import org.apache.log4j.Logger;
 
 import eu.apenet.commons.utils.APEnetUtilities;
-import java.io.*;
-import org.apache.log4j.Logger;
+import eu.apenet.dashboard.AbstractAction;
+import eu.apenet.dashboard.utils.ContentUtils;
 
  /**
  * User: Jara Alvarez
@@ -12,46 +14,15 @@ import org.apache.log4j.Logger;
  *
  */
 
-public class DownloadGeneralALAction extends ActionSupport {
+public class DownloadGeneralALAction extends AbstractAction {
 
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7494922156718594589L;
 	private final Logger log = Logger.getLogger(getClass());
-	private InputStream inputStream; //It's necesary to download and preview a file
-	private String fileName; //It's used to manage the file download name
-	private Long fileSize; //It's used to store the file download size
-	 private String httpFileFileName; 		//The uploaded file name
-	   
-	   public InputStream getInputStream() {
-		return inputStream;
-	}
 
-	public void setInputStream(InputStream inputStream) {
-		this.inputStream = inputStream;
-	}
-
-	public String getFileName() {
-		return fileName;
-	}
-
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-
-	public Long getFileSize() {
-		return fileSize;
-	}
-
-	public void setFileSize(Long fileSize) {
-		this.fileSize = fileSize;
-	}
-
-	public String getHttpFileFileName() {
-		return httpFileFileName;
-	}
-
-	public void setHttpFileFileName(String httpFileFileName) {
-		this.httpFileFileName = httpFileFileName;
-	}
 
 	public String execute() {
         String result=null;
@@ -62,26 +33,11 @@ public class DownloadGeneralALAction extends ActionSupport {
 	            addActionMessage(getText("content.message.errorsmall"));
 	        	result= ERROR;
             } else {
-			    this.setInputStream(new FileInputStream(file));
-				this.setHttpFileFileName(file.getName());
-				this.setFileName("AL.xml");
-	            this.setFileSize(file.length());
+            	ContentUtils.downloadXml(getServletRequest(), getServletResponse(), file);
 		    }
-
-	    	if(this.inputStream==null){
-	    	    addActionMessage(getText("content.message.errorsmall"));
-	    		result= ERROR;
-            } else{
-	    	    try {
-	    		    if(this.inputStream.available()>0){
-	    			    result= "download";
-	    		    }
-	    		} catch (IOException e) {
-	    		    log.error("The general archival landscape could not be downloaded");
-	    		}
-	        }
 		} catch (Exception e){
 		    log.error(e.getMessage());
+		    result= ERROR;
 		}
 		return result;
 	}
