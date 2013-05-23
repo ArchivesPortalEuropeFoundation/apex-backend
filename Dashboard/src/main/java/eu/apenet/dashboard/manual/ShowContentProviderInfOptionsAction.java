3c0e609451b6f120236e621c8f0330b7f2bfb294
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-05-23 10:23:22 +0000

    update xml downloading and remove old classes
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@1002 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/manual/ShowContentProviderInfOptionsAction.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/manual/ShowContentProviderInfOptionsAction.java
@@ -1,31 +1,15 @@
 package eu.apenet.dashboard.manual;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Map;
 
 import org.apache.log4j.Logger;
 
-import com.opensymphony.xwork2.ActionContext;
-import com.opensymphony.xwork2.ActionSupport;
-
 import eu.apenet.commons.utils.APEnetUtilities;
 import eu.apenet.dashboard.AbstractInstitutionAction;
+import eu.apenet.dashboard.utils.ContentUtils;
 import eu.apenet.persistence.dao.ArchivalInstitutionDAO;
 import eu.apenet.persistence.factory.DAOFactory;
 import eu.apenet.persistence.vo.ArchivalInstitution;
-
-/**
- * User: Eloy García
- * Date: Nov 3d, 2010
- */
-
-/**
- * This class is in charge of redirecting the user to 
- * Content Provider Information functionality 
- */
 
 public class ShowContentProviderInfOptionsAction extends AbstractInstitutionAction {
 	/**
@@ -34,41 +18,7 @@ public class ShowContentProviderInfOptionsAction extends AbstractInstitutionActi
 	private static final long serialVersionUID = 3716989273471606868L;
 
 	private final Logger log = Logger.getLogger(getClass());
-	//Attributes
 
-	private InputStream inputStream;
-	private String fileName;
-	private Long fileSize;
-	
-	//Getters & Setters
-	public InputStream getInputStream() {
-		return inputStream;
-	}
-
-
-	public void setInputStream(InputStream inputStream) {
-		this.inputStream = inputStream;
-	}
-
-
-	public String getFileName() {
-		return fileName;
-	}
-
-
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-
-
-	public Long getFileSize() {
-		return fileSize;
-	}
-
-
-	public void setFileSize(Long fileSize) {
-		this.fileSize = fileSize;
-	}
 	
 	
 	//Methods
@@ -78,33 +28,16 @@ public class ShowContentProviderInfOptionsAction extends AbstractInstitutionActi
     }
 	
 	public String download() {
-		
-		String value = "";
-		try {
-			//The ai_id is retrieved from the session
 
-			
+		try {
 			ArchivalInstitutionDAO archivalInstitutionDao = DAOFactory.instance().getArchivalInstitutionDAO();
 			ArchivalInstitution archivalInstitution = archivalInstitutionDao.findById(this.getAiId());
-			File tempFile = new File(APEnetUtilities.getConfig().getRepoDirPath() + APEnetUtilities.FILESEPARATOR + archivalInstitution.getEagPath());
-			this.inputStream = new FileInputStream(tempFile);
-			this.fileName = tempFile.getName();
-			this.fileSize = tempFile.length();
+			File file = new File(APEnetUtilities.getConfig().getRepoDirPath() + APEnetUtilities.FILESEPARATOR + archivalInstitution.getEagPath());
+			ContentUtils.downloadXml(getServletRequest(), getServletResponse(), file);
 		} catch (Exception e){
 			log.error(e.getMessage());
 		}
-		if(inputStream==null){
-			value = "error";
-        } else{
-			try {
-				if(inputStream.available()>0){
-					value = "download";
-				}
-			} catch (IOException e) {
-				log.error(e.getMessage());
-			}
-        }
-		
-		return value;
+	
+		return null;
 	}
 }
