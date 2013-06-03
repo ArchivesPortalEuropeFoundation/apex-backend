21067a3fdb7053a4594a7a88bfb9f86ae4196a73
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-06-03 13:10:33 +0000

    Issue 435: Improving linking
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.3@1118 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ Dashboard/src/main/java/eu/apenet/dashboard/actions/content/LinkToHgSgAction.java
@@ -0,0 +1,176 @@
+package eu.apenet.dashboard.actions.content;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.commons.lang.StringUtils;
+import org.xml.sax.SAXException;
+
+import eu.apenet.commons.types.XmlType;
+import eu.apenet.commons.view.jsp.SelectItem;
+import eu.apenet.dashboard.AbstractInstitutionAction;
+import eu.apenet.dashboard.actions.ajax.AjaxControllerAbstractAction;
+import eu.apenet.dashboard.services.ead.EadService;
+import eu.apenet.dashboard.services.ead.LinkingService;
+import eu.apenet.persistence.dao.CLevelDAO;
+import eu.apenet.persistence.dao.EadDAO;
+import eu.apenet.persistence.dao.EadSearchOptions;
+import eu.apenet.persistence.factory.DAOFactory;
+import eu.apenet.persistence.vo.CLevel;
+import eu.apenet.persistence.vo.Ead;
+import eu.apenet.persistence.vo.HoldingsGuide;
+import eu.apenet.persistence.vo.QueueAction;
+import eu.apenet.persistence.vo.SourceGuide;
+
+public class LinkToHgSgAction  extends AbstractInstitutionAction{
+    protected static final String UTF8 = "utf-8";
+	private String id;
+	private String batchItems;
+    private Set<SelectItem> dynamicHgSgs = new TreeSet<SelectItem>();
+    private Set<SelectItem> clevels = new TreeSet<SelectItem>();
+    private String ecId;
+    private String parentCLevelId;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public String input() throws IOException, SAXException, ParserConfigurationException{
+		EadDAO eadDAO = DAOFactory.instance().getEadDAO();
+		EadSearchOptions eadSearchOptions = new EadSearchOptions();
+		eadSearchOptions.setDynamic(true);
+		eadSearchOptions.setEadClazz(HoldingsGuide.class);
+		List<Ead> temp = eadDAO.getEads(eadSearchOptions);
+		for (Ead ead :temp){
+			dynamicHgSgs.add(new SelectItem(ead.getEadContent().getEcId() +"", ead.getTitle()));
+			
+		}
+		eadSearchOptions.setEadClazz(SourceGuide.class);
+		temp = eadDAO.getEads(eadSearchOptions);
+		for (Ead ead :temp){
+			dynamicHgSgs.add(new SelectItem(ead.getEadContent().getEcId() +"", ead.getTitle()));
+		}
+		if (dynamicHgSgs.size() >= 1){
+			ecId = dynamicHgSgs.iterator().next().getValue();
+		}
+		if (StringUtils.isNotBlank(ecId)){
+			CLevelDAO clevelDAO = DAOFactory.instance().getCLevelDAO();
+			long ecIdTemp = Long.parseLong(ecId);
+			List<CLevel> clevelsTemp = clevelDAO.getParentCLevels(ecIdTemp);
+			clevels.add(new SelectItem("", getText("dashboard.hgcreation.linktohgsg.highestlevel")));
+			for (CLevel clevel :clevelsTemp){
+				clevels.add(new SelectItem(clevel.getClId() +"", clevel.getUnittitle()));
+			}
+			
+		}
+		return SUCCESS;
+	}
+	
+	public String retrieveClevels(){
+		if (StringUtils.isNotBlank(ecId)){
+			CLevelDAO clevelDAO = DAOFactory.instance().getCLevelDAO();
+			long ecIdTemp = Long.parseLong(ecId);
+			List<CLevel> clevelsTemp = clevelDAO.getParentCLevels(ecIdTemp);
+			clevels.add(new SelectItem("", getText("dashboard.hgcreation.linktohgsg.highestlevel")));
+			for (CLevel clevel :clevelsTemp){
+				clevels.add(new SelectItem(clevel.getClId() +"", clevel.getUnittitle()));
+			}
+			
+		}
+		return SUCCESS;
+		
+	}
+	public String execute(){
+		Long ecIdLong = Long.parseLong(ecId);
+		Long parentCLevelIdLong = null;
+		if (StringUtils.isNotBlank(parentCLevelId)){
+			parentCLevelIdLong = Long.parseLong(parentCLevelId);
+		}
+		if (StringUtils.isBlank(batchItems)){
+			LinkingService.addFindingaidToHgOrSg(Integer.parseInt(id), getAiId(), ecIdLong, parentCLevelIdLong);
+		}else {
+			if (BatchEadActions.SELECTED_ITEMS.equals(batchItems)) {
+
+				List<Integer> ids = (List<Integer>) getServletRequest().getSession().getAttribute(
+						AjaxControllerAbstractAction.LIST_IDS);
+				if (ids != null) {
+					LinkingService.addFindingaidsToHgOrSg(ids, getAiId(), ecIdLong, parentCLevelIdLong);
+					return SUCCESS;
+				} else {
+					return ERROR;
+				}
+
+			} else if (BatchEadActions.SEARCHED_ITEMS.equals(batchItems)) {
+				EadSearchOptions eadSearchOptions = (EadSearchOptions)getServletRequest().getSession()
+						.getAttribute(ContentManagerAction.EAD_SEARCH_OPTIONS);
+				LinkingService.addFindingaidsToHgOrSg(eadSearchOptions, ecIdLong, parentCLevelIdLong);
+				return SUCCESS;
+			} else {
+				LinkingService.addFindingaidsToHgOrSg(null, ecIdLong, parentCLevelIdLong);
+				return SUCCESS;
+			}
+		}
+   		return SUCCESS;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getBatchItems() {
+		return batchItems;
+	}
+
+	public void setBatchItems(String batchItems) {
+		this.batchItems = batchItems;
+	}
+    protected Writer openOutputWriter() throws IOException {
+        getServletRequest().setCharacterEncoding(UTF8);
+        getServletResponse().setCharacterEncoding(UTF8);
+        getServletResponse().setContentType("application/json");
+        return new OutputStreamWriter(getServletResponse().getOutputStream(), UTF8);
+    }
+
+	public Set<SelectItem> getDynamicHgSgs() {
+		return dynamicHgSgs;
+	}
+
+	public void setDynamicHgSgs(Set<SelectItem> dynamicHgSgs) {
+		this.dynamicHgSgs = dynamicHgSgs;
+	}
+
+	public String getEcId() {
+		return ecId;
+	}
+
+	public void setEcId(String ecId) {
+		this.ecId = ecId;
+	}
+
+	public Set<SelectItem> getClevels() {
+		return clevels;
+	}
+
+	public void setClevels(Set<SelectItem> clevels) {
+		this.clevels = clevels;
+	}
+
+	public String getParentCLevelId() {
+		return parentCLevelId;
+	}
+
+	public void setParentCLevelId(String parentCLevelId) {
+		this.parentCLevelId = parentCLevelId;
+	}
+
+    
+}
--- Dashboard/src/main/java/eu/apenet/dashboard/services/ead/LinkingService.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/services/ead/LinkingService.java
@@ -1,10 +1,33 @@
 package eu.apenet.dashboard.services.ead;
 
+import java.io.File;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
+import eu.apenet.commons.utils.APEnetUtilities;
+import eu.apenet.dashboard.security.SecurityContext;
+import eu.apenet.dashboard.services.ead.xml.EADNamespaceContext;
+import eu.apenet.dpt.utils.service.TransformationTool;
 import eu.apenet.persistence.dao.CLevelDAO;
+import eu.apenet.persistence.dao.EadSearchOptions;
 import eu.apenet.persistence.factory.DAOFactory;
 import eu.apenet.persistence.hibernate.HibernateUtil;
 import eu.apenet.persistence.vo.CLevel;
@@ -13,22 +36,26 @@ import eu.apenet.persistence.vo.EadContent;
 import eu.apenet.persistence.vo.FindingAid;
 import eu.apenet.persistence.vo.HgSgFaRelation;
 import eu.apenet.persistence.vo.HoldingsGuide;
+import eu.apenet.persistence.vo.QueueItem;
+import eu.apenet.persistence.vo.QueuingState;
 import eu.apenet.persistence.vo.SourceGuide;
 import eu.archivesportaleurope.persistence.jpa.JpaUtil;
 
 public class LinkingService {
 	protected static final Logger LOGGER = Logger.getLogger(LinkingService.class);
-	public static boolean linkWithoutCommit(Ead hgOrSg, CLevel clevel){
-		if (hgOrSg instanceof HoldingsGuide || hgOrSg instanceof SourceGuide){
-			Ead linkedFindingAid = DAOFactory.instance().getEadDAO().getEadByEadid(FindingAid.class, hgOrSg.getAiId(), clevel.getHrefEadid());
-			if (linkedFindingAid != null){
+
+	public static boolean linkWithoutCommit(Ead hgOrSg, CLevel clevel) {
+		if (hgOrSg instanceof HoldingsGuide || hgOrSg instanceof SourceGuide) {
+			Ead linkedFindingAid = DAOFactory.instance().getEadDAO()
+					.getEadByEadid(FindingAid.class, hgOrSg.getAiId(), clevel.getHrefEadid());
+			if (linkedFindingAid != null) {
 				HgSgFaRelation hgSgFaRelation = new HgSgFaRelation();
 				hgSgFaRelation.setFaId(linkedFindingAid.getId());
 				hgSgFaRelation.setAiId(hgOrSg.getAiId());
 				hgSgFaRelation.setHgSgClevelId(clevel.getClId());
-				if(hgOrSg instanceof HoldingsGuide){
+				if (hgOrSg instanceof HoldingsGuide) {
 					hgSgFaRelation.setHgId(hgOrSg.getId());
-				}else if(hgOrSg instanceof SourceGuide){
+				} else if (hgOrSg instanceof SourceGuide) {
 					hgSgFaRelation.setSgId(hgOrSg.getId());
 				}
 				HibernateUtil.getDatabaseSession().save(hgSgFaRelation);
@@ -37,14 +64,15 @@ public class LinkingService {
 		}
 		return false;
 	}
-	public static boolean linkWithHgOrSg(Ead ead){
-		if (ead instanceof FindingAid){
+
+	public static boolean linkWithHgOrSg(Ead ead) {
+		if (ead instanceof FindingAid) {
 			try {
 				FindingAid findingAid = (FindingAid) ead;
 				CLevelDAO clevelDAO = DAOFactory.instance().getCLevelDAO();
 				List<CLevel> clevels = clevelDAO.getClevelsFromSgOrHg(findingAid.getAiId(), findingAid.getEadid());
 				JpaUtil.beginDatabaseTransaction();
-				for (CLevel clevel : clevels){
+				for (CLevel clevel : clevels) {
 					HgSgFaRelation hgSgFaRelation = new HgSgFaRelation();
 					hgSgFaRelation.setFaId(findingAid.getId());
 					hgSgFaRelation.setAiId(findingAid.getAiId());
@@ -55,12 +83,102 @@ public class LinkingService {
 					JpaUtil.getEntityManager().persist(hgSgFaRelation);
 				}
 				JpaUtil.commitDatabaseTransaction();
-			}catch (Exception e){
+			} catch (Exception e) {
 				LOGGER.error("Could not link " + ead.getEadid() + " to HG or SG");
 				JpaUtil.rollbackDatabaseTransaction();
 			}
 			return true;
 		}
 		return false;
+	}
+
+	public static boolean addFindingaidToHgOrSg(Integer id, Integer aiId, Long ecId, Long parentCLevelId) {
+		List<Integer> ids = new ArrayList<Integer>();
+		ids.add(id);
+		return addFindingaidsToHgOrSg(ids, aiId, ecId, parentCLevelId);
+	}
+
+	public static boolean addFindingaidsToHgOrSg(List<Integer> ids, Integer aiId, Long ecId, Long parentCLevelId) {
+
+		EadSearchOptions eadSearchOptions = new EadSearchOptions();
+		eadSearchOptions.setPageSize(0);
+		eadSearchOptions.setEadClazz(FindingAid.class);
+		eadSearchOptions.setArchivalInstitionId(aiId);
+		if (ids != null && ids.size() > 0) {
+			eadSearchOptions.setIds(ids);
+		}
+		return addFindingaidsToHgOrSg(eadSearchOptions, ecId, parentCLevelId);
+	}
+
+	public static boolean addFindingaidsToHgOrSg(EadSearchOptions eadSearchOptions, Long ecId, Long parentCLevelId) {
+
+		SecurityContext.get().checkAuthorized(eadSearchOptions.getArchivalInstitionId());
+		EadContent eadContent = DAOFactory.instance().getEadContentDAO().findById(ecId);
+		SecurityContext.get().checkAuthorized(eadContent.getEad());
+		try {
+			XPath xPath = APEnetUtilities.getDashboardConfig().getXpathFactory().newXPath();
+			xPath.setNamespaceContext(new EADNamespaceContext());
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			factory.setNamespaceAware(true);
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc;
+			XPathExpression unittitleExpr = xPath.compile("/ead:c/ead:did/ead:unittitle/text()");
+			XPathExpression unitidExpr = xPath.compile("/ead:c/ead:did/ead:unitid/text()");
+
+			int sizeChildren = 0;
+			if (parentCLevelId != null)
+				sizeChildren = DAOFactory.instance().getCLevelDAO().countChildCLevels(parentCLevelId).intValue();
+			else
+				sizeChildren = DAOFactory.instance().getCLevelDAO().countTopCLevels(ecId).intValue();
+			JpaUtil.beginDatabaseTransaction();
+			List<Ead> eads = DAOFactory.instance().getEadDAO().getEads(eadSearchOptions);
+			int size = 0;
+			while ((size = eads.size()) > 0) {
+				Ead ead = eads.get(size - 1);
+				InputStream xslIs = TransformationTool.class.getResourceAsStream("/xsl/fa2hg.xsl");
+				Source xsltSource = new StreamSource(xslIs);
+				String filePath = APEnetUtilities.getConfig().getRepoDirPath() + ead.getPathApenetead();
+				InputStream fileIs = FileUtils.openInputStream(new File(filePath));
+				Map<String, String> params = new HashMap<String, String>();
+				params.put("addXMLNS", "true");
+				StringWriter stringWriter = new StringWriter();
+
+				TransformationTool.createTransformation(fileIs, stringWriter, xsltSource, params);
+
+				String cLevelXml = stringWriter.toString();
+
+				doc = builder.parse(new InputSource(new StringReader(cLevelXml)));
+				doc.getDocumentElement().normalize();
+
+				String unittitleString = (String) unittitleExpr.evaluate(doc, XPathConstants.STRING);
+				String unitidString = (String) unitidExpr.evaluate(doc, XPathConstants.STRING);
+
+				CLevel cLevel = new CLevel();
+				cLevel.setLeaf(true);
+				cLevel.setLevel("item");
+				cLevel.setXml(cLevelXml);
+				cLevel.setUnitid(unitidString);
+				cLevel.setUnittitle(unittitleString);
+				cLevel.setOrderId(sizeChildren++);
+				cLevel.setParentClId(parentCLevelId);
+				cLevel.setEcId(ecId);
+				JpaUtil.getEntityManager().persist(cLevel);
+				HgSgFaRelation hgSgFaRelation = new HgSgFaRelation();
+				hgSgFaRelation.setFaId(ead.getId());
+				hgSgFaRelation.setAiId(ead.getAiId());
+				hgSgFaRelation.setHgSgClevelId(cLevel.getClId());
+				hgSgFaRelation.setHgId(eadContent.getHgId());
+				hgSgFaRelation.setSgId(eadContent.getSgId());
+				JpaUtil.getEntityManager().persist(hgSgFaRelation);
+				eads.remove(size - 1);
+			}
+			JpaUtil.commitDatabaseTransaction();
+
+			return true;
+		} catch (Exception e) {
+			LOGGER.error("Unable to link FA to HG/SG", e);
+			JpaUtil.rollbackDatabaseTransaction();
+			return false;
+		}
 	}
 }
