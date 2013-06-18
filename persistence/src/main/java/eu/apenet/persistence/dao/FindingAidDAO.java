63a6d09d2ac3c8378c9ba9caaaf2b65959627748
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-06-18 09:56:54 +0000

    old code
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.3@1240 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/java/eu/apenet/persistence/dao/FindingAidDAO.java
+++ persistence/src/main/java/eu/apenet/persistence/dao/FindingAidDAO.java
@@ -1,9 +1,6 @@
 package eu.apenet.persistence.dao;
 
-import java.util.List;
-
 import eu.apenet.persistence.vo.FindingAid;
-import eu.apenet.persistence.vo.HoldingsGuide;
 
 /**
  * 
@@ -12,9 +9,6 @@ import eu.apenet.persistence.vo.HoldingsGuide;
  */
 
 public interface FindingAidDAO extends GenericDAO<FindingAid, Integer> {
-	public List<FindingAid> getFindingAidsNotLinked(Integer aiId);
-	public List<FindingAid> getAllFindingAidByHgId(HoldingsGuide holdingsGuide);
 	public Long getTotalCountOfUnits();
 	public Long getTotalCountOfUnits(Integer aiId);
-	public List<FindingAid> getFindingAidsByHoldingsGuideId(Integer hgId,boolean published, Integer pageSize, Integer pageNumber);
 }
