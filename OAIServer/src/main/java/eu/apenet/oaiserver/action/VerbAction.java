0d44ea644d00f00514820b2e322884fe738d3af0
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-06-28 09:31:07 +0000

    IssueID 628: remove username password
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.3@1292 9fd842fd-667d-44da-918d-ff91420b3ae1

--- OAIServer/src/main/java/eu/apenet/oaiserver/action/VerbAction.java
+++ OAIServer/src/main/java/eu/apenet/oaiserver/action/VerbAction.java
@@ -46,11 +46,7 @@ public class VerbAction extends ActionSupport implements ServletRequestAware, Se
 	 */
 	public String execute() throws Exception {
 		String url = request.getScheme()+"://"+request.getHeader("Host") + request.getContextPath() + REQUEST_SUFIX;
-		String remoteIp = request.getRemoteAddr();
-		if (remoteIp.startsWith("192")){
-			remoteIp = request.getHeader("X-Forwarded-For");
-		}
-		LOG.info(remoteIp+ ": " + url + request.getQueryString());
+		LOG.info(request.getRemoteHost()+ ": " + url + request.getQueryString());
 		response.setBufferSize(4096);
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/xml");
