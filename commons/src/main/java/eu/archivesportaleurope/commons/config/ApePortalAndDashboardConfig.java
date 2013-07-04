ee6896e50f1ce07d26b6111a0ba5cd194b9a8971
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-07-04 06:41:04 +0000

    IssueID 624: add maintenance code
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.3@1331 9fd842fd-667d-44da-918d-ff91420b3ae1

--- commons/src/main/java/eu/archivesportaleurope/commons/config/ApePortalAndDashboardConfig.java
+++ commons/src/main/java/eu/archivesportaleurope/commons/config/ApePortalAndDashboardConfig.java
@@ -2,17 +2,9 @@ package eu.archivesportaleurope.commons.config;
 
 
 public class ApePortalAndDashboardConfig extends ApeConfig{
-    private String solrSearchUrl;
+
     private String archivalLandscapeDirPath;
     private boolean archdescSearchable = true;
-    public String getSolrSearchUrl(){
-        return solrSearchUrl;
-    }
-
-	public void setSolrSearchUrl(String solrSearchUrl) {
-		checkConfigured();
-		this.solrSearchUrl = solrSearchUrl;
-	}
 
 	public String getArchivalLandscapeDirPath() {
 		return archivalLandscapeDirPath;
--- commons/src/main/java/eu/archivesportaleurope/commons/config/ApePortalConfig.java
+++ commons/src/main/java/eu/archivesportaleurope/commons/config/ApePortalConfig.java
@@ -2,6 +2,15 @@ package eu.archivesportaleurope.commons.config;
 
 public class ApePortalConfig extends ApePortalAndDashboardConfig{
 	private String solrStopwordsUrl;
+    private String solrSearchUrl;
+    public String getSolrSearchUrl(){
+        return solrSearchUrl;
+    }
+
+	public void setSolrSearchUrl(String solrSearchUrl) {
+		checkConfigured();
+		this.solrSearchUrl = solrSearchUrl;
+	}
 
 	public String getSolrStopwordsUrl() {
 		return solrStopwordsUrl;
