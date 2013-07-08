fd3a6486c4f4fb9266784d88dd96e7fabf19cc9f
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-07-08 05:48:35 +0000

    IssueID 624: add maintenance code
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.3@1355 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/listener/MaintenanceTask.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/listener/MaintenanceTask.java
@@ -21,52 +21,56 @@ public class MaintenanceTask extends Thread {
 	private static final Logger LOGGER = Logger.getLogger(MaintenanceTask.class);
 	private static final String REINDEX_ALL = "reindex-all";
 	private static final String DELETE_ALL = "delete-all";
+
 	public MaintenanceTask() {
 		JpaUtil.init();
 	}
 
 	@Override
 	public void run() {
+
 		LOGGER.info("Start maintenance mode");
-		EadSearchOptions eadSearchOptions = new EadSearchOptions();
-		eadSearchOptions.setPageSize(MAX_AMOUNT);
-		if (REINDEX_ALL.equals(APEnetUtilities.getDashboardConfig().getMaintenanceAction())){
-			LOGGER.info("Start to add all published items to the queue");
-			eadSearchOptions.setPublished(true);
-			List<QueuingState> queuingStates = new ArrayList<QueuingState>();
-			queuingStates.add(QueuingState.NO);
-			queuingStates.add(QueuingState.ERROR);
-			eadSearchOptions.setQueuing(queuingStates);
-			try {
-				eadSearchOptions.setEadClazz(HoldingsGuide.class);
-				EadService.updateEverything(eadSearchOptions, QueueAction.PUBLISH);
-				eadSearchOptions.setEadClazz(SourceGuide.class);
-				EadService.updateEverything(eadSearchOptions, QueueAction.PUBLISH);
-				eadSearchOptions.setEadClazz(FindingAid.class);
-				EadService.updateEverything(eadSearchOptions, QueueAction.PUBLISH);
-			} catch (IOException e) {
-				LOGGER.error("unexpected error occurs: " +e.getMessage(),e);
-			}
-			
+		try {
+			EadSearchOptions eadSearchOptions = new EadSearchOptions();
+			eadSearchOptions.setPageSize(MAX_AMOUNT);
+			if (REINDEX_ALL.equals(APEnetUtilities.getDashboardConfig().getMaintenanceAction())) {
+				LOGGER.info("Start to add all published items to the queue");
+				eadSearchOptions.setPublished(true);
+				List<QueuingState> queuingStates = new ArrayList<QueuingState>();
+				queuingStates.add(QueuingState.NO);
+				queuingStates.add(QueuingState.ERROR);
+				eadSearchOptions.setQueuing(queuingStates);
+				try {
+					eadSearchOptions.setEadClazz(HoldingsGuide.class);
+					EadService.updateEverything(eadSearchOptions, QueueAction.PUBLISH);
+					eadSearchOptions.setEadClazz(SourceGuide.class);
+					EadService.updateEverything(eadSearchOptions, QueueAction.PUBLISH);
+					eadSearchOptions.setEadClazz(FindingAid.class);
+					EadService.updateEverything(eadSearchOptions, QueueAction.PUBLISH);
+				} catch (IOException e) {
+					LOGGER.error("unexpected error occurs: " + e.getMessage(), e);
+				}
 
-		}else if (DELETE_ALL.equals(APEnetUtilities.getDashboardConfig().getMaintenanceAction())){
-			LOGGER.info("Start to add all files to the queue to delete them");
-			List<QueuingState> queuingStates = new ArrayList<QueuingState>();
-			queuingStates.add(QueuingState.NO);
-			queuingStates.add(QueuingState.ERROR);
-			eadSearchOptions.setQueuing(queuingStates);
-			try {
-				eadSearchOptions.setEadClazz(HoldingsGuide.class);
-				EadService.updateEverything(eadSearchOptions, QueueAction.DELETE);
-				eadSearchOptions.setEadClazz(SourceGuide.class);
-				EadService.updateEverything(eadSearchOptions, QueueAction.DELETE);
-				eadSearchOptions.setEadClazz(FindingAid.class);
-				EadService.updateEverything(eadSearchOptions, QueueAction.DELETE);
-			} catch (IOException e) {
-				LOGGER.error("unexpected error occurs: " +e.getMessage(),e);
+			} else if (DELETE_ALL.equals(APEnetUtilities.getDashboardConfig().getMaintenanceAction())) {
+				LOGGER.info("Start to add all files to the queue to delete them");
+				List<QueuingState> queuingStates = new ArrayList<QueuingState>();
+				queuingStates.add(QueuingState.NO);
+				queuingStates.add(QueuingState.ERROR);
+				eadSearchOptions.setQueuing(queuingStates);
+				try {
+					eadSearchOptions.setEadClazz(HoldingsGuide.class);
+					EadService.updateEverything(eadSearchOptions, QueueAction.DELETE);
+					eadSearchOptions.setEadClazz(SourceGuide.class);
+					EadService.updateEverything(eadSearchOptions, QueueAction.DELETE);
+					eadSearchOptions.setEadClazz(FindingAid.class);
+					EadService.updateEverything(eadSearchOptions, QueueAction.DELETE);
+				} catch (IOException e) {
+					LOGGER.error("unexpected error occurs: " + e.getMessage(), e);
+				}
 			}
+		} finally {
+			JpaUtil.closeDatabaseSession();
 		}
-
 		LOGGER.info("Ends maintenance mode");
 	}
 }
