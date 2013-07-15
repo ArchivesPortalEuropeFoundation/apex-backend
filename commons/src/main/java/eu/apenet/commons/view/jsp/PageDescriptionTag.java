487122413addc4ce16730691b0f5364e5030a383
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-07-15 06:57:00 +0000

    better formatting of huge numbers
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.3@1401 9fd842fd-667d-44da-918d-ff91420b3ae1

--- commons/src/main/java/eu/apenet/commons/view/jsp/PageDescriptionTag.java
+++ commons/src/main/java/eu/apenet/commons/view/jsp/PageDescriptionTag.java
@@ -1,6 +1,7 @@
 package eu.apenet.commons.view.jsp;
 
 import java.io.IOException;
+import java.text.NumberFormat;
 
 import javax.servlet.jsp.JspException;
 
@@ -10,6 +11,7 @@ import org.apache.log4j.Logger;
 public class PageDescriptionTag extends AbstractPagingTag {
 
 	private Logger log = Logger.getLogger(PageDescriptionTag.class);
+	private Object numberFormat;
 	
 
 	public void doTag() throws JspException, IOException {
@@ -18,20 +20,44 @@ public class PageDescriptionTag extends AbstractPagingTag {
 		int pageNumber =  toInteger(this.getPageNumber());
 		int pageSize =  toInteger(this.getPageSize());
 		StringBuilder description = new StringBuilder();
-		description.append((pageNumber-1)*pageSize + 1);
+		description.append(formatNumber((pageNumber-1)*pageSize + 1));
 		description.append(StringEscapeUtils.escapeHtml(" - "));
 		if (pageNumber*pageSize < numberOfItems) {
-			description.append(pageNumber*pageSize);			
+			description.append(formatNumber(pageNumber*pageSize));			
 		}else {
-			description.append(numberOfItems);
+			description.append(formatNumber(numberOfItems));
 		}
 
 		description.append(StringEscapeUtils.escapeHtml(" / "));
-		description.append(numberOfItems);
+		description.append(formatNumber(numberOfItems));
 		this.getJspContext().getOut().print(description);
 		} catch (Exception e) {
 			log.error("Can't create page description: " + e.getMessage(), e);
 			throw new JspException(e);
 		}
 	}
+
+	private String formatNumber(long longValue){
+		if (numberFormat != null){
+			NumberFormat numberFormatter = (NumberFormat) numberFormat;
+			return numberFormatter.format(longValue);
+		}
+		return longValue +"";
+	}
+	private String formatNumber(int intValue){
+		if (numberFormat != null){
+			NumberFormat numberFormatter = (NumberFormat) numberFormat;
+			return numberFormatter.format(intValue);
+		}
+		return intValue +"";
+	}
+	public Object getNumberFormat() {
+		return numberFormat;
+	}
+
+
+	public void setNumberFormat(Object numberFormat) {
+		this.numberFormat = numberFormat;
+	}
+	
 }
