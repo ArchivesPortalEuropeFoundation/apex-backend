9b707ba5ff381908b1c4a792898415b999d26585
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-07-16 07:29:24 +0000

    IssueID 665: implement fix for linking
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.3@1418 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/java/eu/apenet/persistence/vo/QueueAction.java
+++ persistence/src/main/java/eu/apenet/persistence/vo/QueueAction.java
@@ -2,7 +2,7 @@ package eu.apenet.persistence.vo;
 
 public enum QueueAction {
 	VALIDATE("content.message.validate"), CONVERT("content.message.convert"), 
-	PUBLISH("content.message.publish"), CONVERT_VALIDATE_PUBLISH("content.message.doitall"), 
+	PUBLISH("content.message.publish"), REPUBLISH("content.message.republish"),CONVERT_VALIDATE_PUBLISH("content.message.doitall"), 
 	UNPUBLISH("content.message.unpublish"), DELETE("content.message.delete"), 
 	CONVERT_TO_ESE_EDM("content.message.convert.ese"),DELETE_ESE_EDM("content.message.delete.ese"), 
 	DELIVER_TO_EUROPEANA("content.message.deliver.europeana"), DELETE_FROM_EUROPEANA("content.message.delete.europeana" ), 
@@ -26,7 +26,9 @@ public enum QueueAction {
 	public boolean isPublishAction() {
 		return PUBLISH.equals(this) || CONVERT_VALIDATE_PUBLISH.equals(this);
 	}
-
+	public boolean isRePublishAction() {
+		return REPUBLISH.equals(this);
+	}
 	public boolean isConvertToEseEdmAction() {
 		return CONVERT_TO_ESE_EDM.equals(this);
 	}
