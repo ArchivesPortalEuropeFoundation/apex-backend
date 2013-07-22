ee53f691188c49703c9d72aa193a71fdfa672093
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-07-22 07:16:07 +0000

    IssueID 667: google analytics
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.3@1433 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/webapp/js/breadcrumb.js
+++ Dashboard/src/main/webapp/js/breadcrumb.js
@@ -45,16 +45,21 @@ function bBreadcrumb(bcString){
 	}
 }
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-37870082-1']);
-_gaq.push(['_setDomainName', 'archivesportaleurope.net']);
-_gaq.push(['_trackPageview']);
+
 
 function loadGoogleAnalytics() {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
+	var hostname = window.location.hostname;
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	if (hostname == "www.archivesportaleurope.net" || hostname == "archivesportaleurope.net" || hostname == "dashboard.archivesportaleurope.net"){
+		ga('create', 'UA-37870082-1', 'archivesportaleurope.net');
+	}else if (hostname == "contentchecker.archivesportaleurope.net"){
+		ga('create', 'UA-40082820-1', 'contentchecker.archivesportaleurope.net');
+	}else {
+		ga('create', 'UA-42624055-1', 'development.archivesportaleurope.net');
+	}
+	ga('send', 'pageview');
 }
