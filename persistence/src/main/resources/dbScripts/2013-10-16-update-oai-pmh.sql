b1cc6b091e84acffeaf7b9157dfc44ac305b143d
yoannmoranville <yoann@archivesportaleurope.net>
2013-10-16 12:10:38 +0000

    Add constraint to profile table from oai pmh table
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@1767 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/resources/dbScripts/2013-10-16-update-oai-pmh.sql
+++ persistence/src/main/resources/dbScripts/2013-10-16-update-oai-pmh.sql
@@ -6,3 +6,4 @@ ALTER TABLE archival_institution_oai_pmh ALTER COLUMN oai_pmh_set DROP NOT NULL;
 ALTER TABLE archival_institution_oai_pmh ADD COLUMN oai_pmh_metadata character varying(255) NOT NULL;
 ALTER TABLE archival_institution_oai_pmh ADD COLUMN profile_id bigint NOT NULL;
 ALTER TABLE archival_institution_oai_pmh ADD COLUMN next_harvesting date NOT NULL;
+ALTER TABLE archival_institution_oai_pmh ADD CONSTRAINT archival_institutition_oai_pmh_profile_id_fkey FOREIGN KEY (profile_id) REFERENCES userprofile (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE CASCADE;
