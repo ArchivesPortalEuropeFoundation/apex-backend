fde010c66c15396efa9605e49832cfd9f54eecad
yoannmoranville <yoann@archivesportaleurope.net>
2013-10-16 15:31:36 +0000

    Start implemtation of dynamic creation of harvesting url
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@1778 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ OAIHarvester/src/main/java/org/oclc/oai/harvester/parser/other/set/OaiPmhSetParser.java
@@ -0,0 +1,15 @@
+package org.oclc.oai.harvester.parser;
+
+import org.oclc.oai.harvester.parser.other.OaiPmhMemoryParser;
+
+/**
+ * User: Yoann Moranville
+ * Date: 16/10/2013
+ *
+ * @author Yoann Moranville
+ */
+public class OaiPmhSetParser extends OaiPmhMemoryParser {
+    public OaiPmhSetParser() {
+        super();
+    }
+}
