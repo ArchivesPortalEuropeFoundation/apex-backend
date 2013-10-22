253cd15d5c2ea8cb1edd0ce490fff44283dbcb49
stefanpapp-ape <stefan.papp.ape@gmail.com>
2013-10-22 12:36:03 +0000

    Small update on userprofile form, based on latest comments
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@1835 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/manual/UserprofilesAction.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/manual/UserprofilesAction.java
@@ -5,10 +5,9 @@
 package eu.apenet.dashboard.manual;
 
 import eu.apenet.commons.view.jsp.SelectItem;
-import eu.apenet.dashboard.AbstractAction;
-import eu.apenet.dashboard.security.SecurityService;
+import eu.apenet.dashboard.AbstractInstitutionAction;
 import eu.apenet.persistence.factory.DAOFactory;
-import eu.apenet.persistence.vo.User;
+import eu.apenet.persistence.vo.ArchivalInstitution;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -18,7 +17,7 @@ import java.util.Set;
  *
  * @author papp
  */
-public class UserprofilesAction extends AbstractAction {
+public class UserprofilesAction extends AbstractInstitutionAction {
 
     private List userprofiles = new LinkedList();
     private Set<SelectItem> associatedFiletypes = new LinkedHashSet<SelectItem>();
@@ -34,9 +33,7 @@ public class UserprofilesAction extends AbstractAction {
     private String daoType;
 
     public UserprofilesAction() {
-        //User user = SecurityService.getCurrentPartner();
-        //userprofiles = DAOFactory.instance().getUserprofileDAO().getUserprofiles(user.getId());
-        
+        ArchivalInstitution archivalInstitution = DAOFactory.instance().getArchivalInstitutionDAO().getArchivalInstitution(getAiId());
         associatedFiletypes.add(new SelectItem("1", getText("content.message.fa")));
         associatedFiletypes.add(new SelectItem("2", getText("content.message.hg")));
         associatedFiletypes.add(new SelectItem("3", getText("content.message.sg")));
@@ -45,6 +42,7 @@ public class UserprofilesAction extends AbstractAction {
         uploadedFileActions.add(new SelectItem("2", getText("userprofiles.upload.convertValidatePublishEuropeana")));
         uploadedFileActions.add(new SelectItem("3", getText("userprofiles.upload.convert")));
         uploadedFileActions.add(new SelectItem("4", getText("userprofiles.upload.validate")));
+        uploadedFileActions.add(new SelectItem("0", getText("userprofiles.upload.nothing")));
         uploadedFileAction = "1";
         existingFileActions.add(new SelectItem("1", getText("userprofiles.existing.overwrite")));
         existingFileActions.add(new SelectItem("2", getText("userprofiles.existing.keep")));
@@ -57,8 +55,8 @@ public class UserprofilesAction extends AbstractAction {
         daoTypes.add(new SelectItem("3", getText("userprofiles.dao.sound")));
         daoTypes.add(new SelectItem("4", getText("userprofiles.dao.video")));
         daoTypes.add(new SelectItem("5", getText("userprofiles.dao.3D")));
-        daoTypes.add(new SelectItem("6", getText("userprofiles.dao.unspecified")));
-        daoType = "6";
+        daoTypes.add(new SelectItem("0", getText("userprofiles.dao.unspecified")));
+        daoType = "0";
     }
 
     public List getUserprofiles() {
