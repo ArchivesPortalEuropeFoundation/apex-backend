c9de55666cce9f23a1a29c5e526ad92aea6c34f6
yoannmoranville <yoann@archivesportaleurope.net>
2013-11-15 15:44:25 +0000

    fix bad sql query
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@1950 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/resources/dbScripts/2013-11-12-update-userprofile.sql
+++ persistence/src/main/resources/dbScripts/2013-11-12-update-userprofile.sql
@@ -1,3 +1,1 @@
-﻿ALTER TABLE userprofile ADD COLUMN dao_type_from_file boolean;
-ALTER TABLE userprofile ALTER COLUMN dao_type_from_file SET NOT NULL;
-ALTER TABLE userprofile ALTER COLUMN dao_type_from_file SET DEFAULT true;
+﻿ALTER TABLE userprofile ADD COLUMN dao_type_from_file boolean NOT NULL DEFAULT true;
