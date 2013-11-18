b289c70b8111502a7a265e46161cb589355e8fac
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-11-18 07:29:37 +0000

    fix for sitemap
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@1951 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/resources/dbScripts/2013-11-14-add-published-dates.sql
+++ persistence/src/main/resources/dbScripts/2013-11-14-add-published-dates.sql
@@ -1,6 +1,8 @@
 ALTER TABLE finding_aid ADD COLUMN publish_date timestamp without time zone NULL;
 ALTER TABLE source_guide ADD COLUMN publish_date timestamp without time zone NULL;
 ALTER TABLE holdings_guide ADD COLUMN publish_date timestamp without time zone NULL;
+ALTER TABLE archival_institution ADD COLUMN content_lastmodified_date timestamp without time zone NULL;
 UPDATE finding_aid SET  publish_date=NOW() WHERE published = true;
 UPDATE source_guide SET  publish_date=NOW() WHERE published = true;
 UPDATE holdings_guide SET  publish_date=NOW() WHERE published = true;
+UPDATE archival_institution SET  content_lastmodified_date=NOW() WHERE contain_searchable_items = true;
