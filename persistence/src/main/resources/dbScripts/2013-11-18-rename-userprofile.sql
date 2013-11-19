2d5a505a9e124f8d1b3aa7ff771789b6daf74de7
stefanpapp-ape <stefan.papp.ape@gmail.com>
2013-11-19 08:55:12 +0000

    fix encoding
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@1965 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/resources/dbScripts/2013-11-18-rename-userprofile.sql
+++ persistence/src/main/resources/dbScripts/2013-11-18-rename-userprofile.sql
@@ -1,3 +1,3 @@
 ALTER TABLE userprofile RENAME TO ingestionprofile;
-ALTER TABLE ingestionprofile   RENAME CONSTRAINT userprofile_pkey TO ingestionprofile_pkey;
-ALTER TABLE ingestionprofile   RENAME CONSTRAINT userprofile_ai_id_fkey TO ingestionprofile_ai_id_fkey;
+ALTER TABLE ingestionprofile RENAME CONSTRAINT userprofile_pkey TO ingestionprofile_pkey;
+ALTER TABLE ingestionprofile RENAME CONSTRAINT userprofile_ai_id_fkey TO ingestionprofile_ai_id_fkey;
