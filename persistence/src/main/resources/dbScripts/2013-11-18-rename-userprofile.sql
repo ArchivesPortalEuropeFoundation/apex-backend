76f071d320f1383bdd7b6e6c7853e76c6fae8038
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-11-19 08:39:45 +0000

    fix in sql files
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@1964 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/resources/dbScripts/2013-11-18-rename-userprofile.sql
+++ persistence/src/main/resources/dbScripts/2013-11-18-rename-userprofile.sql
@@ -1,6 +1,3 @@
-﻿ALTER TABLE userprofile
-  RENAME TO ingestionprofile;
-  RENAME CONSTRAINT userprofile_pkey TO ingestionprofile_pkey;
-  RENAME CONSTRAINT userprofile_ai_id_fkey TO ingestionprofile_ai_id_fkey;
-ALTER INDEX public.fki_userprofile_fkey_ai_id
-  RENAME TO fki_ingestionprofile_fkey_ai_id;
+ALTER TABLE userprofile RENAME TO ingestionprofile;
+ALTER TABLE ingestionprofile   RENAME CONSTRAINT userprofile_pkey TO ingestionprofile_pkey;
+ALTER TABLE ingestionprofile   RENAME CONSTRAINT userprofile_ai_id_fkey TO ingestionprofile_ai_id_fkey;
