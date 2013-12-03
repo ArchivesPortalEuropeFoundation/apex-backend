790bb15c20b3e55dcd8f629600120ac82cf1886f
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-12-03 07:59:54 +0000

    remove old AL code.
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.5@2046 9fd842fd-667d-44da-918d-ff91420b3ae1

--- commons/src/main/java/eu/archivesportaleurope/commons/config/ApePortalAndDashboardConfig.java
+++ commons/src/main/java/eu/archivesportaleurope/commons/config/ApePortalAndDashboardConfig.java
@@ -3,25 +3,6 @@ package eu.archivesportaleurope.commons.config;
 
 public class ApePortalAndDashboardConfig extends ApeConfig{
 
-    private String archivalLandscapeDirPath;
-    private boolean archdescSearchable = true;
-
-	public String getArchivalLandscapeDirPath() {
-		return archivalLandscapeDirPath;
-	}
-
-	public void setArchivalLandscapeDirPath(String archivalLandscapeDirPath) {
-		checkConfigured();
-		this.archivalLandscapeDirPath = archivalLandscapeDirPath;
-	}
-
-	public boolean isArchdescSearchable() {
-		return archdescSearchable;
-	}
-
-	public void setArchdescSearchable(boolean archdescSearchable) {
-		this.archdescSearchable = archdescSearchable;
-	}
 
 
 	
