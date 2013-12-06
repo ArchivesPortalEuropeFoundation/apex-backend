97a072fd842153128c7f20fb769ca0cc59ed1d17
yoannmoranville <yoann@archivesportaleurope.net>
2013-12-06 14:53:03 +0000

    All in finally block
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.5@2073 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/harvest/DataHarvester.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/harvest/DataHarvester.java
@@ -102,15 +102,14 @@ public class DataHarvester implements Runnable {
             UserService.sendEmailHarvestFinished(true, archivalInstitution, partner, numberEadHarvested, currentInfoArchivalInstitutionOaiPmh, oldestFileHarvested.toString(), newestFileHarvested.toString());
             LOGGER.info("Harvest completed: harvested " + numberEadHarvested + " EAD files from " + currentInfoArchivalInstitutionOaiPmh + " --- Oldest file harvested: " + oldestFileHarvested.toString() + " --- Newest file harvested: " + newestFileHarvested.toString());
         } catch (Exception e) {
-            if(outputDirectory.exists()) {
-                outputDirectory.delete();
-            }
             JpaUtil.rollbackDatabaseTransaction();
             UserService.sendEmailHarvestFinished(false, archivalInstitution, partner, 0, currentInfoArchivalInstitutionOaiPmh, "-", "-");
             LOGGER.error("Harvest failed for " + currentInfoArchivalInstitutionOaiPmh);
             LOGGER.error("Harvesting failed - should we put an 'error' flag in the DB?");
         } finally {
-            outputDirectory.delete();
+            if(outputDirectory.exists()) {
+                outputDirectory.delete();
+            }
             if(!isNighlySchedule) {
                 HarvesterDaemon.setHarvesterProcessing(false);
             }
