83ca3bf4a7487fe70a0189812d1e8c7576775469
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-12-09 11:56:26 +0000

    IssueID 823
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.5@2096 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ OAIHarvester/src/main/java/eu/archivesportaleurope/harvester/parser/other/OaiPmhMemoryParser.java
@@ -0,0 +1,30 @@
+package eu.archivesportaleurope.harvester.parser.other;
+
+import org.apache.log4j.Logger;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: Yoann Moranville
+ * Date: 16/10/2013
+ *
+ * @author Yoann Moranville
+ */
+public abstract class OaiPmhMemoryParser {
+    protected static final Logger LOG = Logger.getLogger(OaiPmhMemoryParser.class);
+    protected static final String UTF8 = "UTF-8";
+    protected static final String OAI_PMH = "http://www.openarchives.org/OAI/2.0/";
+
+    protected static final QName METADATA_FORMAT = new QName(OAI_PMH, "metadataFormat");
+    protected static final QName SET = new QName(OAI_PMH, "set");
+
+    protected static final QName RESUMPTION_TOKEN = new QName(OAI_PMH, "resumptionToken");
+    protected static final QName ERROR = new QName(OAI_PMH, "error");
+
+    public abstract OaiPmhElements parse(XMLStreamReader xmlReader) throws XMLStreamException;
+}
