e7939980419a4b0e1ece25485d557b30a7f7850c
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-12-12 08:31:47 +0000

    IssueID 446: Sets with only records
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.5@2137 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/java/eu/apenet/persistence/hibernate/EseHibernateDAO.java
+++ persistence/src/main/java/eu/apenet/persistence/hibernate/EseHibernateDAO.java
@@ -5,6 +5,9 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
+
+import javax.persistence.TypedQuery;
+
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.hibernate.Criteria;
@@ -14,16 +17,19 @@ import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Property;
 import org.hibernate.criterion.Restrictions;
 import org.hibernate.criterion.Subqueries;
+
 import eu.apenet.persistence.dao.EseDAO;
 import eu.apenet.persistence.vo.Ese;
 import eu.apenet.persistence.vo.EseState;
+import eu.apenet.persistence.vo.FileType;
 import eu.apenet.persistence.vo.FindingAid;
 import eu.apenet.persistence.vo.MetadataFormat;
+import eu.apenet.persistence.vo.UpFile;
 
 public class EseHibernateDAO extends AbstractHibernateDAO<Ese, Integer> implements EseDAO {
 
 	private final Logger log = Logger.getLogger(getClass());
-	
+
 	@SuppressWarnings("unchecked")
 	public List<Ese> getEses(Integer faId, Integer aiId) {
 		long startTime = System.currentTimeMillis();
@@ -31,7 +37,7 @@ public class EseHibernateDAO extends AbstractHibernateDAO<Ese, Integer> implemen
 		Criteria criteria = getSession().createCriteria(getPersistentClass(), "ese");
 		criteria = criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
 		criteria = criteria.createAlias("ese.findingAid", "findingAid");
-			criteria.add(Restrictions.eq("findingAid.id", faId.intValue()));
+		criteria.add(Restrictions.eq("findingAid.id", faId.intValue()));
 
 		criteria.createAlias("findingAid.archivalInstitution", "archivalInstitution");
 		criteria.add(Restrictions.eq("archivalInstitution.aiId", aiId));
@@ -44,8 +50,7 @@ public class EseHibernateDAO extends AbstractHibernateDAO<Ese, Integer> implemen
 		return results;
 	}
 
-	
-	@SuppressWarnings("unchecked")	
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Ese> getEsesFromDeletedFindingaids(String oaiIdentifier) {
 		long startTime = System.currentTimeMillis();
@@ -64,223 +69,94 @@ public class EseHibernateDAO extends AbstractHibernateDAO<Ese, Integer> implemen
 	}
 
 	@Override
-	public Date getTheEarliestDatestamp(){
-		Criteria criteria = getSession().createCriteria(getPersistentClass(),"ese");
+	public Date getTheEarliestDatestamp() {
+		Criteria criteria = getSession().createCriteria(getPersistentClass(), "ese");
 		criteria = criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
 		criteria.setProjection(Projections.min("creationDate"));
-		return (Date)criteria.uniqueResult();
+		return (Date) criteria.uniqueResult();
 	}
+
 	@Override
-	public List<Ese> getEsesByArguments(Date from,Date until,MetadataFormat metadataFormat,String set,Integer startElement,Integer limitPerResponse){
-		Criteria criteria = getSession().createCriteria(getPersistentClass(),"ese");
+	public List<Ese> getEsesByArguments(Date from, Date until, MetadataFormat metadataFormat, String set,
+			Integer startElement, Integer limitPerResponse) {
+		Criteria criteria = getSession().createCriteria(getPersistentClass(), "ese");
 		criteria = criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
-		if(from!=null && until!=null){
+		if (from != null && until != null) {
 			criteria.add(Restrictions.between("modificationDate", from, until));
-		}else if(until!=null){
+		} else if (until != null) {
 			criteria.add(Restrictions.le("modificationDate", until));
-		}else if(from!=null){
+		} else if (from != null) {
 			criteria.add(Restrictions.ge("modificationDate", from));
 		}
-		if(metadataFormat!=null){
-			criteria.add(Restrictions.eq("metadataFormat",metadataFormat));
+		if (metadataFormat != null) {
+			criteria.add(Restrictions.eq("metadataFormat", metadataFormat));
 		}
-		criteria.add(Restrictions.between("eseState.id",2,3));
-		if(set!=null && !set.isEmpty()){
+		criteria.add(Restrictions.between("eseState.id", 2, 3));
+		if (set != null && !set.isEmpty()) {
 			/*
-			if(set.endsWith(":")){
-				criteria.add(Restrictions.ilike("eset",set+"%"));
-			}else{
-				criteria.add(Restrictions.like("eset",set));
-			}
-			*/
-			criteria.add(Restrictions.like("eset",set + "%"));
+			 * if(set.endsWith(":")){
+			 * criteria.add(Restrictions.ilike("eset",set+"%")); }else{
+			 * criteria.add(Restrictions.like("eset",set)); }
+			 */
+			criteria.add(Restrictions.like("eset", set + "%"));
 		}
 		Integer firstResult = null;
-		if(startElement==null){
+		if (startElement == null) {
 			firstResult = 0;
-		}else{
+		} else {
 			firstResult = startElement;
 		}
 		criteria.setFirstResult(firstResult);
 		Integer limit = null;
-		if(limitPerResponse==null){
+		if (limitPerResponse == null) {
 			limit = 100;
-		}else{
-			limit = limitPerResponse+1;
+		} else {
+			limit = limitPerResponse + 1;
 		}
 		criteria.setMaxResults(limit);
 		return criteria.list();
 	}
 
 	@Override
-	public Set<Ese> getAllEsesInSetFormat(){
-		return null;
-	}
-
-	@Override
-	public List<String> getSetsOfEses(Date startDate,Date endDate,String eset,Integer start,Integer maxResults) {
-		Criteria criteria = getSession().createCriteria(getPersistentClass(),"ese");
-		//criteria = criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
-		criteria.add(Restrictions.eq("eseState.id",2));//Published
-		if(startDate!=null && endDate!=null){
-			criteria.add(Restrictions.between("modificationDate", startDate, endDate));
-		}else if(startDate!=null){
-			criteria.add(Restrictions.le("modificationDate", startDate));
-		}else if(endDate!=null){
-			criteria.add(Restrictions.ge("modificationDate", endDate));
-		}
-		if(eset!=null){
-			criteria.add(Restrictions.like("eset", eset));
-		}if(start!=null){
-			criteria.setFirstResult(start);
-			if(maxResults!=null){
-				criteria.setMaxResults(maxResults);
-			}else{
-				criteria.setMaxResults(101);
-			}
-		}
-		return criteria.setProjection(Projections.distinct(Projections.property("eset"))).list();
-	}
-
-	@Override
-	public List<Ese> getEsesWithoutFindingAid() {
-		Criteria criteria = getSession().createCriteria(getPersistentClass(),"ese");
+	public Ese getEseByIdentifierAndFormat(String identifier, MetadataFormat metadataFormat) {
+		Criteria criteria = getSession().createCriteria(getPersistentClass(), "ese");
 		criteria = criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
-		criteria.add(Restrictions.isNull("findingAid"));
-		return criteria.list();
-	}
-	
-	@Override
-	public Ese getEseByIdentifierAndFormat(String identifier,MetadataFormat metadataFormat){
-		Criteria criteria = getSession().createCriteria(getPersistentClass(),"ese");
-		criteria = criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
-		if(identifier!=null){
-			criteria.add(Restrictions.like("oaiIdentifier",identifier));
+		if (identifier != null) {
+			criteria.add(Restrictions.like("oaiIdentifier", identifier));
 		}
-		if(metadataFormat!=null){
-			criteria.add(Restrictions.eq("metadataFormat",metadataFormat));
+		if (metadataFormat != null) {
+			criteria.add(Restrictions.eq("metadataFormat", metadataFormat));
 		}
-		if(identifier!=null){
+		if (identifier != null) {
 			return (Ese) criteria.uniqueResult();
-		}else if(metadataFormat!=null){
+		} else if (metadataFormat != null) {
 			List<Ese> tempList = criteria.list();
-			if(tempList!=null && !tempList.isEmpty()){
+			if (tempList != null && !tempList.isEmpty()) {
 				return (Ese) tempList.get(0);
 			}
 		}
 		return null;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
-	public List<Ese> getEsesByFindingAidAndState(Integer faId,EseState eseState){
-		Criteria criteria = getSession().createCriteria(getPersistentClass(),"ese");
+	public List<Ese> getEsesByFindingAidAndState(Integer faId, EseState eseState) {
+		Criteria criteria = getSession().createCriteria(getPersistentClass(), "ese");
 		criteria = criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
-		if(faId!=null){
+		if (faId != null) {
 			criteria.add(Restrictions.eq("findingAid.id", faId));
 		}
-		if(eseState!=null){
+		if (eseState != null) {
 			criteria.add(Restrictions.eq("eseState", eseState));
 		}
 		return criteria.list();
 	}
 
 	@Override
-	public Long getNumberOfRecordsByAiId(Integer aiId){
-		if(aiId!=null){
-			Criteria criteria = getSession().createCriteria(getPersistentClass(),"ese");
-			criteria = criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
-			DetachedCriteria subQuery = DetachedCriteria.forClass(FindingAid.class,"findingAid");
-			subQuery.setProjection(Property.forName("findingAid.id"));
-			subQuery.add(Restrictions.between("fileState.id",9,14));
-			subQuery.add(Restrictions.eq("archivalInstitution.aiId", aiId));
-			criteria.setProjection(Projections.sum("ese.numberOfRecords"));
-			criteria.add(Restrictions.between("ese.eseState.id",1,3));
-			criteria.add(Subqueries.propertyIn("ese.findingAid.id",subQuery));
-			return (Long)criteria.uniqueResult();
-		}
-		return new Long(0);
+	public List<String> getSets() {
+		String query = "SELECT DISTINCT(eset) FROM Ese";
+		TypedQuery<String> typedQuery = getEntityManager().createQuery(query, String.class);
+		return typedQuery.getResultList();
 	}
 
-	@Override
-	public Long getNumberOfRecordsDeliveredByAiId(Integer aiId) {
-		if(aiId!=null){
-			Criteria criteria = getSession().createCriteria(getPersistentClass(),"ese");
-			criteria = criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
-			DetachedCriteria subQuery = DetachedCriteria.forClass(FindingAid.class,"findingAid");
-			subQuery.setProjection(Property.forName("findingAid.id"));
-			subQuery.add(Restrictions.eq("archivalInstitution.aiId", aiId));
-			criteria.setProjection(Projections.sum("ese.numberOfRecords"));
-			criteria.add(Restrictions.eq("ese.eseState.id",2));
-			criteria.add(Subqueries.propertyIn("ese.findingAid.id",subQuery));
-			return (Long)criteria.uniqueResult();
-		}
-		return new Long(0);
-	}
-
-	@Override
-	public Long getSearchedNumberOfRecordsByAiId(String searchTerms,Integer aiId,int option,Set<String> convertedToESEFileStates) {
-		if(aiId!=null){
-			Criteria criteria = getSession().createCriteria(getPersistentClass(),"ese");
-			criteria = criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
-			DetachedCriteria subQuery = getSubQueryRestrictions(searchTerms,aiId,option,convertedToESEFileStates);
-			criteria.setProjection(Projections.sum("ese.numberOfRecords"));
-			criteria.add(Restrictions.between("ese.eseState.id",1,3));
-			criteria.add(Subqueries.propertyIn("ese.findingAid.id",subQuery));
-			return (Long)criteria.uniqueResult();
-		}
-		return new Long(0);
-	}
-
-	@Override
-	public Long getTotalDeliveredToEuropeanaRecordsByAiId(String searchTerms,Integer aiId,int option,Set<String> deliveredToEuropeanaFileStates) {
-		if(aiId!=null){
-			Criteria criteria = getSession().createCriteria(getPersistentClass(),"ese");
-			criteria = criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
-			DetachedCriteria subQuery = getSubQueryRestrictions(searchTerms,aiId,option,deliveredToEuropeanaFileStates);
-			criteria.setProjection(Projections.sum("ese.numberOfRecords"));
-			criteria.add(Restrictions.eq("ese.eseState.id",2));
-			criteria.add(Subqueries.propertyIn("ese.findingAid.id",subQuery));
-			return (Long)criteria.uniqueResult();
-		}
-		return new Long(0);
-	}
-
-	private DetachedCriteria getSubQueryRestrictions(String searchTerms, Integer aiId,int option,Set<String> deliveredToEuropeanaFileStates) {
-		DetachedCriteria subQuery = DetachedCriteria.forClass(FindingAid.class,"findingAid");
-		subQuery.setProjection(Property.forName("findingAid.id"));
-		subQuery.add(Restrictions.eq("archivalInstitution.aiId", aiId));
-		subQuery = createMatchFindingAidsCriteria(subQuery, searchTerms, aiId, option, deliveredToEuropeanaFileStates);
-		return subQuery;
-	}
-	
-	private DetachedCriteria createMatchFindingAidsCriteria(DetachedCriteria subQuery,String queries, Integer ai,int option,Collection<String> fileStates) {
-		if (StringUtils.isNotBlank(queries)) {
-			String[] query = StringUtils.split(queries, " ");
-			if (option == 0) {
-				for (int i = 0; i < query.length; i++) {
-					subQuery.add(Restrictions.or(Restrictions.like("findingAid.title", "%" + query[i] + "%"),
-							Restrictions.like("findingAid.eadid", "%" + query[i] + "%")));
-				}
-			} else if (option == 1) {
-				for (int i = 0; i < query.length; i++) {
-					subQuery.add(Restrictions.like("findingAid.eadid", "%" + query[i] + "%"));
-				}
-			} else if (option == 2) {
-				for (int i = 0; i < query.length; i++) {
-					subQuery.add(Restrictions.like("findingAid.title", "%" + query[i] + "%"));
-				}
-			}
-		}
-		if (fileStates != null && fileStates.size() > 0) {
-			subQuery = subQuery.createAlias("findingAid.fileState", "fileState");
-			Disjunction disjunction = Restrictions.disjunction();
-			for (String fileState : fileStates) {
-				disjunction.add(Restrictions.eq("fileState.state", fileState));
-			}
-			subQuery.add(disjunction);
-		}
-        subQuery.add(Restrictions.eq("archivalInstitution.aiId", ai.intValue()));
-		return subQuery;
-	}
 }
