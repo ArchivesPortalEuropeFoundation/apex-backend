59c202cd4155c5f61e8e4726cdce7cd7d2dfd280
stefanpapp-ape <stefan.papp.ape@gmail.com>
2013-12-13 11:23:00 +0000

    bug #930 --> fix permissions
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.5@2146 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ persistence/src/main/resources/dbScripts/2013-12-13-fix-permissions.sql
@@ -0,0 +1,4 @@
+﻿REVOKE ALL ON SEQUENCE userprofile_id_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE userprofile_id_seq FROM admin;
+GRANT ALL ON SEQUENCE userprofile_id_seq TO admin;
+GRANT ALL ON SEQUENCE userprofile_id_seq TO apenet_dashboard;
