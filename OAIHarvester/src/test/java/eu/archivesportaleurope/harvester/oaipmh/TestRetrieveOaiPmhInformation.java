4fc0b93497f9253e3c17fa050234357c4ec6fd88
bastiaan <bastiaan.verhoef.ape@gmail.com>
2013-12-17 06:22:23 +0000

    proxy support for Console Harvester
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.5@2159 9fd842fd-667d-44da-918d-ff91420b3ae1

--- OAIHarvester/src/test/java/eu/archivesportaleurope/harvester/oaipmh/TestRetrieveOaiPmhInformation.java
+++ OAIHarvester/src/test/java/eu/archivesportaleurope/harvester/oaipmh/TestRetrieveOaiPmhInformation.java
@@ -1,28 +1,32 @@
 package eu.archivesportaleurope.harvester.oaipmh;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import eu.archivesportaleurope.harvester.oaipmh.RetrieveOaiPmhInformation;
 import eu.archivesportaleurope.harvester.parser.other.OaiPmhElement;
+import eu.archivesportaleurope.harvester.util.OaiPmhHttpClient;
 
 import java.lang.Exception;
 import java.lang.String;
 import java.lang.System;
-
+@Ignore
 public class TestRetrieveOaiPmhInformation {
-    @Test
+    @Test @Ignore
     public void test() {
        String url = "http://archives.cantal.fr/oai_pmh.cgi";
 //        String url = "http://www.archieven.nl/pls/oai/!pck_oai_pmh.OAIHandler";
         try {
+        	OaiPmhHttpClient oaiPmhHttpClient = new OaiPmhHttpClient();
             System.out.println("Sets:");
-            for(OaiPmhElement s : RetrieveOaiPmhInformation.retrieveSets(url)) {
+            for(OaiPmhElement s : RetrieveOaiPmhInformation.retrieveSets(url,oaiPmhHttpClient)) {
                 System.out.println("- " + s);
             }
             System.out.println("Metadata Formats:");
-            for(OaiPmhElement mf : RetrieveOaiPmhInformation.retrieveMetadataFormats(url)) {
+            for(OaiPmhElement mf : RetrieveOaiPmhInformation.retrieveMetadataFormats(url,oaiPmhHttpClient)) {
                 System.out.println("- " + mf);
             }
+            oaiPmhHttpClient.close();
         } catch (Exception e) {
             System.out.println("Error " + e);;
             e.printStackTrace();
