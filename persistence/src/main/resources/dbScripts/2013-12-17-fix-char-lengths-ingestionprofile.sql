d19d6a4caf29b2a7768bd6bfd70f18842cd8ead6
stefanpapp-ape <stefan.papp.ape@gmail.com>
2013-12-17 13:59:57 +0000

    Fix #930, comment 19
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.5@2173 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ persistence/src/main/resources/dbScripts/2013-12-17-fix-char-lengths-ingestionprofile.sql
@@ -0,0 +1,6 @@
+﻿ALTER TABLE ingestionprofile
+   ALTER COLUMN name_profile TYPE character varying(100);
+   ALTER COLUMN europeana_languages TYPE character varying(100);
+   ALTER COLUMN europeana_hp TYPE character varying(100);
+   ALTER COLUMN europeana_license TYPE character varying(100);
+   ALTER COLUMN europeana_license_details TYPE character varying(100);
