7eb414f5375add1b6e4189677abfa18f1dec8392
yoannmoranville <yoann@archivesportaleurope.net>
2013-12-24 13:16:14 +0000

    Bad SQL... Really bad SQL...
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.5@2196 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/resources/dbScripts/2013-12-17-fix-char-lengths-ingestionprofile.sql
+++ persistence/src/main/resources/dbScripts/2013-12-17-fix-char-lengths-ingestionprofile.sql
@@ -1,6 +1,5 @@
-﻿ALTER TABLE ingestionprofile
-   ALTER COLUMN name_profile TYPE character varying(100);
-   ALTER COLUMN europeana_languages TYPE character varying(100);
-   ALTER COLUMN europeana_hp TYPE character varying(100);
-   ALTER COLUMN europeana_license TYPE character varying(100);
-   ALTER COLUMN europeana_license_details TYPE character varying(100);
+﻿ALTER TABLE ingestionprofile ALTER COLUMN name_profile TYPE character varying(100);
+ALTER TABLE ingestionprofile ALTER COLUMN europeana_languages TYPE character varying(100);
+ALTER TABLE ingestionprofile ALTER COLUMN europeana_hp TYPE character varying(100);
+ALTER TABLE ingestionprofile ALTER COLUMN europeana_license TYPE character varying(100);
+ALTER TABLE ingestionprofile ALTER COLUMN europeana_license_details TYPE character varying(100);
