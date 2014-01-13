aa170c5ba6c385efd5be90890ccb24fcf0911fce
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-01-13 07:14:16 +0000

    IssueID 983 remove ingestion profile and oai-pmh harvesting profile automaticly.
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.5@2233 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/resources/dbScripts/2014-01-13-fix-on-cascade-bug.sql
+++ persistence/src/main/resources/dbScripts/2014-01-13-fix-on-cascade-bug.sql
@@ -1,1 +1,12 @@
-ALTER TABLE queue DROP CONSTRAINT index_queue_sg_id_fkey, ADD CONSTRAINT index_queue_sg_id_fkey FOREIGN KEY (sg_id) REFERENCES source_guide(id) ON DELETE CASCADE;
+ALTER TABLE archival_institution_oai_pmh DROP CONSTRAINT archival_institutition_oai_pmh_profile_id_fkey;
+ALTER TABLE archival_institution_oai_pmh DROP CONSTRAINT archival_institution_oai_pmh_ai_id_fkey;
+
+ALTER TABLE archival_institution_oai_pmh ADD CONSTRAINT archival_institution_oai_pmh_ai_id_fkey FOREIGN KEY (ai_id)
+      REFERENCES archival_institution (id) ON DELETE CASCADE;
+      
+ALTER TABLE archival_institution_oai_pmh ADD CONSTRAINT archival_institutition_oai_pmh_profile_id_fkey FOREIGN KEY (profile_id)
+      REFERENCES ingestionprofile (id);
+
+ALTER TABLE ingestionprofile DROP CONSTRAINT ingestionprofile_ai_id_fkey;      
+ALTER TABLE ingestionprofile ADD CONSTRAINT ingestionprofile_ai_id_fkey FOREIGN KEY (ai_id)
+      REFERENCES archival_institution (id) ON DELETE CASCADE;
