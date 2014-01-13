692601cc5898f5da0e8088cd6885d34255fc1f59
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-01-13 07:26:29 +0000

    IssueID 983 remove ingestion profile and oai-pmh harvesting profile automaticly.
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.5@2235 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/resources/dbScripts/2014-01-13-fix-on-cascade-bug.sql
+++ persistence/src/main/resources/dbScripts/2014-01-13-fix-on-cascade-bug.sql
@@ -10,3 +10,11 @@ ALTER TABLE archival_institution_oai_pmh ADD CONSTRAINT archival_institutition_o
 ALTER TABLE ingestionprofile DROP CONSTRAINT ingestionprofile_ai_id_fkey;      
 ALTER TABLE ingestionprofile ADD CONSTRAINT ingestionprofile_ai_id_fkey FOREIGN KEY (ai_id)
       REFERENCES archival_institution (id) ON DELETE CASCADE;
+      
+ALTER TABLE ai_alternative_name DROP CONSTRAINT archival_institution_ai_id_fkey;    
+ALTER TABLE ai_alternative_name ADD CONSTRAINT archival_institution_ai_id_fkey FOREIGN KEY (ai_id)
+      REFERENCES archival_institution (id) ON DELETE CASCADE;
+      
+ALTER TABLE archival_institution DROP CONSTRAINT archival_institution_parent_ai_id_fkey;         
+ALTER TABLE archival_institution ADD CONSTRAINT archival_institution_parent_ai_id_fkey FOREIGN KEY (parent_ai_id)
+      REFERENCES archival_institution (id) ON DELETE CASCADE;
