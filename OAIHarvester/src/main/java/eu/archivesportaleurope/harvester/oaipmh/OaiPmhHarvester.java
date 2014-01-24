136084aead1b067a433cae0f0f06faf3bd0c2ee9
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-01-24 06:46:17 +0000

    IssueID 1032: Add ListIdentifiers implementation
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.5@2345 9fd842fd-667d-44da-918d-ff91420b3ae1

--- OAIHarvester/src/main/java/eu/archivesportaleurope/harvester/oaipmh/OaiPmhHarvester.java
+++ OAIHarvester/src/main/java/eu/archivesportaleurope/harvester/oaipmh/OaiPmhHarvester.java
@@ -13,6 +13,7 @@ import eu.archivesportaleurope.harvester.oaipmh.parser.record.OaiPmhParser;
 import eu.archivesportaleurope.harvester.oaipmh.parser.record.OaiPmhRecord;
 import eu.archivesportaleurope.harvester.oaipmh.parser.record.ResultInfo;
 import eu.archivesportaleurope.harvester.util.OaiPmhHttpClient;
+import eu.archivesportaleurope.harvester.verb.ListIdentifiersVerb;
 import eu.archivesportaleurope.harvester.verb.ListRecordsSaxWriteDirectly;
 
 public class OaiPmhHarvester {
@@ -78,6 +79,69 @@ public class OaiPmhHarvester {
 		} catch (HarvesterParserException hpe) {
 			LOGGER.error("Unable to parse XML response from the OAI-PMH server, look at the XML response file at "
 					+ hpe.getNotParsebleResponse().getCanonicalPath());
+			throw hpe;
+		}
+	}
+	
+	public static void harvestByListIdentifiers(HarvestObject harvestObject, String baseURL, String from, String until, String metadataPrefix, String setSpec,
+			OaiPmhParser oaiPmhParser, File errorDir, OaiPmhHttpClient oaiPmhHttpClient) throws Exception {
+		try {
+			ListIdentifiersVerb listIdentifiersVerb = new ListIdentifiersVerb(oaiPmhHttpClient);
+			ResultInfo resultInfo = listIdentifiersVerb.harvest(baseURL, from, until, setSpec, metadataPrefix, oaiPmhParser,
+					errorDir, harvestObject.getNumberOfRequests());
+			boolean hasErrors = false;
+			while (resultInfo != null && !harvestObject.isStopHarvesting()) {
+				harvestObject.increaseNumberOfRequests();
+				List<String> errors = resultInfo.getErrors();
+				if (errors != null && errors.size() > 0) {
+					LOGGER.error("Found the following errors:");
+					harvestObject.addErrors("\nUrl that contains errors: '" + resultInfo.getRequestUrl() + "'\n");
+					int length = errors.size();
+					for (int i = 0; i < length; ++i) {
+						String item = errors.get(i);
+						if (StringUtils.isNotBlank(item)) {
+							harvestObject.addErrors(item);
+							LOGGER.error(item);
+						}
+
+					}
+					LOGGER.error("Url that contains errors: '" + resultInfo.getRequestUrl() + "'");
+					hasErrors = true;
+					break;
+				}
+				for (OaiPmhRecord record : resultInfo.getRecords()) {
+					String action = "retrieved";
+					if (record.isDeleted()){
+						action = "deleted";
+					}
+					harvestObject.increaseNumberOfRecords();
+					harvestObject.setLatestRecordId(record.getIdentifier());
+					LOGGER.info("("+harvestObject.getNumberOfRequests()+"," + harvestObject.getNumberOfRecords() +"): Record with ID: " + record.getIdentifier() + " "+ action +" ("
+							+ DATE_TIME_FORMAT.format(record.getTimestamp()) + ")");
+					harvestObject.add(record);							}
+				String resumptionToken = resultInfo.getNewResumptionToken();
+				LOGGER.debug("resumptionToken: '" + resumptionToken + "'");
+				if (StringUtils.isBlank(resumptionToken)) {
+					resultInfo = null;
+				} else {
+					resultInfo = listIdentifiersVerb.harvest(baseURL, resumptionToken, oaiPmhParser, errorDir, harvestObject.getNumberOfRequests());
+				}
+			}
+			String logSuffix = "";
+			if (harvestObject.isStopHarvesting()){
+				logSuffix = "Harvesting is stopped manually.";
+			}
+			if (hasErrors) {
+				LOGGER.error(harvestObject.getNumberOfRecords() + " records harvested, but with errors. " + logSuffix);
+			} else {
+				LOGGER.info(harvestObject.getNumberOfRecords() + " records harvested successfully with no errors. " + logSuffix);
+			}
+			if (harvestObject.isStopHarvesting()){
+				throw new HarvesterInterruptionException(resultInfo.getRequestUrl(), logSuffix);
+			}
+		} catch (HarvesterParserException hpe) {
+			LOGGER.error("Unable to parse XML response from the OAI-PMH server, look at the XML response file at "
+					+ hpe.getNotParsebleResponse().getCanonicalPath(),hpe);
 			throw hpe;
 		}
 	}
