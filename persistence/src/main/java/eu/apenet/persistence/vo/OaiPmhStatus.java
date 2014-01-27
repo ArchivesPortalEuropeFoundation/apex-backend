489a1905bcffae2d4abad71008fdf23d14ddda5a
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-01-27 13:08:18 +0000

    IssueID 1032: Add ListIdentifiers implementation
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.5@2378 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/java/eu/apenet/persistence/vo/OaiPmhStatus.java
+++ persistence/src/main/java/eu/apenet/persistence/vo/OaiPmhStatus.java
@@ -1,16 +1,23 @@
 package eu.apenet.persistence.vo;
 
 public enum OaiPmhStatus {
-	FAILED("label.harvesting.failed"), SUCCEED("label.harvesting.succeed"),SUCCEED_WITH_WARNINGS("label.harvesting.succeedwithwarnings")
-	,SUCCEED_WITH_ERRORS("label.harvesting.succeedwitherrors"),PROCESSING("label.harvesting.processing"),FIRST_TIME("label.harvesting.firsttime");
+	FAILED("label.harvesting.failed", "FAILED"), SUCCEED("label.harvesting.succeed", "SUCCEED"),SUCCEED_WITH_WARNINGS("label.harvesting.succeedwithwarnings","SUCCEED, WITH WARNINGS")
+	,SUCCEED_WITH_ERRORS("label.harvesting.succeedwitherrors", "SUCCEED, WITH ERRORS"),PROCESSING("label.harvesting.processing", "PROCESSING");
 
 	private String resourceName;
-	private OaiPmhStatus(String resourceName){
+	private String name;
+	private OaiPmhStatus(String resourceName, String name){
 		this.resourceName = resourceName;
+		this.name = name;
 	}
 
 	public String getResourceName() {
 		return resourceName;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	
 }
