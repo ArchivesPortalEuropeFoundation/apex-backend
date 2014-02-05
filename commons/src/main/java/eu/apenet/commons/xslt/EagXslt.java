e406d901230dcb11b988c07b1249b1b555442218
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-02-05 08:40:06 +0000

    better logging
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.5@2448 9fd842fd-667d-44da-918d-ff91420b3ae1

--- commons/src/main/java/eu/apenet/commons/xslt/EagXslt.java
+++ commons/src/main/java/eu/apenet/commons/xslt/EagXslt.java
@@ -1,6 +1,7 @@
 package eu.apenet.commons.xslt;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Writer;
 
 import javax.xml.transform.Source;
@@ -14,6 +15,9 @@ import net.sf.saxon.s9api.XdmAtomicValue;
 import net.sf.saxon.s9api.XsltCompiler;
 import net.sf.saxon.s9api.XsltExecutable;
 import net.sf.saxon.s9api.XsltTransformer;
+
+import org.apache.log4j.Logger;
+
 import eu.apenet.commons.ResourceBundleSource;
 import eu.apenet.commons.xslt.extensions.ResourcebundleExtension;
 import eu.apenet.commons.xslt.extensions.RetrieveRelatedAIIdExtension;
@@ -21,6 +25,7 @@ import eu.apenet.persistence.factory.DAOFactory;
 import eu.apenet.persistence.vo.Lang;
 
 public class EagXslt {
+	private static Logger LOGGER = Logger.getLogger(EagXslt.class);
 	private static XsltExecutable getXsltExecutable(String xslUrl, ResourceBundleSource resourceBundleSource, String requiredAIRepositorCode) throws SaxonApiException{
         ClassLoader classLoader = (ClassLoader) Thread.currentThread().getContextClassLoader();
         Source xsltSource = new StreamSource(classLoader.getResourceAsStream(xslUrl));	
@@ -33,7 +38,6 @@ public class EagXslt {
         
         return compiler.compile(xsltSource);
 	}
-	@Deprecated
     public static void displayAiDetails(Writer writer, File xmlFile, ResourceBundleSource resourceBundleSource, String currentAIRepositorCode, String requiredAIRepositorCode) throws SaxonApiException{
 		String language = resourceBundleSource.getLocale().getLanguage();
 		String languageIso3 = "eng";
@@ -44,15 +48,22 @@ public class EagXslt {
 		displayAiDetails(writer, xmlFile, resourceBundleSource, languageIso3, currentAIRepositorCode, requiredAIRepositorCode);
     }
     public static void displayAiDetails(Writer writer, File xmlFile, ResourceBundleSource resourceBundleSource, String language, String currentAIRepositorCode, String requiredAIRepositorCode) throws SaxonApiException{
-		Source xmlSource = new StreamSource(xmlFile);
-    	XsltExecutable executable = getXsltExecutable("xsl/eag/aidetails.xsl", resourceBundleSource, requiredAIRepositorCode);
-        XsltTransformer transformer = executable.load();
-        transformer.setParameter(new QName("language.selected"), new XdmAtomicValue(language));
-        transformer.setSource(xmlSource);
-        Serializer serializer = new Serializer();
-        serializer.setOutputWriter(writer);
-        transformer.setDestination(serializer);
-        transformer.transform();
+		if (xmlFile.exists()){
+	    	Source xmlSource = new StreamSource(xmlFile);
+	    	XsltExecutable executable = getXsltExecutable("xsl/eag/aidetails.xsl", resourceBundleSource, requiredAIRepositorCode);
+	        XsltTransformer transformer = executable.load();
+	        transformer.setParameter(new QName("language.selected"), new XdmAtomicValue(language));
+	        transformer.setSource(xmlSource);
+	        Serializer serializer = new Serializer();
+	        serializer.setOutputWriter(writer);
+	        transformer.setDestination(serializer);
+	        transformer.transform();
+		}else {
+			try {
+				LOGGER.error("No file: " + xmlFile.getCanonicalPath());
+			} catch (IOException e) {
+			}
+		}
     }
 
 	public static void convertAi(Writer writer, File eagPathFile, ResourceBundleSource resourceBundleSource) throws SaxonApiException {
