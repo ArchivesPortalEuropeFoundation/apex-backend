d97f15446577637a6ccdcb7a58892cc67613c62c
stefanpapp-ape <stefan.papp.ape@gmail.com>
2014-02-17 16:00:37 +0000

    [EacCpf] addition of relation columns to db schema
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@2520 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/java/eu/apenet/persistence/vo/EacCpf.java
+++ persistence/src/main/java/eu/apenet/persistence/vo/EacCpf.java
@@ -1,6 +1,5 @@
 package eu.apenet.persistence.vo;
 
-
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -15,141 +14,218 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
+import javax.persistence.Temporal;
 
 @Entity
 @Table(name = "eac_cpf")
 public class EacCpf extends AbstractContent {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 6760184769676870729L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "ai_id")
-	private ArchivalInstitution archivalInstitution;
-	@Column(name = "ai_id", updatable = false, insertable = false)
-	private Integer aiId;
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "um_id")
-	private UploadMethod uploadMethod;
-	private String title;
-	private String path;
-	private boolean published = false;
-	private boolean converted = false;
-	private ValidatedState validated = ValidatedState.NOT_VALIDATED;
-	private EuropeanaState europeana = EuropeanaState.NOT_CONVERTED;
-	private QueuingState queuing = QueuingState.NO;
-	@Column(name = "upload_date")
-	private Date uploadDate;
-	@Column(name = "publish_date")	
-	private Date publishDate;
-	@Column(name = "identifier", nullable = false)
-	private String identifier;
-	
-	@OneToMany(mappedBy = "eacCpf")
-	private Set<QueueItem> queueItems = new HashSet<QueueItem>(0);
-	
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public ArchivalInstitution getArchivalInstitution() {
-		return archivalInstitution;
-	}
-	public void setArchivalInstitution(ArchivalInstitution archivalInstitution) {
-		this.archivalInstitution = archivalInstitution;
-	}
-	public Integer getAiId() {
-		return aiId;
-	}
-	public void setAiId(Integer aiId) {
-		this.aiId = aiId;
-	}
-	public UploadMethod getUploadMethod() {
-		return uploadMethod;
-	}
-	public void setUploadMethod(UploadMethod uploadMethod) {
-		this.uploadMethod = uploadMethod;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getPath() {
-		return path;
-	}
-	public void setPath(String path) {
-		this.path = path;
-	}
-	public boolean isPublished() {
-		return published;
-	}
-	public void setPublished(boolean published) {
-		this.published = published;
-	}
-	public boolean isConverted() {
-		return converted;
-	}
-	public void setConverted(boolean converted) {
-		this.converted = converted;
-	}
-	public ValidatedState getValidated() {
-		return validated;
-	}
-	public void setValidated(ValidatedState validated) {
-		this.validated = validated;
-	}
-	public EuropeanaState getEuropeana() {
-		return europeana;
-	}
-	public void setEuropeana(EuropeanaState europeana) {
-		this.europeana = europeana;
-	}
-	public QueuingState getQueuing() {
-		return queuing;
-	}
-	public void setQueuing(QueuingState queuing) {
-		this.queuing = queuing;
-	}
-	public Date getUploadDate() {
-		return uploadDate;
-	}
-	public void setUploadDate(Date uploadDate) {
-		this.uploadDate = uploadDate;
-	}
-	public Date getPublishDate() {
-		return publishDate;
-	}
-	public void setPublishDate(Date publishDate) {
-		this.publishDate = publishDate;
-	}
-	public String getIdentifier() {
-		return identifier;
-	}
-	public void setIdentifier(String identifier) {
-		this.identifier = identifier;
-	}
-	public Set<QueueItem> getQueueItems() {
-		return queueItems;
-	}
-	public void setQueueItems(Set<QueueItem> queueItems) {
-		this.queueItems = queueItems;
-	}
+    /**
+     *
+     */
+    private static final long serialVersionUID = 6760184769676870729L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ai_id")
+    private ArchivalInstitution archivalInstitution;
+    @Column(name = "ai_id", updatable = false, insertable = false)
+    private Integer aiId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "um_id")
+    private UploadMethod uploadMethod;
+    private String title;
+    private String path;
+    private boolean published = false;
+    private boolean converted = false;
+    private ValidatedState validated = ValidatedState.NOT_VALIDATED;
+    private EuropeanaState europeana = EuropeanaState.NOT_CONVERTED;
+    private long cpfRelations = 0l;
+    private long resourceRelations = 0l;
+    private long functionRelations = 0l;
+    private QueuingState queuing = QueuingState.NO;
+    @Column(name = "upload_date")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date uploadDate;
+    @Column(name = "publish_date")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date publishDate;
+    @Column(name = "identifier", nullable = false)
+    private String identifier;
 
+    @OneToMany(mappedBy = "eacCpf")
+    private Set<QueueItem> queueItems = new HashSet<QueueItem>(0);
 
+    @Override
+    public Integer getId() {
+        return id;
+    }
 
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
+    @Override
+    public ArchivalInstitution getArchivalInstitution() {
+        return archivalInstitution;
+    }
 
+    @Override
+    public void setArchivalInstitution(ArchivalInstitution archivalInstitution) {
+        this.archivalInstitution = archivalInstitution;
+    }
 
-	
+    @Override
+    public Integer getAiId() {
+        return aiId;
+    }
 
+    @Override
+    public void setAiId(Integer aiId) {
+        this.aiId = aiId;
+    }
 
+    @Override
+    public UploadMethod getUploadMethod() {
+        return uploadMethod;
+    }
+
+    @Override
+    public void setUploadMethod(UploadMethod uploadMethod) {
+        this.uploadMethod = uploadMethod;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String getPath() {
+        return path;
+    }
+
+    @Override
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public boolean isPublished() {
+        return published;
+    }
+
+    @Override
+    public void setPublished(boolean published) {
+        this.published = published;
+    }
+
+    @Override
+    public boolean isConverted() {
+        return converted;
+    }
+
+    @Override
+    public void setConverted(boolean converted) {
+        this.converted = converted;
+    }
+
+    @Override
+    public ValidatedState getValidated() {
+        return validated;
+    }
+
+    @Override
+    public void setValidated(ValidatedState validated) {
+        this.validated = validated;
+    }
+
+    public long getCpfRelations() {
+        return cpfRelations;
+    }
+
+    public void setCpfRelations(long cpfRelations) {
+        this.cpfRelations = cpfRelations;
+    }
+
+    public long getResourceRelations() {
+        return resourceRelations;
+    }
+
+    public void setResourceRelations(long resourceRelations) {
+        this.resourceRelations = resourceRelations;
+    }
+
+    public long getFunctionRelations() {
+        return functionRelations;
+    }
+
+    public void setFunctionRelations(long functionRelations) {
+        this.functionRelations = functionRelations;
+    }
+
+    public EuropeanaState getEuropeana() {
+        return europeana;
+    }
+
+    public void setEuropeana(EuropeanaState europeana) {
+        this.europeana = europeana;
+    }
+
+    @Override
+    public QueuingState getQueuing() {
+        return queuing;
+    }
+
+    @Override
+    public void setQueuing(QueuingState queuing) {
+        this.queuing = queuing;
+    }
+
+    @Override
+    public Date getUploadDate() {
+        return uploadDate;
+    }
+
+    @Override
+    public void setUploadDate(Date uploadDate) {
+        this.uploadDate = uploadDate;
+    }
+
+    @Override
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    @Override
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    @Override
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    @Override
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    @Override
+    public Set<QueueItem> getQueueItems() {
+        return queueItems;
+    }
+
+    @Override
+    public void setQueueItems(Set<QueueItem> queueItems) {
+        this.queueItems = queueItems;
+    }
 }
