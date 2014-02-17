794e3ebcd56eb81506a35b9ce44f5420ab1d5aea
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-02-17 08:19:17 +0000

    EAC-CPF data model
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@2510 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ persistence/src/main/resources/dbScripts/2014-02-17-add-eac_cpf.sql
@@ -0,0 +1,41 @@
+-- CPF_content table.
+DROP TABLE cpf_content;
+-- EAC-CPF table.
+
+CREATE TABLE eac_cpf (
+    id serial NOT NULL,
+    title text,
+    upload_date timestamp without time zone NOT NULL,
+    path character varying(255) NOT NULL,
+    um_id integer NOT NULL,
+    ai_id integer NOT NULL,
+    published boolean DEFAULT false,
+    converted boolean DEFAULT false,
+    validated smallint DEFAULT 1,
+    europeana smallint DEFAULT 0,
+    queuing smallint DEFAULT 0,
+    publish_date timestamp without time zone,
+    identifier character varying(255) NOT NULL,
+    CONSTRAINT eac_cpf_pkey PRIMARY KEY (id),
+    CONSTRAINT eac_cpf_ai_id_fkey FOREIGN KEY (ai_id)
+        REFERENCES archival_institution (id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT eac_cpf_um_id_fkey FOREIGN KEY (um_id)
+        REFERENCES upload_method (id) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+REVOKE ALL ON TABLE eac_cpf FROM PUBLIC;
+REVOKE ALL ON TABLE eac_cpf FROM postgres;
+GRANT ALL ON TABLE eac_cpf TO postgres;
+GRANT ALL ON TABLE eac_cpf TO admin;
+GRANT ALL ON TABLE eac_cpf TO apenet_dashboard;
+GRANT SELECT ON TABLE eac_cpf TO apenet_portal;
+
+
+-- Queue table.
+ALTER TABLE queue ADD COLUMN eac_cpf_id integer;
+ALTER TABLE queue
+  ADD CONSTRAINT index_queue_eac_id_fkey FOREIGN KEY (eac_cpf_id)
+      REFERENCES eac_cpf (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE;
