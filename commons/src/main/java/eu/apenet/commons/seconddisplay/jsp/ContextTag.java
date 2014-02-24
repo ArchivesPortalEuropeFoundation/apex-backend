f0c76bce451b9e157bc0f3d8262c76312f7a9ca1
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-02-24 07:24:18 +0000

    update logging
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.5.0@2581 9fd842fd-667d-44da-918d-ff91420b3ae1

--- commons/src/main/java/eu/apenet/commons/seconddisplay/jsp/ContextTag.java
+++ commons/src/main/java/eu/apenet/commons/seconddisplay/jsp/ContextTag.java
@@ -1,64 +1,69 @@
 package eu.apenet.commons.seconddisplay.jsp;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
+import org.apache.log4j.Logger;
+
+import eu.apenet.commons.utils.APEnetUtilities;
 import eu.apenet.persistence.vo.ArchivalInstitution;
 import eu.apenet.persistence.vo.CLevel;
 import eu.apenet.persistence.vo.EadContent;
 
 public class ContextTag extends SimpleTagSupport{
+	private final static Logger LOGGER = Logger.getLogger(ContextTag.class);
 	private Object clevel;
 	private Object eadContent;
 	private String onlyArchives;
 	private Object country;
 
-	public void doTag() throws JspException, IOException {
-		List<String> hierarchy = new ArrayList<String>();
-		EadContent eadContent = null;
-		StringBuilder result = new StringBuilder();
-		if (clevel != null){
-			CLevel currentCLevel = (CLevel) clevel;
-			CLevel parent = currentCLevel.getParent();
-			
-			while (parent != null ){
-				hierarchy.add(parent.getUnittitle());
-				parent = parent.getParent();
-			}
-			eadContent = currentCLevel.getEadContent();
-			hierarchy.add(eadContent.getUnittitle());
-		}else if (this.eadContent != null){
-			eadContent = (EadContent) this.eadContent;
-		}
-		
-		if (!"true".equals(onlyArchives)){
-			hierarchy.add(eadContent.getTitleproper());
-		}
-		if (eadContent != null){
-			ArchivalInstitution ai  = eadContent.getEad().getArchivalInstitution();
-			while (ai != null){
-				hierarchy.add(ai.getAiname());
-				ai = ai.getParent();
-			}
-			hierarchy.add((String) country);
-			int numberOfWhitespaces = 0;
-			for (int i = hierarchy.size() -1; i >=0;i--){
-				//result.append("<span class=\"contextHierarchyItem\">");
-				for (int j = 0; j < numberOfWhitespaces;j++){
-					result.append("&nbsp;&nbsp;&nbsp;");
+	public void doTag() {
+		try {
+			List<String> hierarchy = new ArrayList<String>();
+			EadContent eadContent = null;
+			StringBuilder result = new StringBuilder();
+			if (clevel != null){
+				CLevel currentCLevel = (CLevel) clevel;
+				CLevel parent = currentCLevel.getParent();
+				
+				while (parent != null ){
+					hierarchy.add(parent.getUnittitle());
+					parent = parent.getParent();
 				}
-				result.append(hierarchy.get(i));
-				//result.append("</span>");
-				result.append("<br/>");
-				numberOfWhitespaces++;
+				eadContent = currentCLevel.getEadContent();
+				hierarchy.add(eadContent.getUnittitle());
+			}else if (this.eadContent != null){
+				eadContent = (EadContent) this.eadContent;
 			}
+			
+			if (!"true".equals(onlyArchives)){
+				hierarchy.add(eadContent.getTitleproper());
+			}
+			if (eadContent != null){
+				ArchivalInstitution ai  = eadContent.getEad().getArchivalInstitution();
+				while (ai != null){
+					hierarchy.add(ai.getAiname());
+					ai = ai.getParent();
+				}
+				hierarchy.add((String) country);
+				int numberOfWhitespaces = 0;
+				for (int i = hierarchy.size() -1; i >=0;i--){
+					//result.append("<span class=\"contextHierarchyItem\">");
+					for (int j = 0; j < numberOfWhitespaces;j++){
+						result.append("&nbsp;&nbsp;&nbsp;");
+					}
+					result.append(hierarchy.get(i));
+					//result.append("</span>");
+					result.append("<br/>");
+					numberOfWhitespaces++;
+				}
+			}
+			this.getJspContext().getOut().print(result);
+		}catch (Exception e){
+			LOGGER.error(APEnetUtilities.generateThrowableLog(e));
 		}
-		this.getJspContext().getOut().print(result);
-
 	}
 
 
