97522f37b6e1a399c3c7aac33ba71415a23ac70c
stefanpapp-ape <stefan.papp.ape@gmail.com>
2014-03-19 13:25:11 +0000

    [persistence] add missing sequence for eac_cpf table
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@2654 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ persistence/src/main/resources/dbScripts/2014-03-19-add_eac_cpf_sequence_id.sql
@@ -0,0 +1,18 @@
+﻿-- Sequence: eac_cpf_id_seq
+
+-- DROP SEQUENCE eac_cpf_id_seq;
+
+CREATE SEQUENCE eac_cpf_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE eac_cpf_id_seq
+  OWNER TO postgres;
+GRANT ALL ON TABLE eac_cpf_id_seq TO postgres;
+GRANT ALL ON TABLE eac_cpf_id_seq TO admin;
+GRANT ALL ON TABLE eac_cpf_id_seq TO apenet_dashboard;
+
+ALTER TABLE eac_cpf
+   ALTER COLUMN id SET DEFAULT nextval('eac_cpf_id_seq'::regclass);
