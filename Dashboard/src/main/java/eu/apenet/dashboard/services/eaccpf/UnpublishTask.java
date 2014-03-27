998b14d4c1dd32f283ace2a657420b9c7946acc1
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-03-27 14:17:18 +0000

    IssueID 765: Publish EAC-CPF
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@2689 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/services/eaccpf/UnpublishTask.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/services/eaccpf/UnpublishTask.java
@@ -5,20 +5,25 @@
  */
 package eu.apenet.dashboard.services.eaccpf;
 
-import eu.apenet.persistence.vo.EacCpf;
+import java.io.IOException;
 import java.util.Properties;
+
+import org.apache.solr.client.solrj.SolrServerException;
+
+import eu.apenet.commons.exceptions.APEnetException;
+import eu.apenet.commons.solr.SolrFields;
+import eu.apenet.commons.solr.UpdateSolrServerHolder;
+import eu.apenet.dashboard.utils.ContentUtils;
+import eu.apenet.persistence.dao.EacCpfDAO;
+import eu.apenet.persistence.factory.DAOFactory;
+import eu.apenet.persistence.vo.EacCpf;
+import eu.archivesportaleurope.persistence.jpa.JpaUtil;
 
 /**
  *
  * @author papp
  */
 class UnpublishTask extends AbstractEacCpfTask {
-
-    @Override
-    protected void execute(EacCpf eacCpf, Properties properties) throws Exception {
-        //TODO: Add content
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
     @Override
     protected String getActionName() {
@@ -28,4 +33,29 @@ class UnpublishTask extends AbstractEacCpfTask {
     static boolean valid(EacCpf eacCpf) {
         return eacCpf.isPublished();
     }
+    
+	@Override
+	public void execute(EacCpf eacCpf, Properties properties) throws Exception {
+		if (valid(eacCpf)) {
+			try {
+				long startTime = System.currentTimeMillis();
+				EacCpfDAO eacCpfDAO = DAOFactory.instance().getEacCpfDAO();
+				logger.debug("Removing the EacCpf with id '" + eacCpf.getIdentifier()+ "' from the index");
+				long solrTime = deleteFromSolr(eacCpf.getId(), eacCpf.getAiId());
+				JpaUtil.beginDatabaseTransaction();
+				ContentUtils.changeSearchable(eacCpf, false);
+				eacCpfDAO.insertSimple(eacCpf);
+				JpaUtil.commitDatabaseTransaction();
+				logSolrAction(eacCpf, "", solrTime, System.currentTimeMillis()-(startTime+solrTime));
+			} catch (Exception e) {
+				logAction(eacCpf, e);
+				throw new APEnetException(this.getActionName() + " " + e.getMessage(), e);
+			}
+		}
+	}
+
+	private static long deleteFromSolr(Integer id, int aiId) throws SolrServerException, IOException {
+		UpdateSolrServerHolder server = UpdateSolrServerHolder.getInstance();
+		return server.deleteByQuery("(" + SolrFields.AI_ID + ":" + aiId + " AND " + SolrFields.ID + ":\"" + id + "\")");
+	}
 }
