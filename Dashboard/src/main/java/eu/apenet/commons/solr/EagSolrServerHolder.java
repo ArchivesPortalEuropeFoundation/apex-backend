6dc9ac0cf7f48ee615109e684fe8197d3c872dec
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-04-23 11:59:46 +0000

    IssueID 669: Implement EAG search
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@2862 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ Dashboard/src/main/java/eu/apenet/commons/solr/EagSolrServerHolder.java
@@ -0,0 +1,22 @@
+package eu.apenet.commons.solr;
+
+import eu.apenet.commons.utils.APEnetUtilities;
+
+public class EagSolrServerHolder extends AbstractSolrServerHolder {
+	private static EagSolrServerHolder instance;
+	private EagSolrServerHolder() {
+
+	}
+
+	public static EagSolrServerHolder getInstance() {
+		if (instance == null) {
+			instance = new EagSolrServerHolder();
+		}
+		return instance;
+	}
+	@Override
+	protected String getSolrUrl() {
+		return APEnetUtilities.getDashboardConfig().getBaseSolrIndexUrl() + "/eags";
+	}
+
+}
