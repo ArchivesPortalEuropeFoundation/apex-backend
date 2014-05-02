84a7a89b25bd946df5be4aa528b70d6d03c40829
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-05-02 08:40:52 +0000

    IssueID 1176: Google maps
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.5@2929 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/resources/dbScripts/2014-05-02-new-coordinates.sql
+++ persistence/src/main/resources/dbScripts/2014-05-02-new-coordinates.sql
@@ -1,4 +1,3 @@
 ALTER TABLE coordinates ADD CONSTRAINT coordinates_pkey PRIMARY KEY (id);
 ALTER INDEX partner_pkey RENAME TO dashboard_user_pkey;
 ALTER INDEX index_queue_pkey RENAME TO queue_pkey;
-ALTER TABLE queue ADD CONSTRAINT queue_pkey PRIMARY KEY (id);
