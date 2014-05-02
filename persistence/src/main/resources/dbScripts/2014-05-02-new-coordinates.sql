79a1453cf0253ae2c5e893bb1e1738dc7a8ed549
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-05-02 06:31:24 +0000

    IssueID 1176: Google maps
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.5@2921 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ persistence/src/main/resources/dbScripts/2014-05-02-new-coordinates.sql
@@ -0,0 +1,27 @@
+CREATE TABLE coordinates (
+    id serial NOT NULL,
+    ai_id integer NOT NULL,
+    name_institution character varying NOT NULL,
+    street character varying(255) NOT NULL,
+    postalcity character varying(255) NOT NULL,
+    country character varying(255) NOT NULL,
+    lat numeric,
+    lon numeric,
+      CONSTRAINT coordinates_pkey PRIMARY KEY (id),
+      CONSTRAINT coordinates_archival_institution_fkey FOREIGN KEY (ai_id)
+      REFERENCES archival_institution (id) ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+REVOKE ALL ON TABLE coordinates FROM PUBLIC;
+REVOKE ALL ON TABLE coordinates FROM postgres;
+GRANT ALL ON TABLE coordinates TO postgres;
+GRANT ALL ON TABLE coordinates TO admin;
+GRANT ALL ON TABLE coordinates TO apenet_dashboard;
+GRANT SELECT ON TABLE coordinates TO apenet_portal;
+
+REVOKE ALL ON SEQUENCE coordinates_id_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE coordinates_id_seq FROM postgres;
+GRANT ALL ON SEQUENCE coordinates_id_seq TO postgres;
+GRANT ALL ON SEQUENCE coordinates_id_seq TO apenet_dashboard;
+GRANT SELECT ON SEQUENCE coordinates_id_seq TO apenet_portal;
+GRANT ALL ON SEQUENCE coordinates_id_seq TO admin;
