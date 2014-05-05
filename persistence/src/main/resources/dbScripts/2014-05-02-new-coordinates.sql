e777734081e85b165474773977f31f87b0005045
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-05-05 07:49:12 +0000

    MERGE FROM BRANCH 1.5 (5-5-2014)
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@2933 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ persistence/src/main/resources/dbScripts/2014-05-02-new-coordinates.sql
@@ -0,0 +1,3 @@
+ALTER TABLE coordinates ADD CONSTRAINT coordinates_pkey PRIMARY KEY (id);
+ALTER INDEX partner_pkey RENAME TO dashboard_user_pkey;
+ALTER INDEX index_queue_pkey RENAME TO queue_pkey;
