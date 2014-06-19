0764a305859b340a427bab16b7fce534fb1b73e5
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-06-19 11:58:42 +0000

    IssueID 669: EAG search
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@3258 9fd842fd-667d-44da-918d-ff91420b3ae1

--- commons/src/main/java/eu/apenet/commons/solr/SolrValues.java
+++ commons/src/main/java/eu/apenet/commons/solr/SolrValues.java
@@ -3,7 +3,7 @@ package eu.apenet.commons.solr;
 import org.apache.commons.lang.StringUtils;
 
 public final class SolrValues {
-	public static final String ESCAPING_CHARACTER= "\\\\";
+	public static final String ESCAPING_CHARACTER = "\\\\";
 	public final static String HG_PREFIX = "H";
 	public final static String FA_PREFIX = "F";
 	public final static String SG_PREFIX = "S";
@@ -13,7 +13,7 @@ public final class SolrValues {
 	public final static String FA_TYPE = "fa";
 	public final static String SG_TYPE = "sg";
 	@Deprecated
-	public final static String GROUP_SUFFIX= "G";
+	public final static String GROUP_SUFFIX = "G";
 	public final static String TYPE_LEAF = "L";
 	public final static String TYPE_GROUP = "G";
 	public final static String DATE_TYPE_NO_DATE_SPECIFIED = "nodate";
@@ -23,27 +23,43 @@ public final class SolrValues {
 	public final static String DATE_TYPE_NORMALIZED_NO_ENDDATE = "noenddate";
 	public static final String LEVEL_CLEVEL = "clevel";
 	public static final String LEVEL_ARCHDESC = "archdesc";
-		
+
 	public final static String EAC_CPF_FACET_ENTITY_TYPE_FAMILY = "family";
 	public final static String EAC_CPF_FACET_ENTITY_TYPE_PERSON = "person";
 	public final static String EAC_CPF_FACET_ENTITY_TYPE_CORPORATE_BODY = "corporatebody";
+	public final static String EAG_REPOSITORY_TYPE_BUSINESS_ARCHIVES = "businessArchives";
+	public final static String EAG_REPOSITORY_TYPE_CHURCH_ARCHIVES = "churchArchives";
+	public final static String EAG_REPOSITORY_TYPE_COUNTY_ARCHIVES = "countyArchives";
+	public final static String EAG_REPOSITORY_TYPE_CULTURAL_ARCHIVES = "culturalArchives";
+	public final static String EAG_REPOSITORY_TYPE_MEDIA_ARCHIVES = "mediaArchives";
+	public final static String EAG_REPOSITORY_TYPE_MUNICIPAL_ARCHIVES = "municipalArchives";
+	public final static String EAG_REPOSITORY_TYPE_NATIONAL_ARCHIVES = "nationalArchives";
+	public final static String EAG_REPOSITORY_TYPE_POLITICAL_ARCHIVES = "politicalArchives";
+	public final static String EAG_REPOSITORY_TYPE_PRIVATE_ARCHIVES = "privateArchives";
+	public final static String EAG_REPOSITORY_TYPE_REGIONAL_ARCHIVES = "regionalArchives";
+	public final static String EAG_REPOSITORY_TYPE_SPECIALISED_ARCHIVES = "specialisedArchives";
+	public final static String EAG_REPOSITORY_TYPE_UNIVERSITY_ARCHIVES = "universityArchives";
+	public final static String[] EAG_REPOSITORY_TYPES = new String[] {EAG_REPOSITORY_TYPE_BUSINESS_ARCHIVES, EAG_REPOSITORY_TYPE_CHURCH_ARCHIVES, 
+		EAG_REPOSITORY_TYPE_COUNTY_ARCHIVES, EAG_REPOSITORY_TYPE_CULTURAL_ARCHIVES, EAG_REPOSITORY_TYPE_CULTURAL_ARCHIVES,EAG_REPOSITORY_TYPE_MEDIA_ARCHIVES,
+		EAG_REPOSITORY_TYPE_MUNICIPAL_ARCHIVES, EAG_REPOSITORY_TYPE_NATIONAL_ARCHIVES, EAG_REPOSITORY_TYPE_POLITICAL_ARCHIVES, EAG_REPOSITORY_TYPE_PRIVATE_ARCHIVES, EAG_REPOSITORY_TYPE_REGIONAL_ARCHIVES,
+		EAG_REPOSITORY_TYPE_SPECIALISED_ARCHIVES,EAG_REPOSITORY_TYPE_UNIVERSITY_ARCHIVES };
 	
-		public static String escapeSolrCharacters(String value){
-		if (StringUtils.isNotBlank(value)){
+	public static String escapeSolrCharacters(String value) {
+		if (StringUtils.isNotBlank(value)) {
 			value = value.replaceAll(ESCAPING_CHARACTER, ESCAPING_CHARACTER + ESCAPING_CHARACTER);
 			value = value.replaceAll("-", ESCAPING_CHARACTER + "-");
 			value = value.replaceAll("\\+", ESCAPING_CHARACTER + "+");
-			value = value.replaceAll("&&", ESCAPING_CHARACTER + "&&");	
-			value = value.replaceAll("\\|\\|", ESCAPING_CHARACTER + "||");		
+			value = value.replaceAll("&&", ESCAPING_CHARACTER + "&&");
+			value = value.replaceAll("\\|\\|", ESCAPING_CHARACTER + "||");
 			value = value.replaceAll("!", ESCAPING_CHARACTER + "!");
-			value = value.replaceAll("\\(", ESCAPING_CHARACTER + "(");	
+			value = value.replaceAll("\\(", ESCAPING_CHARACTER + "(");
 			value = value.replaceAll("\\)", ESCAPING_CHARACTER + ")");
-			value = value.replaceAll("\\{", ESCAPING_CHARACTER + "{");	
-			value = value.replaceAll("\\}", ESCAPING_CHARACTER + "}");	
-			value = value.replaceAll("\\[", ESCAPING_CHARACTER + "[");	
-			value = value.replaceAll("\\]", ESCAPING_CHARACTER + "]");	
-			value = value.replaceAll("\\^", ESCAPING_CHARACTER + "^");	
-			value = value.replaceAll("\"", ESCAPING_CHARACTER + "\"");	
+			value = value.replaceAll("\\{", ESCAPING_CHARACTER + "{");
+			value = value.replaceAll("\\}", ESCAPING_CHARACTER + "}");
+			value = value.replaceAll("\\[", ESCAPING_CHARACTER + "[");
+			value = value.replaceAll("\\]", ESCAPING_CHARACTER + "]");
+			value = value.replaceAll("\\^", ESCAPING_CHARACTER + "^");
+			value = value.replaceAll("\"", ESCAPING_CHARACTER + "\"");
 			value = value.replaceAll("~", ESCAPING_CHARACTER + "~");
 			value = value.replaceAll("\\*", ESCAPING_CHARACTER + "*");
 			value = value.replaceAll("\\?", ESCAPING_CHARACTER + "?");
@@ -52,4 +68,5 @@ public final class SolrValues {
 		}
 		return value;
 	}
+
 }
