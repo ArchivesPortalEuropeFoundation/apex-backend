82204a426785a41b4d3d62f092c3efbc9e6ffd43
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-06-19 09:58:33 +0000

    IssueID 669: EAG search
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@3253 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/services/eag/EagService.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/services/eag/EagService.java
@@ -1,16 +1,28 @@
 package eu.apenet.dashboard.services.eag;
 
-import org.apache.log4j.Logger;
+import java.io.IOException;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.apache.solr.client.solrj.SolrServerException;
+
+import eu.apenet.dashboard.services.eag.xml.XmlEagParser;
+import eu.apenet.dashboard.services.eag.xml.stream.publish.EagSolrPublisher;
 import eu.apenet.persistence.vo.ArchivalInstitution;
 
 public class EagService {
 	private static final Logger LOGGER = Logger.getLogger(EagService.class);
 	
-	public static void unpublish(ArchivalInstitution archivalInstitution){
-		LOGGER.info("unpublish: " + archivalInstitution.getAiname() + " " + archivalInstitution.isGroup() + " " + archivalInstitution.getEagPath());
+	public static void unpublish(ArchivalInstitution archivalInstitution) throws SolrServerException, IOException{
+		if (!archivalInstitution.isGroup() && StringUtils.isNotBlank(archivalInstitution.getEagPath())){
+			new EagSolrPublisher().unpublish(archivalInstitution);
+			LOGGER.info("unpublish: " + archivalInstitution.getAiname());
+		}
 	}
-	public static void publish(ArchivalInstitution archivalInstitution){
-		LOGGER.info("publish: " + archivalInstitution.getAiname() + " " + archivalInstitution.isGroup() + " "+ archivalInstitution.getEagPath());
+	public static void publish(ArchivalInstitution archivalInstitution) throws Exception{
+		if (!archivalInstitution.isGroup() && StringUtils.isNotBlank(archivalInstitution.getEagPath())){
+			XmlEagParser.parseAndPublish(archivalInstitution);
+			LOGGER.info("publish: " + archivalInstitution.getAiname());
+		}
 	}
 }
