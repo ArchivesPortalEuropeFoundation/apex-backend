d66d2f09f8ce488079ab2dd3cb7ca29b1dbccff2
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-07-14 13:01:16 +0000

    IssueID 1330: nameEntry
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.6@3448 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/archivesportaleurope/xml/xpath/StringXpathHandler.java
+++ Dashboard/src/main/java/eu/archivesportaleurope/xml/xpath/StringXpathHandler.java
@@ -5,8 +5,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.apache.commons.lang.StringUtils;
-
 public abstract class StringXpathHandler extends AbstractXpathHandler {
 
 	private List<String> result = new ArrayList<String>();
@@ -28,11 +26,11 @@ public abstract class StringXpathHandler extends AbstractXpathHandler {
 	}
 	
 	public String getResultAsString(){
-		return convertToString(0, NO_SEPARATOR);	
+		return convertToString(result, 0, NO_SEPARATOR);	
 	}
 
 	public String getResultAsStringWithWhitespace(){
-		return convertToString(0, WHITE_SPACE);	
+		return convertToString(result,0, WHITE_SPACE);	
 	}
 	public String getFirstResult(){
 		if (result.size() > 0){
@@ -42,25 +40,9 @@ public abstract class StringXpathHandler extends AbstractXpathHandler {
 	}
 	
 	public String getOtherResultsAsStringWithWhitespace(){
-		return convertToString(1, WHITE_SPACE);	
+		return convertToString(result,1, WHITE_SPACE);	
 	}
-	private String convertToString(int start, String separator){
-		if (result.size() == 0){
-			return null;
-		}
-		StringBuilder builder = new StringBuilder();
-		for (int i = start; i < result.size(); i++){
-			builder.append(result.get(i) + separator);
-		}
-		return convertEmptyStringToNull(builder.toString());
-	}
-	private String convertEmptyStringToNull(String string){
-		if (StringUtils.isBlank(string)){
-			return null;
-		}else {
-			return string.trim();
-		}
-	}
+
 	@Override
 	protected void clear() {
 		result.clear();
