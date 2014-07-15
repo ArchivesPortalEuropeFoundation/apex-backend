4bc608e7a1827524838ecaac0d5e43dd5cdf0e5e
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-07-15 05:37:06 +0000

    IssueID 1329: maintenance tasks
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.6@3458 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/listener/MaintenanceTask.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/listener/MaintenanceTask.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 import eu.apenet.commons.utils.APEnetUtilities;
@@ -21,9 +22,8 @@ import eu.archivesportaleurope.persistence.jpa.JpaUtil;
 public class MaintenanceTask extends Thread {
 	private static final int MAX_AMOUNT = 1000;
 	private static final Logger LOGGER = Logger.getLogger(MaintenanceTask.class);
-	private static final String REPUBLISH_ALL = "republish-all";
-	private static final String REPUBLISH_HG_SG = "republish-hg-sg";
-	private static final String DELETE_ALL = "delete-all";
+	private static final String REPUBLISH = "republish-";
+	private static final String DELETE = "delete-";
 
 	public MaintenanceTask() {
 		JpaUtil.init();
@@ -36,64 +36,49 @@ public class MaintenanceTask extends Thread {
 		try {
 			ContentSearchOptions contentSearchOptions = new ContentSearchOptions();
 			contentSearchOptions.setPageSize(MAX_AMOUNT);
-			if (REPUBLISH_ALL.equals(APEnetUtilities.getDashboardConfig().getMaintenanceAction())) {
-				LOGGER.info("Execute command: " + REPUBLISH_ALL);
-				contentSearchOptions.setPublished(true);
-				List<QueuingState> queuingStates = new ArrayList<QueuingState>();
-				queuingStates.add(QueuingState.NO);
-				queuingStates.add(QueuingState.ERROR);
-				contentSearchOptions.setQueuing(queuingStates);
-				try {
-					contentSearchOptions.setContentClass(HoldingsGuide.class);
-					EadService.updateEverything(contentSearchOptions, QueueAction.REPUBLISH);
-					contentSearchOptions.setContentClass(SourceGuide.class);
-					EadService.updateEverything(contentSearchOptions, QueueAction.REPUBLISH);
-					contentSearchOptions.setContentClass(FindingAid.class);
-					EadService.updateEverything(contentSearchOptions, QueueAction.REPUBLISH);
-					contentSearchOptions.setContentClass(EacCpf.class);
-					EacCpfService.updateEverything(contentSearchOptions, QueueAction.REPUBLISH);	
-				} catch (IOException e) {
-					LOGGER.error("unexpected error occurs: " + e.getMessage(), e);
-				}
-
-			} else if (REPUBLISH_HG_SG.equals(APEnetUtilities.getDashboardConfig().getMaintenanceAction())) {
-				LOGGER.info("Execute command: " + REPUBLISH_HG_SG);
-				contentSearchOptions.setPublished(true);
-				List<QueuingState> queuingStates = new ArrayList<QueuingState>();
-				queuingStates.add(QueuingState.NO);
-				queuingStates.add(QueuingState.ERROR);
-				contentSearchOptions.setQueuing(queuingStates);
-				try {
-					contentSearchOptions.setContentClass(HoldingsGuide.class);
-					EadService.updateEverything(contentSearchOptions, QueueAction.REPUBLISH);
-					contentSearchOptions.setContentClass(SourceGuide.class);
-					EadService.updateEverything(contentSearchOptions, QueueAction.REPUBLISH);
-				} catch (IOException e) {
-					LOGGER.error("unexpected error occurs: " + e.getMessage(), e);
-				}
-
-			} else if (DELETE_ALL.equals(APEnetUtilities.getDashboardConfig().getMaintenanceAction())) {
-				LOGGER.info("Start to add all files to the queue to delete them");
-				List<QueuingState> queuingStates = new ArrayList<QueuingState>();
-				queuingStates.add(QueuingState.NO);
-				queuingStates.add(QueuingState.ERROR);
-				contentSearchOptions.setQueuing(queuingStates);
-				try {
-					contentSearchOptions.setContentClass(HoldingsGuide.class);
-					EadService.updateEverything(contentSearchOptions, QueueAction.DELETE);
-					contentSearchOptions.setContentClass(SourceGuide.class);
-					EadService.updateEverything(contentSearchOptions, QueueAction.DELETE);
-					contentSearchOptions.setContentClass(FindingAid.class);
-					EadService.updateEverything(contentSearchOptions, QueueAction.DELETE);
-					contentSearchOptions.setContentClass(EacCpf.class);
-					EacCpfService.updateEverything(contentSearchOptions, QueueAction.DELETE);					
-				} catch (IOException e) {
-					LOGGER.error("unexpected error occurs: " + e.getMessage(), e);
+			List<QueuingState> queuingStates = new ArrayList<QueuingState>();
+			queuingStates.add(QueuingState.NO);
+			queuingStates.add(QueuingState.ERROR);
+			contentSearchOptions.setQueuing(queuingStates);
+			String maintenanceAction = APEnetUtilities.getDashboardConfig().getMaintenanceAction();
+			if (StringUtils.isNotBlank(maintenanceAction)){
+				if (maintenanceAction.startsWith(REPUBLISH)) {
+					LOGGER.info("Execute command: " + maintenanceAction);
+					contentSearchOptions.setPublished(true);
+					updateEverything(maintenanceAction,contentSearchOptions, QueueAction.REPUBLISH);
+	
+				} else if (maintenanceAction.startsWith(DELETE)) {
+					LOGGER.info("Start to add all files to the queue to delete them");
+					updateEverything(maintenanceAction,contentSearchOptions, QueueAction.DELETE);
 				}
 			}
 		} finally {
 			JpaUtil.closeDatabaseSession();
 		}
+		
 		LOGGER.info("Ends maintenance mode");
+	}
+	private void updateEverything(String maintenanceAction,ContentSearchOptions contentSearchOptions, QueueAction queueAction){
+		boolean all = maintenanceAction.endsWith("all");
+		try {
+			if (all || maintenanceAction.contains("hg")){
+				contentSearchOptions.setContentClass(HoldingsGuide.class);
+				EadService.updateEverything(contentSearchOptions, queueAction);
+			}
+			if (all || maintenanceAction.contains("sg")){
+				contentSearchOptions.setContentClass(SourceGuide.class);
+				EadService.updateEverything(contentSearchOptions, queueAction);
+			}
+			if (all || maintenanceAction.contains("fa")){
+				contentSearchOptions.setContentClass(FindingAid.class);
+				EadService.updateEverything(contentSearchOptions, queueAction);
+			}
+			if (all || maintenanceAction.contains("eaccpf")){
+				contentSearchOptions.setContentClass(EacCpf.class);
+				EacCpfService.updateEverything(contentSearchOptions, queueAction);
+			}
+		} catch (IOException e) {
+			LOGGER.error("unexpected error occurs: " + e.getMessage(), e);
+		}
 	}
 }
