cd19cfb4fc217e7f8bb4bdf25dec5bdb53c3eb30
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-07-15 12:02:34 +0000

    MERGE BASTIAAN 2014-07-15
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@3472 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/archivesportaleurope/xml/xpath/TextMapXpathHandler.java
+++ Dashboard/src/main/java/eu/archivesportaleurope/xml/xpath/TextMapXpathHandler.java
@@ -1,39 +1,45 @@
 package eu.archivesportaleurope.xml.xpath;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.xml.stream.XMLStreamReader;
 
 import org.apache.commons.lang.StringUtils;
 
 public class TextMapXpathHandler extends AbstractXpathHandler {
-	private Map<String, String> results = new HashMap<String, String>();
+	private Map<String, List<String>> results = new HashMap<String, List<String>>();
 	private String currentKey = null;
-
+	private String temp = "";
+	private boolean addWhitespaces;
 	public TextMapXpathHandler(String defaultNamespace, String[] xpathQueryArray) {
 		super(defaultNamespace, xpathQueryArray);
+	}
+	public TextMapXpathHandler(String defaultNamespace, String[] xpathQueryArray, boolean addWhitespaces) {
+		super(defaultNamespace, xpathQueryArray);
+		this.addWhitespaces = addWhitespaces;
 	}
 
 	@Override
 	protected void writeContent(String content) {
-		if (StringUtils.isNotBlank(content) && currentKey != null){
-			String item = results.get(currentKey);
-			if (StringUtils.isBlank(item)){
-				item = removeUnusedCharacters(content);
-			}else {
-				item += removeUnusedCharacters(content);
+		if (StringUtils.isNotBlank(content)){
+			temp += removeUnusedCharacters(content);
+			if (addWhitespaces){
+				temp += WHITE_SPACE;
 			}
-			results.put(currentKey, item);
 		}
-		
+
 	}
+
 	@Override
 	protected void processExactEndElementMatch() {
 		currentKey=null;
 	}
-
-
 
 	@Override
 	public void processChildStartElementMatch(XMLStreamReader xmlReader) {
@@ -42,17 +48,40 @@ public class TextMapXpathHandler extends AbstractXpathHandler {
 
 	@Override
 	public void processChildEndElementMatch(XMLStreamReader xmlReader) {
+		if (currentKey != null && StringUtils.isNotBlank(temp)){
+			List<String> values = results.get(currentKey);
+			if (values == null){
+				values = new ArrayList<String>();
+				results.put(currentKey, values);
+			}
+			values.add(temp);
+		}
 		currentKey = null;
+		temp = "";
 	}
 
-	public Map<String, String> getResults() {
+	public Map<String, List<String>> getResults() {
 		return results;
 	}
 
+	public Set<String> getResultSet(String key) {
+		List<String> temp = results.get(key);
+		if (temp == null){
+			return new HashSet<String>();
+		}else {
+			return new LinkedHashSet<String>(temp);
+		}
+	}
+	public String getResultAsStringWithWhitespace(String key){
+		return convertToString(getResultSet(key),0, WHITE_SPACE);	
+	}
 	@Override
 	protected void clear() {
 		results.clear();
 		
 	}
-	
+	@Override
+	public boolean isAllTextBelow() {
+		return true;
+	}
 }
