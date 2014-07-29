5c551286ee5b8e7a3eb04ea9a72e49ca5b0d8c72
stefanpapp-ape <stefan.papp.ape@gmail.com>
2014-07-29 14:52:49 +0000

    [Dashboard] fix bug for EAC-CPF deletion where it was possible to delete whole directories without notice
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.6@3530 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/services/eaccpf/DeleteTask.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/services/eaccpf/DeleteTask.java
@@ -2,7 +2,6 @@ package eu.apenet.dashboard.services.eaccpf;
 
 import java.util.Properties;
 
-
 import eu.apenet.commons.exceptions.APEnetException;
 import eu.apenet.commons.utils.APEnetUtilities;
 import eu.apenet.dashboard.utils.ContentUtils;
@@ -10,37 +9,41 @@ import eu.apenet.persistence.dao.EacCpfDAO;
 
 import eu.apenet.persistence.factory.DAOFactory;
 import eu.apenet.persistence.vo.EacCpf;
-
+import java.io.File;
 
 public class DeleteTask extends AbstractEacCpfTask {
 
-	@Override
-	protected String getActionName() {
-		return "delete";
-	}
-	public static boolean valid(EacCpf eacCpf) {
-		boolean valid = false;
-		if (!eacCpf.isPublished()) {
-				valid =true;	
-		}
-		return valid;
-	}
-	@Override
-	protected void execute(EacCpf eacCpf, Properties properties) throws Exception {
+    @Override
+    protected String getActionName() {
+        return "delete";
+    }
 
-		if (valid(eacCpf)) {
-			try {
-               
-				ContentUtils.deleteFile(APEnetUtilities.getConfig().getRepoDirPath() + eacCpf.getPath());
-				EacCpfDAO eacCpfDAO = DAOFactory.instance().getEacCpfDAO();
-				eacCpfDAO.delete(eacCpf);
-				logAction(eacCpf);
-			} catch (Exception e) {
-				logAction(eacCpf, e);
-				throw new APEnetException(this.getActionName() + " " + e.getMessage(), e);
-			}
-		}
+    public static boolean valid(EacCpf eacCpf) {
+        boolean valid = false;
+        if (!eacCpf.isPublished()) {
+            valid = true;
+        }
+        return valid;
+    }
 
-	}
+    @Override
+    protected void execute(EacCpf eacCpf, Properties properties) throws Exception {
+
+        if (valid(eacCpf)) {
+            try {
+                File fileToDelete = new File(APEnetUtilities.getConfig().getRepoDirPath() + eacCpf.getPath());
+                if (fileToDelete.exists() && !fileToDelete.isDirectory()) {
+                    ContentUtils.deleteFile(fileToDelete, true);
+                }
+                EacCpfDAO eacCpfDAO = DAOFactory.instance().getEacCpfDAO();
+                eacCpfDAO.delete(eacCpf);
+                logAction(eacCpf);
+            } catch (Exception e) {
+                logAction(eacCpf, e);
+                throw new APEnetException(this.getActionName() + " " + e.getMessage(), e);
+            }
+        }
+
+    }
 
 }
