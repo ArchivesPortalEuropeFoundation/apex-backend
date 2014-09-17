743b1ff6d84827324f921e7cbc5036591f5c3330
fernando <fernando@archivesportaleurope.net>
2014-09-17 15:53:59 +0000

    updates the type of some columns in the database in order to allow the user to store large texts
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@3734 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ persistence/src/main/resources/dbScripts/2014-09-17-update-saved_bookmarks.sql
@@ -0,0 +1,3 @@
+alter table saved_bookmarks alter column bookmark_name TYPE text;
+alter table saved_bookmarks alter column description TYPE text;
+alter table saved_bookmarks alter column persistent_link TYPE text;
