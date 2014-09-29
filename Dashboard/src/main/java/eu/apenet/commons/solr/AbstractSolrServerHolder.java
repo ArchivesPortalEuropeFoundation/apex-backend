6403fd7d1686b4a8dc65fce657c134bec3e0cea9
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-09-29 10:47:23 +0000

    IssueID 1618: Solr problems
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.6@3771 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/commons/solr/AbstractSolrServerHolder.java
+++ Dashboard/src/main/java/eu/apenet/commons/solr/AbstractSolrServerHolder.java
@@ -8,9 +8,15 @@ import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.common.SolrInputDocument;
 
+import eu.apenet.dashboard.utils.PropertiesKeys;
+import eu.apenet.dashboard.utils.PropertiesUtil;
+
 public abstract class AbstractSolrServerHolder {
 
 	private final static Logger LOGGER = Logger.getLogger(AbstractSolrServerHolder.class);
+	private final static Integer HTTP_TIMEOUT = PropertiesUtil.getInt(PropertiesKeys.APE_SOLR_HTTP_TIMEOUT);
+	private final static Integer HTTP_LONG_TIMEOUT = PropertiesUtil.getInt(PropertiesKeys.APE_SOLR_HTTP_LONG_TIMEOUT);
+
 	protected abstract String getSolrUrl();
 	private HttpSolrServer solrServer;
 	
@@ -23,6 +29,8 @@ public abstract class AbstractSolrServerHolder {
 		if (isAvailable()) {
 			try {
 				long startTime = System.currentTimeMillis();
+				solrServer.setConnectionTimeout(HTTP_TIMEOUT);
+				solrServer.setSoTimeout(HTTP_TIMEOUT);
 				solrServer.deleteByQuery(query);
 				return System.currentTimeMillis() - startTime;
 			} catch (Exception e) {
@@ -37,6 +45,8 @@ public abstract class AbstractSolrServerHolder {
 		if (isAvailable()) {
 			try {
 				long startTime = System.currentTimeMillis();
+				solrServer.setConnectionTimeout(HTTP_TIMEOUT);
+				solrServer.setSoTimeout(HTTP_TIMEOUT);
 				solrServer.add(documents);
 				return System.currentTimeMillis() - startTime;
 			} catch (Exception e) {
@@ -51,6 +61,8 @@ public abstract class AbstractSolrServerHolder {
 		if (isAvailable()) {
 			try {
 				long startTime = System.currentTimeMillis();
+				solrServer.setConnectionTimeout(HTTP_LONG_TIMEOUT);
+				solrServer.setSoTimeout(HTTP_LONG_TIMEOUT);		
 				solrServer.commit(true, true, false);
 				LOGGER.info("hardcommit: " + (System.currentTimeMillis() - startTime) + "ms");
 				return System.currentTimeMillis() - startTime;
@@ -66,6 +78,8 @@ public abstract class AbstractSolrServerHolder {
 			if (solrServer == null) {
 				LOGGER.debug("Create new solr client: " + getSolrUrl());
 				solrServer = new HttpSolrServer(getSolrUrl());
+				solrServer.setConnectionTimeout(HTTP_TIMEOUT);
+				solrServer.setSoTimeout(HTTP_TIMEOUT);
 			}
 		} catch (Exception e) {
 			LOGGER.error("Solr server " + getSolrUrl() + " is not available", e);
@@ -83,6 +97,8 @@ public abstract class AbstractSolrServerHolder {
 				query.set("spellcheck", "on");
 				query.set("spellcheck.build", "true");
 				query.set("spellcheck.count", "0");
+				solrServer.setConnectionTimeout(HTTP_LONG_TIMEOUT);
+				solrServer.setSoTimeout(HTTP_LONG_TIMEOUT);				
 				solrServer.query(query);
 				LOGGER.info("rebuild spellchecker of " + getSolrUrl() + ": " + (System.currentTimeMillis() - startTime) + "ms");
 				return System.currentTimeMillis() - startTime;
