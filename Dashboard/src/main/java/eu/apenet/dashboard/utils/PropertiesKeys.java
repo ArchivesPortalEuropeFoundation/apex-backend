a59c9f7c297a50482438c6b92703825a519abf00
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-09-30 06:33:39 +0000

    merge from bastiaan (30-09-2014)
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@3783 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ Dashboard/src/main/java/eu/apenet/dashboard/utils/PropertiesKeys.java
@@ -0,0 +1,19 @@
+package eu.apenet.dashboard.utils;
+
+
+
+public class PropertiesKeys {
+
+	public static final String APE_EMAILS_TROUBLES = "ape.emails.troubles";
+	public static final String APE_EMAILS_DATA = "ape.emails.data";
+	public static final String APE_EMAILS_PROCESSING = "ape.emails.processing";
+	public static final String APE_EMAILS_EUROPEANA = "ape.emails.europeana";
+	public static final String APE_EMAILS_OTHER = "ape.emails.other";	
+	public static final String APE_EMAILS_SUGGESTIONS = "ape.emails.suggestions";		
+	public static final String APE_EMAILS_ADMINS = "ape.emails.admins";
+	
+	public static final String APE_SOLR_HTTP_TIMEOUT = "ape.solr.http.timeout";
+	public static final String APE_SOLR_HTTP_LONG_TIMEOUT = "ape.solr.http.long.timeout";
+	
+
+}
