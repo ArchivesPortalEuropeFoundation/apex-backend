f1591f01797b1618746940e5f5da7641e43cd272
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-10-10 10:37:50 +0000

    IssueID 1631: Topics
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@3885 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/java/eu/apenet/persistence/dao/TopicMappingDAO.java
+++ persistence/src/main/java/eu/apenet/persistence/dao/TopicMappingDAO.java
@@ -1,10 +1,12 @@
 package eu.apenet.persistence.dao;
 
+import java.util.List;
+
 import eu.apenet.persistence.vo.TopicMapping;
 
 public interface TopicMappingDAO extends GenericDAO<TopicMapping, Long> {
 
-
+	public List<TopicMapping> getTopicMappingsByAiId(Integer aiId);
 
 
 }
