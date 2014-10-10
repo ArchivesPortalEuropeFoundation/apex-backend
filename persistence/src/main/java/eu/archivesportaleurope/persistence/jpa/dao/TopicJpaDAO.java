34891caf88d98a2aca32d4c2e960afab8e66ee89
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-10-10 09:53:21 +0000

    IssueID 1631: Topics
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@3879 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ persistence/src/main/java/eu/archivesportaleurope/persistence/jpa/dao/TopicJpaDAO.java
@@ -0,0 +1,10 @@
+package eu.archivesportaleurope.persistence.jpa.dao;
+
+import eu.apenet.persistence.dao.TopicDAO;
+import eu.apenet.persistence.hibernate.AbstractHibernateDAO;
+import eu.apenet.persistence.vo.Topic;
+
+public class TopicJpaDAO extends AbstractHibernateDAO<Topic, Long> implements TopicDAO {
+
+
+}
---
+++ persistence/src/main/java/eu/archivesportaleurope/persistence/jpa/dao/TopicMappingJpaDAO.java
@@ -0,0 +1,10 @@
+package eu.archivesportaleurope.persistence.jpa.dao;
+
+import eu.apenet.persistence.dao.TopicMappingDAO;
+import eu.apenet.persistence.hibernate.AbstractHibernateDAO;
+import eu.apenet.persistence.vo.TopicMapping;
+
+public class TopicMappingJpaDAO extends AbstractHibernateDAO<TopicMapping, Long> implements TopicMappingDAO {
+
+
+}
