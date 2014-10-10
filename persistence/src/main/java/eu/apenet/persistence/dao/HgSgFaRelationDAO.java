dd7634b6ea61d9bc6567b41714fb7acbce385d4d
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-10-10 10:18:26 +0000

    IssueID 1631: Topics
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@3884 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/java/eu/apenet/persistence/dao/HgSgFaRelationDAO.java
+++ persistence/src/main/java/eu/apenet/persistence/dao/HgSgFaRelationDAO.java
@@ -9,4 +9,5 @@ import eu.apenet.persistence.vo.HgSgFaRelation;
 public interface HgSgFaRelationDAO extends GenericDAO<HgSgFaRelation, Long> {
 	public List<HgSgFaRelation> getHgSgFaRelations(Integer id,Class<? extends Ead> clazz, Boolean published);
 	public Long countHgSgFaRelations(Integer id,Class<? extends Ead> clazz, Boolean published);
+	public boolean existHgSgFaRelations(Integer sgId, Integer faId);
 }
