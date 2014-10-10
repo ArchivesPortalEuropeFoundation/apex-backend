3e9eea9061ae59bf16cb0bbde14d172de89ba55b
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-10-10 09:53:32 +0000

    IssueID 1631: Topics
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@3880 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ persistence/src/main/java/eu/apenet/persistence/vo/Topic.java
@@ -0,0 +1,39 @@
+package eu.apenet.persistence.vo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "topic")
+public class Topic {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "property_key")
+	private String propertyKey;
+	private String description;
+	public Long getId() {
+		return id;
+	}
+	public String getPropertyKey() {
+		return propertyKey;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public void setPropertyKey(String propertyKey) {
+		this.propertyKey = propertyKey;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
---
+++ persistence/src/main/java/eu/apenet/persistence/vo/TopicMapping.java
@@ -0,0 +1,90 @@
+package eu.apenet.persistence.vo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "topic_mapping")
+public class TopicMapping {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="ai_id",insertable=false, updatable=false)
+	private ArchivalInstitution archivalInstitution;
+	@Column(name = "ai_id")
+	private Integer aiId;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="topic_id",insertable=false, updatable=false)
+	private Topic topic;
+	@Column(name = "topic_id")
+	private Long topicId;
+	
+	@Column(name = "controlaccess_keyword")
+	private String controlaccessKeyword;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="sg_id",insertable=false, updatable=false)
+	private SourceGuide sourceGuide;
+	@Column(name = "sg_id")
+	private Integer sgId;
+	public Long getId() {
+		return id;
+	}
+	public ArchivalInstitution getArchivalInstitution() {
+		return archivalInstitution;
+	}
+	public Integer getAiId() {
+		return aiId;
+	}
+	public Topic getTopic() {
+		return topic;
+	}
+	public Long getTopicId() {
+		return topicId;
+	}
+	public String getControlaccessKeyword() {
+		return controlaccessKeyword;
+	}
+	public SourceGuide getSourceGuide() {
+		return sourceGuide;
+	}
+	public Integer getSgId() {
+		return sgId;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public void setArchivalInstitution(ArchivalInstitution archivalInstitution) {
+		this.archivalInstitution = archivalInstitution;
+	}
+	public void setAiId(Integer aiId) {
+		this.aiId = aiId;
+	}
+	public void setTopic(Topic topic) {
+		this.topic = topic;
+	}
+	public void setTopicId(Long topicId) {
+		this.topicId = topicId;
+	}
+	public void setControlaccessKeyword(String controlaccessKeyword) {
+		this.controlaccessKeyword = controlaccessKeyword;
+	}
+	public void setSourceGuide(SourceGuide sourceGuide) {
+		this.sourceGuide = sourceGuide;
+	}
+	public void setSgId(Integer sgId) {
+		this.sgId = sgId;
+	}
+
+}
