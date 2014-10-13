89fc9350a0d1f7be14a9c7cb87a0e42dd1890a15
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-10-13 10:03:05 +0000

    IssueID 1631: Topics
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@3896 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/resources/dbScripts/2014-10-13-update-topics.sql
+++ persistence/src/main/resources/dbScripts/2014-10-13-update-topics.sql
@@ -1,1 +1,5 @@
 alter table ead_saved_search alter column refinement_topic text DEFAULT NULL;
+alter table ead_saved_search drop column pagenumber;
+alter table ead_saved_search drop column sorting;
+alter table ead_saved_search drop column results_per_page;
+alter table ead_saved_search drop column sorting_asc;
