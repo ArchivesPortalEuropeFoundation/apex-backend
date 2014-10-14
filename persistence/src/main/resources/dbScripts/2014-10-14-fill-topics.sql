cc62cddf9460dd6627f46a1a0fc0436f25cff4aa
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-10-14 13:26:53 +0000

    IssueID 1631: Topics
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@3937 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ persistence/src/main/resources/dbScripts/2014-10-14-fill-topics.sql
@@ -0,0 +1,6 @@
+INSERT INTO topic(property_key, description) VALUES ('slavery', 'Slavery');
+INSERT INTO topic(property_key, description) VALUES ('slave', 'Slave trade');
+INSERT INTO topic(property_key, description) VALUES ('war.world.first', 'First World War');
+INSERT INTO topic(property_key, description) VALUES ('war.world.second', 'Second World War');
+INSERT INTO topic(property_key, description) VALUES ('war.cold', 'Cold War');
+INSERT INTO topic(property_key, description) VALUES ('collection.state', 'Rijkscollectie');
