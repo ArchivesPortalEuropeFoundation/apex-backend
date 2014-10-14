52aaf4ba06d9100d45cb09892d3ef9b8ec19eb81
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-10-14 07:41:58 +0000

    IssueID 1631: Topics
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@3925 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/topics/ManageTopicMappingsAction.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/topics/ManageTopicMappingsAction.java
@@ -5,16 +5,24 @@ package eu.apenet.dashboard.topics;
 import java.util.List;
 
 import eu.apenet.dashboard.AbstractInstitutionAction;
+import eu.apenet.persistence.dao.TopicMappingDAO;
 import eu.apenet.persistence.factory.DAOFactory;
 import eu.apenet.persistence.vo.TopicMapping;
 
 public class ManageTopicMappingsAction extends AbstractInstitutionAction {
 	
 	private static final long serialVersionUID = -5957772691331439159L;
+	private Long topicMappingId;
+	
+	
+	public Long getTopicMappingId() {
+		return topicMappingId;
+	}
 
-	
-	
-	
+	public void setTopicMappingId(Long topicMappingId) {
+		this.topicMappingId = topicMappingId;
+	}
+
 	@Override
 	protected void buildBreadcrumbs() {
         super.buildBreadcrumbs();
@@ -32,6 +40,13 @@ public class ManageTopicMappingsAction extends AbstractInstitutionAction {
 		return SUCCESS;
 	}
 	
-
+	public String deleteTopicMapping(){
+		TopicMappingDAO topicMappingDAO = DAOFactory.instance().getTopicMappingDAO();
+		TopicMapping topicMapping = topicMappingDAO.getTopicMappingByIdAndAiId(topicMappingId, this.getAiId());
+		if (topicMapping != null){
+			topicMappingDAO.delete(topicMapping);
+		}
+		return SUCCESS;
+	}
 }
 
