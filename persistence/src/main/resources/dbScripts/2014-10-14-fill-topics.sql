6a91a1a56250c4fdf5025c5deb8861798195feba
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-10-24 04:37:36 +0000

    IssueID 1631: Topics
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@3979 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/resources/dbScripts/2014-10-14-fill-topics.sql
+++ persistence/src/main/resources/dbScripts/2014-10-14-fill-topics.sql
@@ -1,6 +1,8 @@
 INSERT INTO topic(property_key, description) VALUES ('slavery', 'Slavery');
-INSERT INTO topic(property_key, description) VALUES ('slave', 'Slave trade');
-INSERT INTO topic(property_key, description) VALUES ('war.world.first', 'First World War');
-INSERT INTO topic(property_key, description) VALUES ('war.world.second', 'Second World War');
-INSERT INTO topic(property_key, description) VALUES ('war.cold', 'Cold War');
-INSERT INTO topic(property_key, description) VALUES ('collection.state', 'Rijkscollectie');
+INSERT INTO topic(property_key, description) VALUES ('slave.trade', 'Slave trade');
+INSERT INTO topic(property_key, description) VALUES ('first.world.war', 'First World War');
+INSERT INTO topic(property_key, description) VALUES ('second.world.war', 'Second World War');
+INSERT INTO topic(property_key, description) VALUES ('cold.war', 'Cold War');
+INSERT INTO topic(property_key, description) VALUES ('state.collection', 'State collection');
+INSERT INTO topic(property_key, description) VALUES ('middle.ages', 'Middle Ages');
+INSERT INTO topic(property_key, description) VALUES ('gdr', 'German Democratic Republic');
