fb20a7aec6ab07a5d210fc8fbc3384c001328d51
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-10-28 06:57:56 +0000

    IssueID 1403: feedback email
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@4011 9fd842fd-667d-44da-918d-ff91420b3ae1

--- commons/src/main/java/eu/apenet/commons/utils/APEnetUtilities.java
+++ commons/src/main/java/eu/apenet/commons/utils/APEnetUtilities.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.Locale;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 
@@ -29,8 +30,10 @@ public class APEnetUtilities {
 	private static final Logger LOG = Logger.getLogger(APEnetUtilities.class);
 	public static final String FILESEPARATOR = "/";
 	private static final String UTF8 = "utf-8";
+    public static final Pattern EMAIL_PATTERN = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])",Pattern.CASE_INSENSITIVE);
 	private static Map<String, String> iso2ToIso3LanguageCodesMap;
 	private static Map<String, Locale> iso3ToIso2LanguageCodesMap;
+
 	static {
 		iso2ToIso3LanguageCodesMap = new TreeMap<String, String>();
 		iso3ToIso2LanguageCodesMap = new TreeMap<String, Locale>();
