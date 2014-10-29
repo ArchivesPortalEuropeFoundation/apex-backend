db50e539aade7f5f6c32f1f56dd0ac5d03d71cda
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-10-29 13:31:30 +0000

    Add better WebDAV support
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@4038 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ files/src/main/java/eu/archivesportaleurope/webdav/AbstractResource.java
@@ -0,0 +1,151 @@
+package eu.archivesportaleurope.webdav;
+
+import io.milton.http.Auth;
+import io.milton.http.Range;
+import io.milton.http.Request;
+import io.milton.http.Request.Method;
+import io.milton.http.exceptions.BadRequestException;
+import io.milton.http.exceptions.NotAuthorizedException;
+import io.milton.http.exceptions.NotFoundException;
+import io.milton.resource.GetableResource;
+import io.milton.resource.PropFindableResource;
+import io.milton.servlet.MiltonServlet;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+
+import eu.apenet.commons.utils.APEnetUtilities;
+import eu.archivesportaleurope.webdav.security.SecurityContext;
+import eu.archivesportaleurope.webdav.security.SecurityService;
+import eu.archivesportaleurope.webdav.security.SecurityService.LoginResult;
+
+public abstract class AbstractResource implements PropFindableResource, GetableResource {
+	protected final Logger logger = Logger.getLogger(this.getClass());
+
+	private File file;
+	private String url;
+
+	public AbstractResource(File file, String url) {
+		this.file = file;
+		this.url = url;
+	}
+
+	@Override
+	public final Object authenticate(String user, String password) {
+		try {
+			HttpServletRequest request = MiltonServlet.request();
+			SecurityContext securityContext = getSecurityContext();
+			if (securityContext == null) {
+				LoginResult loginResult = SecurityService.webDavLogin(user, password, request);
+				securityContext = loginResult.getContext();
+			}
+			return securityContext;
+
+		} catch (NotAuthorizedException e) {
+			logger.error("Error while trying to login with name: " + user);
+			return null;
+		}
+
+	}
+
+	@Override
+	public final boolean authorise(Request request, Method method, Auth auth) {
+		 if (auth != null && auth.getTag() != null) {
+			logger.info(auth.getUser() + " " + method);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public final String getRealm() {
+		return "Dashboard Archives Portal Europe";
+	}
+
+	protected File getFile() {
+		return file;
+	}
+
+	protected String getUrl() {
+		return url;
+	}
+
+	protected static SecurityContext getSecurityContext() {
+		HttpServletRequest request = MiltonServlet.request();
+		return SecurityContext.getWebDavContext(request);
+	}
+
+	protected static boolean acceptRead(File file) {
+		SecurityContext securityContext = getSecurityContext();
+		if (securityContext.isAdmin()) {
+			return true;
+		} else {
+			// make relative path
+			String path = file.getAbsolutePath();
+			if (path.startsWith(APEnetUtilities.getConfig().getRepoDirPath())) {
+				path = path.substring(APEnetUtilities.getConfig().getRepoDirPath().length());
+			}
+			// everyone authenticated user may see the base dir.
+			if (path.length() == 0) {
+				return true;
+			}
+			String countryPath = APEnetUtilities.FILESEPARATOR + securityContext.getCountryIsoname();
+			if (securityContext.isCountryManager() && path.startsWith(countryPath)) {
+				return true;
+			} else if (securityContext.isInstitutionManager()) {
+				if (path.startsWith(countryPath)) {
+					path = path.substring(countryPath.length());
+					if (path.length() == 0) {
+						return true;
+					} else {
+						boolean match = false;
+						Iterator<Integer> aiIds = securityContext.getAiIds().iterator();
+						while (!match && aiIds.hasNext()) {
+							Integer aiId = aiIds.next();
+							String aiPath = APEnetUtilities.FILESEPARATOR + aiId;
+							if (path.startsWith(aiPath)) {
+								return true;
+							}
+						}
+					}
+				}
+
+			}
+		}
+		return false;
+	}
+
+//	@Override
+//	public final void sendContent(OutputStream out, Range range, Map<String, String> params, String contentType)
+//			throws IOException, NotAuthorizedException, BadRequestException, NotFoundException {
+//		if (acceptRead(getFile())) {
+//			sendContentInternal(out, range, params, contentType);
+//		} else {
+//			throw new NotFoundException("not found");
+//		}
+//
+//	}
+
+	@Override
+	public void sendContent(OutputStream out, Range range, Map<String, String> params, String contentType)
+			throws IOException, io.milton.http.exceptions.NotAuthorizedException, BadRequestException,
+			NotFoundException {
+		if (acceptRead(getFile())) {
+		sendContentInternal(out, range, params, contentType);
+	} else {
+		throw new NotFoundException("not found");
+	}
+		
+	}
+
+	protected abstract void sendContentInternal(OutputStream out, Range range, Map<String, String> params,
+			String contentType) throws IOException, NotAuthorizedException, BadRequestException, NotFoundException;
+
+}
---
+++ files/src/main/java/eu/archivesportaleurope/webdav/DirectoryResource.java
@@ -0,0 +1,188 @@
+package eu.archivesportaleurope.webdav;
+
+import io.milton.http.Auth;
+import io.milton.http.Range;
+import io.milton.http.Request;
+import io.milton.http.exceptions.BadRequestException;
+import io.milton.http.exceptions.NotAuthorizedException;
+import io.milton.http.exceptions.NotFoundException;
+import io.milton.resource.CollectionResource;
+import io.milton.resource.GetableResource;
+import io.milton.resource.PropFindableResource;
+import io.milton.resource.Resource;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+
+import eu.apenet.commons.utils.APEnetUtilities;
+import eu.apenet.persistence.factory.DAOFactory;
+import eu.apenet.persistence.vo.ArchivalInstitution;
+
+public class DirectoryResource extends AbstractResource implements PropFindableResource, CollectionResource,
+		GetableResource {
+
+	private File[] childs = null;
+	private String name;
+	public DirectoryResource(File file, String url) {
+		super(file, url);
+		name = getName(file);
+	}
+	private String getName(File file){
+		if (file.isDirectory() && StringUtils.isNumeric(file.getName())){
+			// make relative path
+			String path = file.getAbsolutePath();
+			if (path.startsWith(APEnetUtilities.getConfig().getRepoDirPath())) {
+				path = path.substring(APEnetUtilities.getConfig().getRepoDirPath().length());
+			}
+			if (path.length() > 0) {
+				path = path.substring(1);
+				if (path.split(APEnetUtilities.FILESEPARATOR).length == 2){
+					Integer aiId = Integer.parseInt(file.getName());
+					ArchivalInstitution ai = DAOFactory.instance().getArchivalInstitutionDAO().getArchivalInstitution(aiId);
+					if (ai != null){
+						return file.getName() + "-" + APEnetUtilities.convertToFilename(ai.getAiname());
+					}
+				}
+
+			}
+		}
+
+
+		return file.getName();
+	}
+
+	@Override
+	public Resource child(String childName) throws NotAuthorizedException, BadRequestException {
+		File child = new File(getFile(), childName);
+		if (acceptRead(child)) {
+			if (child.exists()) {
+				if (child.isDirectory()) {
+					return new DirectoryResource(child, getUrl());
+				} else {
+					return new FileResource(child, getUrl());
+				}
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public List<? extends Resource> getChildren() throws NotAuthorizedException, BadRequestException {
+		List<Resource> results = new ArrayList<Resource>();
+		for (File child : getChilds()) {
+				if (child.isDirectory()) {
+					results.add(new DirectoryResource(child, getUrl()));
+				} else {
+					results.add(new FileResource(child, getUrl()));
+				}
+		}
+		return results;
+	}
+
+	@Override
+	public String getUniqueId() {
+		return getFile().getName();
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public Date getModifiedDate() {
+		return new Date(getFile().lastModified());
+	}
+
+	@Override
+	public String checkRedirect(Request request) {
+		return null;
+	}
+
+	@Override
+	public Date getCreateDate() {
+		return new Date();
+	}
+	private static String toFileSize(Long fileSize) {
+		if (fileSize == null) {
+			return null;
+		}
+		long size = fileSize.longValue();
+		if (size > 1024 * 1024) {
+			return size / (1024 * 1024) + " MB";
+		} else if (size > 1024) {
+			return size / 1024 + " KB";
+		} else {
+			return size + " B";
+		}
+	}
+
+	@Override
+	public void sendContentInternal(OutputStream out, Range range, Map<String, String> params, String contentType)
+			throws IOException, NotAuthorizedException, BadRequestException, NotFoundException {
+		PrintWriter printWriter = new PrintWriter(out);
+		printWriter.append("<html><body><table  border=\"1\" cellpadding=\"10\"><tr><th>Name</th><th>Type</th><th>Last modified</th><th>Size</th></tr>");
+		String baseUrl = getUrl();
+		if (!getUrl().endsWith("/")){
+			baseUrl = baseUrl + "/";
+		}
+		for (File child : getChilds()) {
+			String name = getName(child);
+				String uri =baseUrl + name;
+				if (child.isDirectory()) {
+					printWriter.append("<tr><td><b><a href=\"" + uri + "\"/>" + name
+							+ "</a></b></td><td>Directory</td><td>" + new Date(child.lastModified()) + "</td><td></td></tr>");
+				} else {
+					printWriter.append("<tr><td><a href=\"" + uri + "\"/>" + name + "</a></td><td>File</td><td>"
+							+ new Date(child.lastModified()) + "</td><td  ALIGN=\"right\">" + toFileSize(child.length()) + "</td></tr>");
+				}
+	
+		}
+		printWriter.append("</table></body></html>");
+		printWriter.flush();
+		// 
+		out.flush();
+		printWriter.close();
+		out.close();
+
+	
+	}
+	
+
+	@Override
+	public Long getMaxAgeSeconds(Auth auth) {
+		return 30l;
+	}
+
+	@Override
+	public String getContentType(String accepts) {
+		return "text/html";
+	}
+
+	@Override
+	public Long getContentLength() {
+		return -1l;
+	}
+
+	protected File[] getChilds(){
+		if (childs == null){
+			childs = getFile().listFiles(new FileFilter() {
+				
+				@Override
+				public boolean accept(File file) {
+					return DirectoryResource.acceptRead(file);
+				}
+			});
+		}
+		return childs;
+	}
+}
