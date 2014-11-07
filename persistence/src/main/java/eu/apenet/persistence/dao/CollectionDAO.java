e5a6b22146c6decbc6ff0a859407304bc9557ec3
fernando <fernando@archivesportaleurope.net>
2014-11-07 13:33:41 +0000

    Feature #1684 Select collections from second display
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@4141 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/java/eu/apenet/persistence/dao/CollectionDAO.java
+++ persistence/src/main/java/eu/apenet/persistence/dao/CollectionDAO.java
@@ -9,6 +9,7 @@ public interface CollectionDAO extends GenericDAO<Collection, Long>{
 	
 	public List<Collection> getCollectionsByUserId(Long liferayUserId,Integer pageNumber, Integer pagesize, String field, boolean showDesc);
 	public Collection getCollectionById(Long id);
+	public List<Collection>  getCollectionByName (Long liferayUserId, String name, int pageSize);
 	public Long countCollectionsByUserId(Long liferayUserId);
 	public Collection getCollectionByIdAndUserId(Long id, Long liferayUserId);
 }
--- persistence/src/main/java/eu/apenet/persistence/hibernate/CollectionHibernateDAO.java
+++ persistence/src/main/java/eu/apenet/persistence/hibernate/CollectionHibernateDAO.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Expression;
+import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
@@ -15,6 +18,8 @@ import eu.archivesportaleurope.util.ApeUtil;
 
 public class CollectionHibernateDAO extends AbstractHibernateDAO<Collection, Long> implements CollectionDAO{
 	private static final Logger LOGGER = Logger.getLogger(CollectionHibernateDAO.class);
+	private static final String ORDER = "modified_date";
+	
 	@Override
 	public List<Collection> getCollectionsByUserId(Long liferayUserId,Integer pageNumber,Integer pageSize, String sortValue,
 			boolean ascending) {
@@ -83,5 +88,21 @@ public class CollectionHibernateDAO extends AbstractHibernateDAO<Collection, Lon
 		}
 		return collection;
 	}
-
+		
+	@Override
+	public List<Collection> getCollectionByName(Long liferayUserId, String title, int pageSize) {
+		if(liferayUserId!=null && liferayUserId>0){
+			Criteria criteria = getSession().createCriteria(getPersistentClass(), "collection");
+			criteria = criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+			if(liferayUserId!=null && liferayUserId>0){
+				criteria.add(Restrictions.eq("collection.liferayUserId", liferayUserId));
+				criteria.addOrder(Order.desc("modified_date"));
+				criteria.setMaxResults(pageSize);
+				criteria.add(Restrictions.like("collection.title",title, MatchMode.ANYWHERE));
+			}
+			return criteria.list();
+		}
+		return null;
+	}
 }
+
