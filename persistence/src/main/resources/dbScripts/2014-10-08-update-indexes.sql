b4d50732f571a3b93cda76fa40017ebac5493d19
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-11-14 11:09:17 +0000

    db fix
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-1.7@4181 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/resources/dbScripts/2014-10-08-update-indexes.sql
+++ persistence/src/main/resources/dbScripts/2014-10-08-update-indexes.sql
@@ -1,11 +1,11 @@
 CREATE INDEX finding_aid_path
   ON finding_aid
-  USING btree (path_apenetead)
+  USING btree (path_apenetead);
 
 CREATE INDEX holdings_guide_path
   ON holdings_guide
-  USING btree (path_apenetead)
+  USING btree (path_apenetead);
 
 CREATE INDEX source_guide_path
   ON source_guide
-  USING btree (path_apenetead)
+  USING btree (path_apenetead);
