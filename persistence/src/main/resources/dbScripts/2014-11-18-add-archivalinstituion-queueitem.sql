815ebb6cf208311af5b0a86bd5bfeeea11e214f9
yoannmoranville <yoann@archivesportaleurope.net>
2014-11-19 09:01:54 +0000

    #1739
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@4197 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/resources/dbScripts/2014-11-18-add-archivalinstituion-queueitem.sql
+++ persistence/src/main/resources/dbScripts/2014-11-18-add-archivalinstituion-queueitem.sql
@@ -1,9 +1,9 @@
 alter table queue add column ai_id integer;
 
-update queue set queue.ai_id = finding_aid.ai_id FROM queue, finding_aid WHERE queue.fa_id = finding_aid.id;
-update queue set queue.ai_id = holdings_guide.ai_id FROM queue, holdings_guide WHERE queue.hg_id = holdings_guide.id;
-update queue set queue.ai_id = source_guide.ai_id FROM queue, source_guide WHERE queue.sg_id = source_guide.id;
-update queue set queue.ai_id = eac_cpf.ai_id FROM queue, eac_cpf WHERE queue.eac_cpf_id = eac_cpf.id;
-update queue set queue.ai_id = up_file.ai_id FROM queue, up_file WHERE queue.uf_id = up_file.id;
+update queue set ai_id = finding_aid.ai_id FROM queue queue1, finding_aid WHERE queue1.fa_id = finding_aid.id;
+update queue set ai_id = holdings_guide.ai_id FROM queue queue1, holdings_guide WHERE queue1.hg_id = holdings_guide.id;
+update queue set ai_id = source_guide.ai_id FROM queue queue1, source_guide WHERE queue1.sg_id = source_guide.id;
+update queue set ai_id = eac_cpf.ai_id FROM queue queue1, eac_cpf WHERE queue1.eac_cpf_id = eac_cpf.id;
+update queue set ai_id = up_file.ai_id FROM queue queue1, up_file WHERE queue1.uf_id = up_file.id;
 
 alter table queue alter column ai_id set not null;
