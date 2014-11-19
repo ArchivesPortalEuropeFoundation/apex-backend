60c555685ad31858a2fa74bbd03c2c243a4fa2cf
yoannmoranville <yoann@archivesportaleurope.net>
2014-11-19 11:46:20 +0000

    Getting rid of apeEAD_XSD1.0.xsd affiliations
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@4208 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/archivesportaleurope/xml/ApeXMLConstants.java
+++ Dashboard/src/main/java/eu/archivesportaleurope/xml/ApeXMLConstants.java
@@ -12,7 +12,7 @@ public class ApeXMLConstants {
 	public static final String METS_NAMESPACE = "http://www.loc.gov/METS/";
 	public static final String XLINK_PREFIX = "xlink";
 	public static final String XSI_PREFIX = "xsi";
-	public static final String APE_EAD_LOCATION = "http://schemas.archivesportaleurope.net/profiles/apeEAD_XSD1.0.xsd";
+	public static final String APE_EAD_LOCATION = "http://schemas.archivesportaleurope.net/profiles/apeEAD.xsd";
 	public static final String XLINK_LOCATION = "http://www.loc.gov/standards/xlink/xlink.xsd";
 	public static final String SCHEMA_LOCATION = "schemaLocation";
 
