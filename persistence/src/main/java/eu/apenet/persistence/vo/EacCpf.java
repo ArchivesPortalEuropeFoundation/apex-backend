b9d4b8c55cfb90a95c38050b20c0c2b8e44af7a2
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-11-28 13:47:44 +0000

    IssueID 1404: queue processing message
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@4277 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/java/eu/apenet/persistence/vo/EacCpf.java
+++ persistence/src/main/java/eu/apenet/persistence/vo/EacCpf.java
@@ -22,224 +22,230 @@ import javax.persistence.TemporalType;
 @Table(name = "eac_cpf")
 public class EacCpf extends AbstractContent {
 
-    /**
+	/**
      *
      */
-    private static final long serialVersionUID = 6760184769676870729L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    private String title;
-    @Column(name = "upload_date")
+	private static final long serialVersionUID = 6760184769676870729L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String title;
+	@Column(name = "upload_date")
 	@Temporal(TemporalType.TIMESTAMP)
-    private Date uploadDate;
-    private String path;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "um_id")
-    private UploadMethod uploadMethod;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "ai_id")
-    private ArchivalInstitution archivalInstitution;
-    @Column(name = "ai_id", updatable = false, insertable = false)
-    private Integer aiId;
-    private boolean published = false;
-    private boolean converted = false;
-    private ValidatedState validated = ValidatedState.NOT_VALIDATED;
-    private EuropeanaState europeana = EuropeanaState.NOT_CONVERTED;
-    private long cpfRelations = 0l;
-    private long resourceRelations = 0l;
-    private long functionRelations = 0l;
-    private QueuingState queuing = QueuingState.NO;
-    @Column(name = "publish_date")
-    @Temporal(javax.persistence.TemporalType.DATE)
-    private Date publishDate;
-    @Column(nullable = false)
-    private String identifier;
+	private Date uploadDate;
+	private String path;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "um_id")
+	private UploadMethod uploadMethod;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ai_id")
+	private ArchivalInstitution archivalInstitution;
+	@Column(name = "ai_id", updatable = false, insertable = false)
+	private Integer aiId;
+	private boolean published = false;
+	private boolean converted = false;
+	private ValidatedState validated = ValidatedState.NOT_VALIDATED;
+	private EuropeanaState europeana = EuropeanaState.NOT_CONVERTED;
+	private long cpfRelations = 0l;
+	private long resourceRelations = 0l;
+	private long functionRelations = 0l;
+	private QueuingState queuing = QueuingState.NO;
+	@Column(name = "publish_date")
+	@Temporal(javax.persistence.TemporalType.DATE)
+	private Date publishDate;
+	@Column(nullable = false)
+	private String identifier;
 
-    @OneToMany(mappedBy = "eacCpf")
-    private Set<QueueItem> queueItems = new HashSet<QueueItem>(0);
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "eacCpf")
-    private Set<Warnings> warningses = new HashSet<Warnings>(0);
+	@OneToMany(mappedBy = "eacCpf")
+	private Set<QueueItem> queueItems = new HashSet<QueueItem>(0);
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "eacCpf")
+	private Set<Warnings> warningses = new HashSet<Warnings>(0);
 
-    @Override
-    public Integer getId() {
-        return id;
-    }
+	@Override
+	public Integer getId() {
+		return id;
+	}
 
-    @Override
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	@Override
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    @Override
-    public ArchivalInstitution getArchivalInstitution() {
-        return archivalInstitution;
-    }
+	@Override
+	public ArchivalInstitution getArchivalInstitution() {
+		return archivalInstitution;
+	}
 
-    @Override
-    public void setArchivalInstitution(ArchivalInstitution archivalInstitution) {
-        this.archivalInstitution = archivalInstitution;
-    }
+	@Override
+	public void setArchivalInstitution(ArchivalInstitution archivalInstitution) {
+		this.archivalInstitution = archivalInstitution;
+	}
 
-    @Override
-    public Integer getAiId() {
-        return aiId;
-    }
+	@Override
+	public Integer getAiId() {
+		return aiId;
+	}
 
-    @Override
-    public void setAiId(Integer aiId) {
-        this.aiId = aiId;
-    }
+	@Override
+	public void setAiId(Integer aiId) {
+		this.aiId = aiId;
+	}
 
-    @Override
-    public UploadMethod getUploadMethod() {
-        return uploadMethod;
-    }
+	@Override
+	public UploadMethod getUploadMethod() {
+		return uploadMethod;
+	}
 
-    @Override
-    public void setUploadMethod(UploadMethod uploadMethod) {
-        this.uploadMethod = uploadMethod;
-    }
+	@Override
+	public void setUploadMethod(UploadMethod uploadMethod) {
+		this.uploadMethod = uploadMethod;
+	}
 
-    @Override
-    public String getTitle() {
-        return title;
-    }
+	@Override
+	public String getTitle() {
+		return title;
+	}
 
-    @Override
-    public void setTitle(String nameEntry) {
-        this.title = nameEntry;
-    }
+	@Override
+	public void setTitle(String nameEntry) {
+		this.title = nameEntry;
+	}
 
-    @Override
-    public String getPath() {
-        return path;
-    }
+	@Override
+	public String getPath() {
+		return path;
+	}
 
-    @Override
-    public void setPath(String path) {
-        this.path = path;
-    }
+	@Override
+	public void setPath(String path) {
+		this.path = path;
+	}
 
-    @Override
-    public boolean isPublished() {
-        return published;
-    }
+	@Override
+	public boolean isPublished() {
+		return published;
+	}
 
-    @Override
-    public void setPublished(boolean published) {
-        this.published = published;
-    }
+	@Override
+	public void setPublished(boolean published) {
+		this.published = published;
+	}
 
-    @Override
-    public boolean isConverted() {
-        return converted;
-    }
+	@Override
+	public boolean isConverted() {
+		return converted;
+	}
 
-    @Override
-    public void setConverted(boolean converted) {
-        this.converted = converted;
-    }
+	@Override
+	public void setConverted(boolean converted) {
+		this.converted = converted;
+	}
 
-    @Override
-    public ValidatedState getValidated() {
-        return validated;
-    }
+	@Override
+	public ValidatedState getValidated() {
+		return validated;
+	}
 
-    @Override
-    public void setValidated(ValidatedState validated) {
-        this.validated = validated;
-    }
+	@Override
+	public void setValidated(ValidatedState validated) {
+		this.validated = validated;
+	}
 
-    public long getCpfRelations() {
-        return cpfRelations;
-    }
+	public long getCpfRelations() {
+		return cpfRelations;
+	}
 
-    public void setCpfRelations(long cpfRelations) {
-        this.cpfRelations = cpfRelations;
-    }
+	public void setCpfRelations(long cpfRelations) {
+		this.cpfRelations = cpfRelations;
+	}
 
-    public long getResourceRelations() {
-        return resourceRelations;
-    }
+	public long getResourceRelations() {
+		return resourceRelations;
+	}
 
-    public void setResourceRelations(long resourceRelations) {
-        this.resourceRelations = resourceRelations;
-    }
+	public void setResourceRelations(long resourceRelations) {
+		this.resourceRelations = resourceRelations;
+	}
 
-    public long getFunctionRelations() {
-        return functionRelations;
-    }
+	public long getFunctionRelations() {
+		return functionRelations;
+	}
 
-    public void setFunctionRelations(long functionRelations) {
-        this.functionRelations = functionRelations;
-    }
+	public void setFunctionRelations(long functionRelations) {
+		this.functionRelations = functionRelations;
+	}
 
-    public EuropeanaState getEuropeana() {
-        return europeana;
-    }
+	public EuropeanaState getEuropeana() {
+		return europeana;
+	}
 
-    public void setEuropeana(EuropeanaState europeana) {
-        this.europeana = europeana;
-    }
+	public void setEuropeana(EuropeanaState europeana) {
+		this.europeana = europeana;
+	}
 
-    @Override
-    public QueuingState getQueuing() {
-        return queuing;
-    }
+	@Override
+	public QueuingState getQueuing() {
+		return queuing;
+	}
 
-    @Override
-    public void setQueuing(QueuingState queuing) {
-        this.queuing = queuing;
-    }
+	@Override
+	public void setQueuing(QueuingState queuing) {
+		this.queuing = queuing;
+	}
 
-    @Override
-    public Date getUploadDate() {
-        return uploadDate;
-    }
+	@Override
+	public Date getUploadDate() {
+		return uploadDate;
+	}
 
-    @Override
-    public void setUploadDate(Date uploadDate) {
-        this.uploadDate = uploadDate;
-    }
+	@Override
+	public void setUploadDate(Date uploadDate) {
+		this.uploadDate = uploadDate;
+	}
 
-    @Override
-    public Date getPublishDate() {
-        return publishDate;
-    }
+	@Override
+	public Date getPublishDate() {
+		return publishDate;
+	}
 
-    @Override
-    public void setPublishDate(Date publishDate) {
-        this.publishDate = publishDate;
-    }
+	@Override
+	public void setPublishDate(Date publishDate) {
+		this.publishDate = publishDate;
+	}
 
-    @Override
-    public String getIdentifier() {
-        return identifier;
-    }
+	@Override
+	public String getIdentifier() {
+		return identifier;
+	}
 
-    @Override
-    public void setIdentifier(String identifier) {
-        this.identifier = identifier;
-    }
+	@Override
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
 
-    @Override
-    public Set<QueueItem> getQueueItems() {
-        return queueItems;
-    }
+	@Override
+	public Set<QueueItem> getQueueItems() {
+		return queueItems;
+	}
 
-    @Override
-    public void setQueueItems(Set<QueueItem> queueItems) {
-        this.queueItems = queueItems;
-    }
+	@Override
+	public void setQueueItems(Set<QueueItem> queueItems) {
+		this.queueItems = queueItems;
+	}
 
-    @Override
-    public Set<Warnings> getWarningses() {
-        return warningses;
-    }
+	@Override
+	public Set<Warnings> getWarningses() {
+		return warningses;
+	}
 
-    @Override
-    public void setWarningses(Set<Warnings> warningses) {
-        this.warningses = warningses;
-    }
+	@Override
+	public void setWarningses(Set<Warnings> warningses) {
+		this.warningses = warningses;
+	}
+
+	@Override
+	public String toString() {
+		return this.getClass().getSimpleName() + " - (" + getIdentifier() + "," + getId() + ") ";
+	}
+
 }
