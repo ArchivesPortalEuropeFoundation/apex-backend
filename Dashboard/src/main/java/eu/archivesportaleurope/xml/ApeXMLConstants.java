eab110abf42550c91412b94292bf52f8351efc83
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-11-28 08:40:51 +0000

    IssueID 1622: METS rights
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@4270 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/archivesportaleurope/xml/ApeXMLConstants.java
+++ Dashboard/src/main/java/eu/archivesportaleurope/xml/ApeXMLConstants.java
@@ -10,6 +10,7 @@ public class ApeXMLConstants {
 	public static final String XLINK_NAMESPACE = "http://www.w3.org/1999/xlink";
 	public static final String XSI_NAMESPACE = "http://www.w3.org/2001/XMLSchema-instance";
 	public static final String METS_NAMESPACE = "http://www.loc.gov/METS/";
+	public static final String METS_RIGHTS_NAMESPACE = "http://www.archivesportaleurope.net/Portal/profiles/rights/";
 	public static final String XLINK_PREFIX = "xlink";
 	public static final String XSI_PREFIX = "xsi";
 	public static final String APE_EAD_LOCATION = "http://schemas.archivesportaleurope.net/profiles/apeEAD.xsd";
