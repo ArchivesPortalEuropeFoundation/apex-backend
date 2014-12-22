20636320424b8b235144d2746f485a28271b3c1f
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-12-22 11:54:28 +0000

    IssueID 1793: better preview
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@4370 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/actions/PreviewSecondDisplayAction.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/actions/PreviewSecondDisplayAction.java
@@ -1,38 +1,68 @@
 package eu.apenet.dashboard.actions;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 
-import eu.apenet.commons.seconddisplay.SecondDisplayAction;
 import eu.apenet.commons.types.XmlType;
+import eu.apenet.dashboard.AbstractInstitutionAction;
 import eu.apenet.dashboard.services.eaccpf.EacCpfService;
 import eu.apenet.dashboard.services.ead.EadService;
+import eu.apenet.dashboard.utils.PropertiesKeys;
+import eu.apenet.dashboard.utils.PropertiesUtil;
+import eu.apenet.persistence.factory.DAOFactory;
+import eu.apenet.persistence.vo.Ead;
 
-public class PreviewSecondDisplayAction extends SecondDisplayAction {
+public class PreviewSecondDisplayAction extends AbstractInstitutionAction {
+	private static final Logger LOGGER = Logger.getLogger(PreviewSecondDisplayAction.class);
+	private String id;
+	private String xmlTypeId;
+	
+	public String getId() {
+		return id;
+	}
+
+	public String getXmlTypeId() {
+		return xmlTypeId;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setXmlTypeId(String xmlTypeId) {
+		this.xmlTypeId = xmlTypeId;
+	}
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1953305569537661585L;
 
-	@Override
 	public String execute() throws Exception {
-		dashboard = true;
 		try {
 			XmlType xmlType = XmlType.getType(Integer.parseInt(getXmlTypeId()));
 			if (StringUtils.isNotBlank(getId()) && StringUtils.isNumeric(getId())) {
 				if (xmlType.equals(XmlType.EAC_CPF)){
 					EacCpfService.createPreviewHTML(xmlType, Integer.parseInt(getId()));
-					return super.displayEacCpf();
+					//return super.displayEacCpf();
 				}else{
 					EadService.createPreviewHTML(xmlType, Integer.parseInt(getId()));
+					Ead ead = DAOFactory.instance().getEadDAO().findById(Integer.parseInt(id), xmlType.getClazz());
+					if (ead != null && getAiId() != null && getAiId().equals(ead.getAiId())){
+						getServletRequest().setAttribute("xmlTypeName", xmlType.getResourceName());
+						getServletRequest().setAttribute("identifier", ead.getEncodedIdentifier());
+						getServletRequest().setAttribute("repoCode", ead.getArchivalInstitution().getEncodedRepositorycode());
+						String url = "http://" + PropertiesUtil.get(PropertiesKeys.APE_PORTAL_DOMAIN) + PropertiesUtil.get(PropertiesKeys.APE_PORTAL_EAD_DISPLAY);
+						getServletRequest().setAttribute("url", url);
+					}
 				}
 			}
 		}catch (Exception e){
-			logger.error(getText("previewseconddisplay.unabletopreview") + " (id,xmlType): (" + getId() + "," + getXmlTypeId() + "): " + e.getMessage() ,e);
+			LOGGER.error(getText("previewseconddisplay.unabletopreview") + " (id,xmlType): (" + getId() + "," + getXmlTypeId() + "): " + e.getMessage() ,e);
 			addActionError(getText("error.user.second.display.notindexed"));
 			return ERROR;
 		}
-		return super.execute();
+		return SUCCESS;
 	}
 
 }
