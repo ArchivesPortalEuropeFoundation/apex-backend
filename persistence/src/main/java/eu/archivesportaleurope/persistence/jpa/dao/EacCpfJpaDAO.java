45e8fe3f73046119247cfa577ea698479c3ef117
bastiaan <bastiaan.verhoef.ape@gmail.com>
2014-12-23 10:33:51 +0000

    IssueID  	1815: better preview
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@4409 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/java/eu/archivesportaleurope/persistence/jpa/dao/EacCpfJpaDAO.java
+++ persistence/src/main/java/eu/archivesportaleurope/persistence/jpa/dao/EacCpfJpaDAO.java
@@ -60,12 +60,15 @@ public class EacCpfJpaDAO extends AbstractHibernateDAO<EacCpf, Integer> implemen
 
     @SuppressWarnings("unchecked")
     @Override
-    public EacCpf getEacCpfByIdentifier(String repositorycode, String identifier) {
+    public EacCpf getEacCpfByIdentifier(String repositorycode, String identifier, boolean onlyPublished) {
         Criteria criteria = getSession().createCriteria(EacCpf.class, "eacCpf");
         criteria = criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
         criteria = criteria.createAlias("eacCpf.archivalInstitution", "archivalInstitution");
         criteria.add(Restrictions.eq("archivalInstitution.repositorycode", ApeUtil.decodeRepositoryCode(repositorycode)));
         criteria.add(Restrictions.eq("identifier", ApeUtil.decodeSpecialCharacters(identifier)));
+        if (onlyPublished){
+    		criteria.add(Restrictions.eq("published", true));        	
+        }
         criteria.setMaxResults(1);
         List<EacCpf> list = criteria.list();
         if (list.size() > 0) {
@@ -74,22 +77,6 @@ public class EacCpfJpaDAO extends AbstractHibernateDAO<EacCpf, Integer> implemen
         return null;
     }
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public EacCpf getPublishedEacCpfByIdentifier(String repositorycode, String identifier) {
-        Criteria criteria = getSession().createCriteria(EacCpf.class, "eacCpf");
-        criteria = criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
-        criteria = criteria.createAlias("eacCpf.archivalInstitution", "archivalInstitution");
-        criteria.add(Restrictions.eq("archivalInstitution.repositorycode", ApeUtil.decodeRepositoryCode(repositorycode)));
-        criteria.add(Restrictions.eq("identifier", ApeUtil.decodeSpecialCharacters(identifier)));
-		criteria.add(Restrictions.eq("published", true));
-        criteria.setMaxResults(1);
-        List<EacCpf> list = criteria.list();
-        if (list.size() > 0) {
-            return list.get(0);
-        }
-        return null;
-    }
 
     @Override
     public List<EacCpf> getEacCpfs(ContentSearchOptions contentSearchOptions) {
