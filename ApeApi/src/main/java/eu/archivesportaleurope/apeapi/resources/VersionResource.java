371ed72862e6729925278a551d6b40c0929c7507
sowrov <sowrov@gmail.com>
2015-11-24 11:58:50 +0000

    1st Sample service: version/current
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@4881 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ ApeApi/src/main/java/eu/archivesportaleurope/apeapi/resources/VersionResource.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.archivesportaleurope.apeapi.resources;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author sowrov
+ */
+@Component
+@Path("/version")
+//@Produces({"application/vnd.roz-v1+json"})
+//@Api(value = "/configuration", description = "Simple config service")
+public class VersionResource {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GET
+    @Path("current")
+//    @Consumes({"application/vnd.roz-v1+json"})
+//    @ApiOperation(value = "Return current app version 1")
+    public Response appVersion() {
+        String appVersion = "{\"version\":1.0}";
+        // assume SLF4J is bound to logback in the current environment
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        // print logback's internal status
+        StatusPrinter.print(lc);
+        logger.debug("System name: " + System.getProperty("os.name"));
+        logger.debug("My current version is " + appVersion);
+        return Response.ok(appVersion).build();
+    }
+
+    @GET
+    @Path("previous")
+//    @Consumes({"application/vnd.roz-v2+json"})
+//    @ApiOperation(value = "Return current app version 2")
+    public Response appVersion2() {
+        String appVersion = "{\"version\":0.0}";
+        logger.debug("My current version is " + appVersion);
+        return Response.ok(appVersion).build();
+    }
+}
