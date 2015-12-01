225d2b54e9dd2fd414cea86c31e06da7b3db4f46
smartKaisar <kaisar39@gmail.com>
2015-12-01 08:47:37 +0000

    Test cases and test context.
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@4901 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ ApeApi/src/test/java/JerseySpringTest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Application;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ *
+ * @author M.Mozadded
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:META-INF/applicationContext-test.xml")
+@ActiveProfiles("unit-test")
+@WebAppConfiguration
+public abstract class JerseySpringTest {
+
+    private JerseyTest _jerseyTest;
+
+    public final WebTarget target(final String path) {
+        return _jerseyTest.target(path);
+    }
+
+    @Before
+    public void setupJerseyTest() throws Exception {
+        _jerseyTest.setUp();
+    }
+
+    @After
+    public void tearDownJerseyTest() throws Exception {
+        _jerseyTest.tearDown();
+    }
+
+    @Autowired
+    public void setApplicationContext(final ApplicationContext context) {
+        _jerseyTest = new JerseyTest() {
+            @Override
+            protected Application configure() {
+                ResourceConfig application = JerseySpringTest.this.configure();
+                application.property("contextConfig", context);
+                return application;
+            }
+
+        };
+    }
+
+    protected abstract ResourceConfig configure();
+}
---
+++ ApeApi/src/test/java/VersionResourceTest.java
@@ -0,0 +1,60 @@
+
+import eu.archivesportaleurope.apeapi.resources.VersionResource;
+import javax.ws.rs.core.Response;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.validateMockitoUsage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author kaisar
+ */
+public class VersionResourceTest extends JerseySpringTest {
+
+    final private transient Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Before
+    public void setUpTest() {
+
+    }
+
+    @After
+    public void tearDownTest() {
+        validateMockitoUsage();
+    }
+
+    @Test
+    public void testCurrentAppVersion() {
+        logger.debug("Application current version Version");
+        Response response = super.target("version").path("current").request().header("Content-Type", "application/json").get(Response.class);
+        Assert.assertEquals(response.getStatus(), HttpStatus.OK.value());
+        response.bufferEntity();
+        String appVersion = "{\"version\":1.0}";
+        Assert.assertEquals(appVersion, response.readEntity(String.class));
+    }
+
+    @Test
+    public void testPreviousAppVersion() {
+        logger.debug("Application previous version Version");
+        Response response = super.target("version").path("previous").request().header("Content-Type", "application/json").get(Response.class);
+        String appVersion = "{\"version\":0.0}";
+        Assert.assertEquals(appVersion, response.readEntity(String.class));
+    }
+
+    @Override
+    protected ResourceConfig configure() {
+        return new ResourceConfig(VersionResource.class);
+    }
+
+}
