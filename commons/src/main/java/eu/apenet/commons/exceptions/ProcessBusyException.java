e4e011ae1655974555732a94a9bd2e5c7a69cc34
sowrov <sowrov@gmail.com>
2016-01-19 17:13:57 +0000

    ProcessBusyException
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@4943 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ commons/src/main/java/eu/apenet/commons/exceptions/ProcessBusyException.java
@@ -0,0 +1,21 @@
+package eu.apenet.commons.exceptions;
+
+public class ProcessBusyException extends Exception {
+
+	public ProcessBusyException() {
+		super("Process is busy");
+	}
+
+	public ProcessBusyException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ProcessBusyException(String message) {
+		super(message);
+	}
+
+	public ProcessBusyException(Throwable cause) {
+		super(cause);
+	}
+
+}
