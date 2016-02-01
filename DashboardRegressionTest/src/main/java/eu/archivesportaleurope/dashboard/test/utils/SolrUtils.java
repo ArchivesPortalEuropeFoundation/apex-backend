11e2f4ab9df987923692f8522d536be788ea1722
smartKaisar <kaisar39@gmail.com>
2016-02-01 15:09:14 +0000

    Create Solr Util to clear all the cores.
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@4956 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ DashboardRegressionTest/src/main/java/eu/archivesportaleurope/dashboard/test/utils/SolrUtils.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.archivesportaleurope.dashboard.test.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+
+/**
+ *
+ * @author kaisar
+ */
+public class SolrUtils {
+
+    public static enum Cores {
+
+        EAG("eag"), EAD("ead"), EAC_CPF("eac-cpfs");
+        private final String coreName;
+
+        private Cores(final String coreName) {
+            this.coreName = coreName;
+        }
+
+        @Override
+        public String toString() {
+            return coreName;
+        }
+    }
+
+    private String baseSolrUrl;
+
+    private static final Properties PROPERTIES = new Properties();
+    private static SolrUtils solrUtils;
+
+    private SolrUtils() {
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("config.properties");
+            PROPERTIES.load(is);
+            this.baseSolrUrl = PROPERTIES.getProperty("baseSolrUrl", "http://localhost:8080");
+        } catch (IOException ex) {
+            Logger.getLogger(SolrUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static SolrUtils getSolrUtil() {
+        if (solrUtils == null) {
+            solrUtils = new SolrUtils();
+        }
+        return solrUtils;
+    }
+
+    public String getBaseSolrUrl() {
+        return baseSolrUrl;
+    }
+
+    public void setBaseSolrUrl(String baseSolrUrl) {
+        this.baseSolrUrl = baseSolrUrl;
+    }
+
+    public void clearAllCore() {
+        for (Cores c : Cores.values()) {
+            clearCore(c.toString());
+        }
+    }
+
+    public void clearCore(String coreName) {
+        HttpSolrServer solr = new HttpSolrServer(this.getBaseSolrUrl() + "/" + coreName);
+        try {
+            solr.deleteByQuery("*:*");
+        } catch (SolrServerException | IOException ex) {
+            Logger.getLogger(SolrUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return getSolrUtil();
+    }
+
+}
