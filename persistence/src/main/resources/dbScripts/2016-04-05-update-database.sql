564b23451a3deaf3bdcae92a331b98c8ebd57054
sowrov <sowrov@gmail.com>
2016-04-05 12:58:11 +0000

    multiple opendataenabled removed
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-2.2@5060 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/resources/dbScripts/2016-04-05-update-database.sql
+++ persistence/src/main/resources/dbScripts/2016-04-05-update-database.sql
@@ -1,5 +1,4 @@
 ALTER TABLE archival_institution ADD COLUMN "openDataEnabled" boolean DEFAULT false;
-ALTER TABLE archival_institution ADD COLUMN opendataenabled boolean;
 ALTER TABLE archival_institution ADD COLUMN totalsolrdocsforopendata bigint;
 ALTER TABLE archival_institution ADD COLUMN unprocessedsolrdocs bigint;
 
