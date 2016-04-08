441e579413585bb3ea834b2846f784f7f3be2959
stefanpapp-ape <stefan.papp.ape@gmail.com>
2016-04-08 12:25:12 +0000

    update SQL diff script with missing sequence
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/branches/backend-project-2.2@5077 9fd842fd-667d-44da-918d-ff91420b3ae1

--- persistence/src/main/resources/dbScripts/2016-04-05-update-database.sql
+++ persistence/src/main/resources/dbScripts/2016-04-05-update-database.sql
@@ -2,3 +2,11 @@ ALTER TABLE archival_institution ADD COLUMN "openDataEnabled" boolean DEFAULT fa
 ALTER TABLE archival_institution ADD COLUMN totalsolrdocsforopendata bigint;
 ALTER TABLE archival_institution ADD COLUMN unprocessedsolrdocs bigint;
 
+CREATE SEQUENCE pk_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE pk_seq
+  OWNER TO apenet_dashboard;
