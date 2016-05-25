aadb9749e12e6e18fa3b2982dcc63361fb29d5b5
sowrov <sowrov@gmail.com>
2016-05-25 12:49:38 +0000

    refactor common codes
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@5146 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ commons/src/main/java/eu/apenet/commons/solr/facet/ListFacetContainer.java
@@ -0,0 +1,46 @@
+package eu.apenet.commons.solr.facet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.solr.client.solrj.response.FacetField;
+import org.apache.solr.client.solrj.response.FacetField.Count;
+
+import eu.apenet.commons.ResourceBundleSource;
+
+public class ListFacetContainer {
+	private List<FacetValue> values = new ArrayList<FacetValue>();
+	private ListFacetSettings facetSettings;
+	public ListFacetContainer(FacetField facetField, ListFacetSettings facetSettings, List<String> selectedItems,ResourceBundleSource resourceBundleSource) {
+		super();
+		this.facetSettings = facetSettings;
+		boolean multiSelect = facetSettings.getFacetType().isMultiSelect();
+		for (Count count: facetField.getValues()){
+			// Display refinement only if it is multi select or if the number of result is higher than 0
+			if (multiSelect || count.getCount() > 0){
+				values.add(new FacetValue(facetField, count, facetSettings.getFacetType(), selectedItems,resourceBundleSource));
+			}
+		}
+	}
+	public ListFacetSettings getFacetSettings() {
+		return facetSettings;
+	}
+	public String getName(){
+		return facetSettings.getFacetType().getName();
+	}
+	public boolean isMultiSelect(){
+		return facetSettings.getFacetType().isMultiSelect();
+	}
+	public int getLimit(){
+		return facetSettings.getLimit();
+	}
+	public boolean isExpanded(){
+		return facetSettings.isExpanded();
+	}
+	public List<FacetValue> getValues() {
+		return values;
+	}
+	
+
+	
+}
