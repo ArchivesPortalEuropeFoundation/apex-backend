279edcee5f06b87be6fd37a2315af3fd04fcd1fb
smartKaisar <kaisar39@gmail.com>
2016-06-02 07:53:26 +0000

    Add test case to increase code coverage.
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@5159 9fd842fd-667d-44da-918d-ff91420b3ae1

--- ApeApi/src/test/java/eu/archivesportaleurope/apeapi/resources/ArchivalInstituteStatResourceWithMockTest.java
+++ ApeApi/src/test/java/eu/archivesportaleurope/apeapi/resources/ArchivalInstituteStatResourceWithMockTest.java
@@ -7,11 +7,8 @@ package eu.archivesportaleurope.apeapi.resources;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 import eu.archivesportaleurope.apeapi.common.datatypes.ServerConstants;
-import eu.archivesportaleurope.apeapi.exceptions.ViolationException;
-import eu.archivesportaleurope.apeapi.jersey.JerseySpringWithSecurityTest;
-import eu.archivesportaleurope.apeapi.response.ArchivalInstitutesResponse;
+import eu.archivesportaleurope.apeapi.jersey.JerseySpringTest;
 import eu.archivesportaleurope.apeapi.response.utils.JsonDateDeserializer;
 import eu.archivesportaleurope.apeapi.services.AiStatService;
 import java.util.Date;
@@ -20,28 +17,27 @@ import javax.ws.rs.core.Response;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mockito;
+import static org.mockito.Mockito.validateMockitoUsage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 
 /**
  *
  * @author kaisar
  */
-public class ArchivalInstituteStatResourceTest extends JerseySpringWithSecurityTest {
+public class ArchivalInstituteStatResourceWithMockTest extends JerseySpringTest {
 
     final private transient Logger logger = LoggerFactory.getLogger(this.getClass());
     private Gson gson;
     @Autowired
     private AiStatService aiStatService;
 
-    public ArchivalInstituteStatResourceTest() {
+    public ArchivalInstituteStatResourceWithMockTest() {
     }
 
     @BeforeClass
@@ -50,10 +46,12 @@ public class ArchivalInstituteStatResourceTest extends JerseySpringWithSecurityT
 
     @AfterClass
     public static void tearDownClass() {
+        validateMockitoUsage();
     }
 
     @Before
     public void setUp() {
+        Mockito.reset(aiStatService);
         gson = new GsonBuilder().serializeNulls().registerTypeAdapter(Date.class, new JsonDateDeserializer()).create();
     }
 
@@ -65,59 +63,35 @@ public class ArchivalInstituteStatResourceTest extends JerseySpringWithSecurityT
      * Test of getInsByOpenData method, of class ArchivalInstituteStatResource.
      */
     @Test
-    public void testGetInsByOpenData() {
-        logger.debug(":::Test Get Institute that has open data enabled");
-        int start = 0;
-        int count = 2;
-
-        Response response = super.target("institute").path("getInstitute").path(String.valueOf(start))
+    public void testWebApplicationException() {
+        logger.debug("Test invalid request with Web application exception");
+        int startIndex = 1;
+        int count = 5;
+        Mockito.when(aiStatService.getAiWithOpenDataEnabled(startIndex, count)).thenThrow(new WebApplicationException("Exception throw success"));
+        Response response = super.target("institute").path("getInstitute").path(String.valueOf(startIndex))
                 .path(String.valueOf(count)).request().header("APIkey", "myApiKeyXXXX123456789").header("Content-Type", ServerConstants.APE_API_V1).accept(ServerConstants.APE_API_V1).get();
         response.bufferEntity();
         String jsonResponse = response.readEntity(String.class);
         logger.debug("Response Json: " + jsonResponse);
-
-        TypeToken<ArchivalInstitutesResponse> token = new TypeToken<ArchivalInstitutesResponse>() {
-        };
-        ArchivalInstitutesResponse ais = gson.fromJson(jsonResponse, token.getType());
-        Assert.assertEquals(2, ais.getInstitutes().size());
-        Assert.assertEquals(3, ais.getTotal());
     }
 
     @Test
-    public void testWithStartLessThanZero() {
-        logger.debug(":::Test Get Institute that has open data enabled with start less than zero");
-        int start = -1;
-        int count = 2;
-        Response response = super.target("institute").path("getInstitute").path(String.valueOf(start))
+    public void testException() throws InterruptedException {
+        logger.debug("Test invalid request with  exception");
+        int startIndex = 1;
+        int count = 5;
+        Thread.sleep(1000);
+        Mockito.when(aiStatService.getAiWithOpenDataEnabled(startIndex, count)).thenThrow(new NullPointerException("Intentional Exception"));
+        Thread.sleep(1000);
+        Response response = super.target("institute").path("getInstitute").path(String.valueOf(startIndex))
                 .path(String.valueOf(count)).request().header("APIkey", "myApiKeyXXXX123456789").header("Content-Type", ServerConstants.APE_API_V1).accept(ServerConstants.APE_API_V1).get();
         response.bufferEntity();
         String jsonResponse = response.readEntity(String.class);
         logger.debug("Response Json: " + jsonResponse);
-
-        TypeToken<ArchivalInstitutesResponse> token = new TypeToken<ArchivalInstitutesResponse>() {
-        };
-        ArchivalInstitutesResponse ais = gson.fromJson(jsonResponse, token.getType());
-        Assert.assertEquals(2, ais.getInstitutes().size());
-        Assert.assertEquals(3, ais.getTotal());
     }
 
-    @Test
-    public void testInvalidRequest() {
-        logger.debug("Test invalid request with count > 50");
-        int start = 1;
-        int count = 51;
-        Response response = super.target("institute").path("getInstitute").path(String.valueOf(start))
-                .path(String.valueOf(count)).request().header("APIkey", "myApiKeyXXXX123456789").header("Content-Type", ServerConstants.APE_API_V1).accept(ServerConstants.APE_API_V1).get();
-        response.bufferEntity();
-        String jsonResponse = response.readEntity(String.class);
-        logger.debug("Response Json: " + jsonResponse);
-
-        Assert.assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatus());
-    }
-
-    @Override
+    @Override()
     protected ResourceConfig configure() {
         return new ResourceConfig(ArchivalInstituteStatResource.class);
     }
-
 }
