ffae02615144e41caf33edc1d7c58d2d2d49657d
smartKaisar <kaisar39@gmail.com>
2016-06-07 12:16:08 +0000

    Get ins data from solr instead of DB.
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@5171 9fd842fd-667d-44da-918d-ff91420b3ae1

--- ApeApi/src/main/java/eu/archivesportaleurope/apeapi/response/ArchivalInstitutesResponse.java
+++ ApeApi/src/main/java/eu/archivesportaleurope/apeapi/response/ArchivalInstitutesResponse.java
@@ -16,46 +16,47 @@ import org.apache.solr.client.solrj.response.QueryResponse;
  * @author kaisar
  */
 public class ArchivalInstitutesResponse {
-
+    
     private long total;
     private List<ArchivalInstituteResponse> institutes;
-
+    
     public ArchivalInstitutesResponse() {
         this.institutes = new ArrayList<>();
     }
-
+    
     public ArchivalInstitutesResponse(List<ArchivalInstitution> ais) {
         this();
         for (ArchivalInstitution ai : ais) {
             this.addInstitutes(new ArchivalInstituteResponse(ai));
         }
     }
-
+    
     public ArchivalInstitutesResponse(QueryResponse response) {
         this();
+        this.setTotal(response.getGroupResponse().getValues().get(0).getValues().size());
         for (Group group : response.getGroupResponse().getValues().get(0).getValues()) {
             this.addInstitutes(new ArchivalInstituteResponse(group));
         }
     }
-
+    
     public long getTotal() {
         return total;
     }
-
-    public void setTotal(long total) {
+    
+    public final void setTotal(long total) {
         this.total = total;
     }
-
+    
     public List<ArchivalInstituteResponse> getInstitutes() {
         return institutes;
     }
-
+    
     public void setInstitutes(List<ArchivalInstituteResponse> institutes) {
         this.institutes = institutes;
     }
-
+    
     public final void addInstitutes(ArchivalInstituteResponse ai) {
         this.institutes.add(ai);
     }
-
+    
 }
