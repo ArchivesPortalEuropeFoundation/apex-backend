e8f35546b2ab0e2eac818b86543f3a3004c69eff
sowrov <sowrov@gmail.com>
2016-06-13 13:33:05 +0000

    fix spring security issue in content service
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@5178 9fd842fd-667d-44da-918d-ff91420b3ae1

--- ApeApi/src/main/java/eu/archivesportaleurope/apeapi/resources/ContentResource.java
+++ ApeApi/src/main/java/eu/archivesportaleurope/apeapi/resources/ContentResource.java
@@ -22,20 +22,16 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.InputStream;
 import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.container.ResourceContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
-import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -45,12 +41,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author mahbub
  */
-@Provider
 @Component
 @Path("/content")
 @Api("/content")
@@ -60,7 +56,7 @@ public class ContentResource {
     @Autowired
     private EadContentService eadContentService;
     
-    @Context
+    @Autowired
     private ServletContext servletContext;
     
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
