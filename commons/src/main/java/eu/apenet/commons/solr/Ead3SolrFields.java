25fe01df77622f22b67268a740c0400d2011a87a
sowrov <sowrov@gmail.com>
2016-11-17 11:12:58 +0000

    ead3 solr fields
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@5409 9fd842fd-667d-44da-918d-ff91420b3ae1

--- commons/src/main/java/eu/apenet/commons/solr/Ead3SolrFields.java
+++ commons/src/main/java/eu/apenet/commons/solr/Ead3SolrFields.java
@@ -10,49 +10,52 @@ package eu.apenet.commons.solr;
  * @author mahbub
  */
 public final class Ead3SolrFields {
-    public final static String ID = "id";
-    public final static String UNIT_TITLE = "unitTitle";
-    public final static String SCOPE_CONTENT = "scopeContent";
-    public final static String OTHER = "other";
-    public final static String UNIT_DATE = "unitDate";
-    public final static String UNIT_ID = "unitId";
-    public final static String OTHER_UNIT_ID = "otherUnitId";
-    public final static String TITLE_PROPER = "titleProper";
-    public final static String COUNTRY_ID = "countryId";
-    public final static String COUNTRY_NAME = "countryName";
-    public final static String AI_ID = "aiId";
-    public final static String AI_NAME = "aiName";
-    public final static String REPOSITORY_CODE = "repositoryCode";
-    public final static String NUMBER_OF_DECENDENTS = "numberOfDecendents";
-    public final static String NUMBER_OF_ANCESTORS = "numberOfAncestors";
+    public final static String ID = "id"; //indexed+stored - unique
+    public final static String UNIT_TITLE = "unitTitle"; //indexed+stored - 
+    public final static String SCOPE_CONTENT = "scopeContent"; //indexed+stored - 
+    public final static String OTHER = "other";  //indexed+stored - other fields valaues, excluding unitTitle, scopeContent, titleProper, etc already stored data fields
+    public final static String UNIT_DATE = "unitDate"; //indexed+stored - 
+    public final static String UNIT_ID = "unitId"; //indexed+stored - 
+    public final static String OTHER_UNIT_ID = "otherUnitId"; //indexed+stored - array
+    public final static String TITLE_PROPER = "titleProper"; //indexed+stored - 
+    public final static String COUNTRY_ID = "countryId"; //indexed+stored - 
+    public final static String COUNTRY_NAME = "countryName"; //indexed+stored - 
+    public final static String AI_ID = "aiId"; //indexed+stored - 
+    public final static String AI_NAME = "aiName"; //indexed+stored - 
+    public final static String REPOSITORY_CODE = "repositoryCode"; //indexed+stored - 
+    public final static String NUMBER_OF_DESCENDENTS = "numberOfDescendents"; //stored - 
+    public final static String NUMBER_OF_ANCESTORS = "numberOfAncestors"; //stored - 
     
-    public final static String LEVEL_NAME = "levelName"; //archdes or clevel
-    public final static String START_DATE = "startDate";
-    public final static String END_DATE = "endDate";
-    public final static String DATE_TYPE = "dateType"; //normal or noDate or otherDate
+    public final static String LEVEL_NAME = "levelName";  //indexed+stored - archdes, clevel
+    public final static String START_DATE = "startDate"; //indexed+stored - 
+    public final static String END_DATE = "endDate"; //indexed+stored - 
+    public final static String DATE_TYPE = "dateType"; //indexed+stored - normal, noDate, otherDate
     
-    public final static String LANGUAGE = "language";
-    public final static String LANG_MATERIAL = "langMaterial";
-    public final static String RECORD_ID = "recordId";
-    public final static String RECORD_TYPE = "recordType"; //fa, sg, hg - generated
+    public final static String LANGUAGE = "language"; //indexed+stored - 
+    public final static String LANG_MATERIAL = "langMaterial"; //stored - 
+    public final static String RECORD_ID = "recordId"; //indexed+stored - 
+    public final static String RECORD_TYPE = "recordType"; //indexed+stored - fa, sg, hg - generated
     
-    public final static String DAO = "dao";
-    public final static String NUMBER_OF_DAO = "numberOfDao";
-    public final static String DAO_LINKS = "daoLinks";
-    public final static String DAO_TYPE = "daoType";
-    public final static String TIMESTAMP = "timestamp";
+    public final static String DAO = "dao";//indexed+stored - true, false
+    public final static String NUMBER_OF_DAO = "numberOfDao";//stored
+    public final static String DAO_LINKS = "daoLinks"; //stored - array of objects, object = link and clevel's Id
+    public final static String DAO_TYPE = "daoType";//indexed+stored
+    public final static String TIMESTAMP = "timestamp";//indexed+stored
     
-    public final static String TOPIC = "topic";
-    public final static String PARENT_ID = "parentId";
-    public final static String PARENT_UNIT_ID = "parentUnitId";
-    public final static String DUPLICATE_UNIT_ID = "duplicateParentId";
-    public final static String SIBLING_POSITION = "siblingPosition";
-    public final static String OPEN_DATA = "openData";
-    public final static String SPELL = "spell";
-    public final static String OPEN_DATA_SPELL = "openDataSpell";
+    public final static String TOPIC = "topic";//indexed+stored
+    public final static String PARENT_ID = "parentId";//indexed+stored
+    public final static String PARENT_UNIT_ID = "parentUnitId";//indexed+stored
+    public final static String DUPLICATE_UNIT_ID = "duplicateParentId";//stored
+    public final static String SIBLING_POSITION = "siblingPosition";//stored
+    public final static String OPEN_DATA = "openData";//indexed+stored
     
+    public final static String SPELL = "spell";//indexed+stored
+    public final static String OPEN_DATA_SPELL = "openDataSpell"; //indexed+stored
     
-    public final static String SORTABLE_RECORD_ID = "unitRecordIDSort";
-    public final static String SORTABLE_UNIT_ID = "unitIdSort";
-    public final static String SORTABLE_UNIT_TITLE = "unitTitleSort";
+    public final static String DYNAMIC_STRING_SUFFIX = "_s";
+    public final static String DYNAMIC_NUMBER_SUFFIX = "_i";
+    
+    public final static String SORTABLE_RECORD_ID = "unitRecordIDSort"; //indexed
+    public final static String SORTABLE_UNIT_ID = "unitIdSort"; //indexed
+    public final static String SORTABLE_UNIT_TITLE = "unitTitleSort"; //indexed
 }
