143d33061fb5b0a106860da86458884f91efa1b6
sowrov <sowrov@gmail.com>
2016-11-21 14:28:59 +0000

    ead3 in progress
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@5417 9fd842fd-667d-44da-918d-ff91420b3ae1

--- commons/src/main/java/eu/apenet/commons/solr/Ead3SolrFields.java
+++ commons/src/main/java/eu/apenet/commons/solr/Ead3SolrFields.java
@@ -11,18 +11,28 @@ package eu.apenet.commons.solr;
  */
 public final class Ead3SolrFields {
     public final static String ID = "id"; //indexed+stored - unique
-    public final static String UNIT_TITLE = "unitTitle"; //indexed+stored - 
-    public final static String SCOPE_CONTENT = "scopeContent"; //indexed+stored - 
-    public final static String OTHER = "other";  //indexed+stored - other fields valaues, excluding unitTitle, scopeContent, titleProper, etc already stored data fields
-    public final static String UNIT_DATE = "unitDate"; //indexed+stored - 
+    public final static String PARENT_ID = "parentId";//indexed+stored -
+    
+    public final static String TITLE_PROPER = "titleProper"; //indexed+stored - global
+    public final static String LANGUAGE = "language"; //indexed+stored - global
+    public final static String LANG_MATERIAL = "langMaterial"; //stored - global
+    public final static String RECORD_ID = "recordId"; //indexed+stored - global
+    public final static String RECORD_TYPE = "recordType"; //indexed+stored - fa, sg, hg - generated
+    public final static String PARENT_UNIT_ID = "parentUnitId";//indexed+stored - global
+    
     public final static String UNIT_ID = "unitId"; //indexed+stored - 
     public final static String OTHER_UNIT_ID = "otherUnitId"; //indexed+stored - array
-    public final static String TITLE_PROPER = "titleProper"; //indexed+stored - 
+    
+    public final static String UNIT_TITLE = "unitTitle"; //indexed+stored - 
+    public final static String UNIT_DATE = "unitDate"; //indexed+stored - 
+    public final static String SCOPE_CONTENT = "scopeContent"; //indexed+stored - 
+    public final static String OTHER = "other";  //indexed+stored - other fields valaues, excluding unitTitle, scopeContent, titleProper, etc already stored data fields
+    
     public final static String COUNTRY_ID = "countryId"; //indexed+stored - 
     public final static String COUNTRY_NAME = "countryName"; //indexed+stored - 
-    public final static String AI_ID = "aiId"; //indexed+stored - 
-    public final static String AI_NAME = "aiName"; //indexed+stored - 
-    public final static String REPOSITORY_CODE = "repositoryCode"; //indexed+stored - 
+    public final static String AI_ID = "aiId"; //indexed+stored - db value
+    public final static String AI_NAME = "aiName"; //indexed+stored - db value
+    public final static String REPOSITORY_CODE = "repositoryCode"; //indexed+stored - db value
     public final static String NUMBER_OF_DESCENDENTS = "numberOfDescendents"; //stored - 
     public final static String NUMBER_OF_ANCESTORS = "numberOfAncestors"; //stored - 
     
@@ -31,11 +41,6 @@ public final class Ead3SolrFields {
     public final static String END_DATE = "endDate"; //indexed+stored - 
     public final static String DATE_TYPE = "dateType"; //indexed+stored - normal, noDate, otherDate
     
-    public final static String LANGUAGE = "language"; //indexed+stored - 
-    public final static String LANG_MATERIAL = "langMaterial"; //stored - 
-    public final static String RECORD_ID = "recordId"; //indexed+stored - 
-    public final static String RECORD_TYPE = "recordType"; //indexed+stored - fa, sg, hg - generated
-    
     public final static String DAO = "dao";//indexed+stored - true, false
     public final static String NUMBER_OF_DAO = "numberOfDao";//stored
     public final static String DAO_LINKS = "daoLinks"; //stored - array of objects, object = link and clevel's Id
@@ -43,9 +48,7 @@ public final class Ead3SolrFields {
     public final static String TIMESTAMP = "timestamp";//indexed+stored
     
     public final static String TOPIC = "topic";//indexed+stored
-    public final static String PARENT_ID = "parentId";//indexed+stored
-    public final static String PARENT_UNIT_ID = "parentUnitId";//indexed+stored
-    public final static String DUPLICATE_UNIT_ID = "duplicateParentId";//stored
+    public final static String DUPLICATE_UNIT_ID = "duplicateUnitId";//stored
     public final static String SIBLING_POSITION = "siblingPosition";//stored
     public final static String OPEN_DATA = "openData";//indexed+stored
     
