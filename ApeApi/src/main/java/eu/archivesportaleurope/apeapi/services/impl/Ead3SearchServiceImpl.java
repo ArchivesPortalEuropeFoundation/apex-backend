035fd67478021f6642fc7dbd338d5303626b9086
smartKaisar <kaisar39@gmail.com>
2016-12-20 10:57:39 +0000

    Add facet for Ead3
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@5459 9fd842fd-667d-44da-918d-ff91420b3ae1

--- ApeApi/src/main/java/eu/archivesportaleurope/apeapi/services/impl/Ead3SearchServiceImpl.java
+++ ApeApi/src/main/java/eu/archivesportaleurope/apeapi/services/impl/Ead3SearchServiceImpl.java
@@ -8,23 +8,12 @@ package eu.archivesportaleurope.apeapi.services.impl;
 import eu.apenet.commons.solr.SolrQueryBuilder;
 import eu.apenet.commons.solr.facet.FacetType;
 import eu.apenet.commons.solr.facet.ListFacetSettings;
-import eu.archivesportaleurope.apeapi.common.datatypes.ServerResponseDictionary;
-import eu.archivesportaleurope.apeapi.exceptions.InternalErrorException;
-import eu.archivesportaleurope.apeapi.request.DateFilterRequest;
-import eu.archivesportaleurope.apeapi.request.SearchFilterRequest;
 import eu.archivesportaleurope.apeapi.request.SearchRequest;
-import eu.archivesportaleurope.apeapi.request.SortRequest;
-import eu.archivesportaleurope.apeapi.response.common.SortFields;
 import eu.archivesportaleurope.apeapi.response.utils.PropertiesUtil;
 import eu.archivesportaleurope.apeapi.services.Ead3SearchService;
 import eu.archivesportaleurope.apeapi.utils.SolrSearchUtil;
-import java.text.ParseException;
 import java.util.List;
-import java.util.Map;
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServer;
-import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -76,81 +65,80 @@ public class Ead3SearchServiceImpl extends Ead3SearchService {
         return this.search(request, extraSearchParam, facetSettingsList, this.propertiesUtil, this.searchUtil);
     }
 
-    @Override
-    protected QueryResponse search(SearchRequest searchRequest, String extraSearchParam,
-            List<ListFacetSettings> facetSettingsList, PropertiesUtil propertiesUtil, SolrSearchUtil eadSearchUtil) {
-        try {
-
-            SolrQuery query = this.getFacatedQuery(searchRequest, facetSettingsList);
-
-            if (extraSearchParam != null && !"".equals(extraSearchParam)) {
-                query.setQuery(searchRequest.getQuery() + " AND " + extraSearchParam);
-            } else {
-                query.setQuery(searchRequest.getQuery());
-            }
-
-            if (searchRequest.getCount() <= 0) {
-                logger.info(":::Default Count vale from prop is : " + propertiesUtil.getValueFromKey("search.request.default.count"));
-                query.setRows(Integer.parseInt(propertiesUtil.getValueFromKey("search.request.default.count")));
-            } else {
-                query.setRows(searchRequest.getCount());
-            }
-
-            //openData - true or false should be managed by the query
-            assert (query.getQuery().contains("openData") == true);
-
-            logger.debug("Final search query: " + query);
-            eadSearchUtil.setQuery(query);
-
-            return eadSearchUtil.getSearchResponse();
-        } catch (SolrServerException | ParseException ex) {
-            throw new InternalErrorException("Solarserver Exception", ExceptionUtils.getStackTrace(ex));
-        }
-    }
-
-    private SolrQuery getFacatedQuery(SearchRequest searchRequest,
-            List<ListFacetSettings> facetSettingsList) throws SolrServerException, ParseException {
-        SolrQuery query = queryBuilder.getEad3ListViewQuery(searchRequest.getStartIndex(), facetSettingsList, null, null, null, true);
-
-        for (SearchFilterRequest searchFilter : searchRequest.getFilters()) {
-            queryBuilder.addFilters(query,
-                    FacetType.getFacetByName(ServerResponseDictionary.getSolrFieldName(searchFilter.getFacetFieldName())),
-                    searchFilter.getFacetFieldIds());
-        }
-
-        for (DateFilterRequest dateFilter : searchRequest.getDateFilters()) {
-            if (dateFilter.getDateFieldName().equalsIgnoreCase("fromDate")) {
-                queryBuilder.addFromDateFilter(query, dateFilter.getDateFieldId());
-            } else if (dateFilter.getDateFieldName().equalsIgnoreCase("toDate")) {
-                queryBuilder.addToDateFilter(query, dateFilter.getDateFieldId());
-            }
-        }
-
-        SortRequest sortFilterRequest = searchRequest.getSortRequest();
-        SolrQuery.ORDER order = SolrQuery.ORDER.asc;
-        if (sortFilterRequest != null) {
-            if ("desc".equals(sortFilterRequest.getSortType())) {
-                order = SolrQuery.ORDER.desc;
-            }
-            if (sortFilterRequest.getFields() != null) {
-                //ToDo: For every search type, sort fileds could be different
-                //If we don't want exception because sort field was not found in specific type
-                //this should be fixed
-                Map<String, String> sortFieldMap = new SortFields().getSolrSortFieldMap();
-                for (String field : sortFilterRequest.getFields()) {
-                    String solrSortField = sortFieldMap.get(field);
-                    if (solrSortField != null) {
-                        query.addSort(solrSortField, order);
-                    }
-                }
-            }
-        }
-        return query;
-    }
-
+//    @Override
+//    protected QueryResponse search(SearchRequest searchRequest, String extraSearchParam,
+//            List<ListFacetSettings> facetSettingsList, PropertiesUtil propertiesUtil, SolrSearchUtil eadSearchUtil) {
+//        try {
+//
+//            SolrQuery query = this.getFacatedQuery(searchRequest, facetSettingsList);
+//
+//            if (extraSearchParam != null && !"".equals(extraSearchParam)) {
+//                query.setQuery(searchRequest.getQuery() + " AND " + extraSearchParam);
+//            } else {
+//                query.setQuery(searchRequest.getQuery());
+//            }
+//
+//            if (searchRequest.getCount() <= 0) {
+//                logger.info(":::Default Count vale from prop is : " + propertiesUtil.getValueFromKey("search.request.default.count"));
+//                query.setRows(Integer.parseInt(propertiesUtil.getValueFromKey("search.request.default.count")));
+//            } else {
+//                query.setRows(searchRequest.getCount());
+//            }
+//
+//            //openData - true or false should be managed by the query
+//            assert (query.getQuery().contains("openData") == true);
+//
+//            logger.debug("Final search query: " + query);
+//            eadSearchUtil.setQuery(query);
+//
+//            return eadSearchUtil.getSearchResponse();
+//        } catch (SolrServerException | ParseException ex) {
+//            throw new InternalErrorException("Solarserver Exception", ExceptionUtils.getStackTrace(ex));
+//        }
+//    }
+//
+//    private SolrQuery getFacatedQuery(SearchRequest searchRequest,
+//            List<ListFacetSettings> facetSettingsList) throws SolrServerException, ParseException {
+//        SolrQuery query = queryBuilder.getEad3ListViewQuery(searchRequest.getStartIndex(), facetSettingsList, null, null, null, true);
+//
+//        for (SearchFilterRequest searchFilter : searchRequest.getFilters()) {
+//            queryBuilder.addFilters(query,
+//                    FacetType.getFacetByName(ServerResponseDictionary.getSolrFieldName(searchFilter.getFacetFieldName())),
+//                    searchFilter.getFacetFieldIds());
+//        }
+//
+//        for (DateFilterRequest dateFilter : searchRequest.getDateFilters()) {
+//            if (dateFilter.getDateFieldName().equalsIgnoreCase("fromDate")) {
+//                queryBuilder.addFromDateFilter(query, dateFilter.getDateFieldId());
+//            } else if (dateFilter.getDateFieldName().equalsIgnoreCase("toDate")) {
+//                queryBuilder.addToDateFilter(query, dateFilter.getDateFieldId());
+//            }
+//        }
+//
+//        SortRequest sortFilterRequest = searchRequest.getSortRequest();
+//        SolrQuery.ORDER order = SolrQuery.ORDER.asc;
+//        if (sortFilterRequest != null) {
+//            if ("desc".equals(sortFilterRequest.getSortType())) {
+//                order = SolrQuery.ORDER.desc;
+//            }
+//            if (sortFilterRequest.getFields() != null) {
+//                //ToDo: For every search type, sort fileds could be different
+//                //If we don't want exception because sort field was not found in specific type
+//                //this should be fixed
+//                Map<String, String> sortFieldMap = new SortFields().getSolrSortFieldMap();
+//                for (String field : sortFilterRequest.getFields()) {
+//                    String solrSortField = sortFieldMap.get(field);
+//                    if (solrSortField != null) {
+//                        query.addSort(solrSortField, order);
+//                    }
+//                }
+//            }
+//        }
+//        return query;
+//    }
     @Override
     public QueryResponse searchOpenData(SearchRequest request) {
-        return this.search(request, this.onlyOpenData, true);
+        return this.search(request, this.onlyOpenData, false);
     }
 
 }
