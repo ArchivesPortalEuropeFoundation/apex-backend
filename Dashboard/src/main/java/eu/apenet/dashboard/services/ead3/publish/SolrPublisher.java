9cc06298957b7e1b6aefafb35b87226f71457f0c
sowrov <sowrov@gmail.com>
2017-01-25 13:06:46 +0000

    ead3 delete from queue
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@5468 9fd842fd-667d-44da-918d-ff91420b3ae1

--- Dashboard/src/main/java/eu/apenet/dashboard/services/ead3/publish/SolrPublisher.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/services/ead3/publish/SolrPublisher.java
@@ -27,11 +27,6 @@ public class SolrPublisher {
         List<SolrInputDocument> solrDocList = new ArrayList<>();
         while (docIterator.hasNext()) {
             SolrDocNode node = docIterator.next();
-//            if (!first) {
-//                System.out.println(",");
-//            }
-//            System.out.println(new Gson().toJson(node));
-//            first=false;
             Map<String, Object> nodeMap = node.getData();
             SolrInputDocument solrDoc = new SolrInputDocument();
 
@@ -49,7 +44,6 @@ public class SolrPublisher {
             SolrDocNode node = docIterator.next();
             System.out.println(new Gson().toJson(node));
         }
-        System.out.println("]}");
     }
 
     public void hardCommit() throws SolrServerException {
