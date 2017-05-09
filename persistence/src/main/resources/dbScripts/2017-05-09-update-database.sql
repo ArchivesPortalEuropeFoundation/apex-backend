57d56f2035f40b0e48279cba779c17b05b514cfe
stefanpapp-ape <stefan.papp.ape@gmail.com>
2017-05-09 09:11:46 +0000

    new SQL update script containing new EAD3 and reindexation tables and a few related tweaks
    git-svn-id: https://build.archivesportaleurope.net/svn/apex/trunk@5521 9fd842fd-667d-44da-918d-ff91420b3ae1

---
+++ persistence/src/main/resources/dbScripts/2017-05-09-update-database.sql
@@ -0,0 +1,140 @@
+--
+-- Name: ead3; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE ead3 (
+    id integer NOT NULL,
+    ai_id integer,
+    converted boolean NOT NULL,
+    europeana integer,
+    identifier character varying(255) NOT NULL,
+    path character varying(255),
+    publish_date date,
+    published boolean NOT NULL,
+    queuing integer,
+    title character varying(255),
+    upload_date timestamp without time zone,
+    validated integer,
+    um_id integer,
+    totalnumberofdaos bigint,
+    totalnumberofunits bigint
+);
+
+
+ALTER TABLE public.ead3 OWNER TO postgres;
+
+--
+-- Name: ead3_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE ead3_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.ead3_id_seq OWNER TO postgres;
+
+--
+-- Name: ead3_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE ead3_id_seq OWNED BY ead3.id;
+
+
+--
+-- Add new column for EAD3 to queue
+--
+
+ALTER TABLE queue
+   ADD COLUMN ead3_id integer;
+
+
+--
+-- Name: reindex_doc; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE reindex_doc (
+    id integer NOT NULL,
+    ai_id integer,
+    archivalinstitution bytea,
+    holdingsguide bytea,
+    eac_cpf_id integer,
+    ead3_id integer,
+    fa_id integer,
+    sg_id integer
+);
+
+
+ALTER TABLE public.reindex_doc OWNER TO postgres;
+
+--
+-- Name: reindex_doc_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE reindex_doc_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.reindex_doc_id_seq OWNER TO postgres;
+
+--
+-- Name: reindex_doc_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE reindex_doc_id_seq OWNED BY reindex_doc.id;
+
+
+--
+-- Add new column for EAD3 to warnings
+--
+
+ALTER TABLE warnings
+   ADD COLUMN ead3_id integer;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY reindex_doc ALTER COLUMN id SET DEFAULT nextval('reindex_doc_id_seq'::regclass);
+
+
+--
+-- Name: ead3_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY ead3
+    ADD CONSTRAINT ead3_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: reindex_doc_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY reindex_doc
+    ADD CONSTRAINT reindex_doc_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: ead3_ai_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY ead3
+    ADD CONSTRAINT ead3_ai_id_fkey FOREIGN KEY (ai_id) REFERENCES archival_institution(id);
+
+
+--
+-- Name: ead3_um_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY ead3
+    ADD CONSTRAINT ead3_um_id_fkey FOREIGN KEY (um_id) REFERENCES upload_method(id);
+
+
