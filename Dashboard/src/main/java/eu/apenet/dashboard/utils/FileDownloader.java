cfbbe334860f89964453fdf08c77d25faa36a275
Mahbub Mozadded <sowrov@gmail.com>
2017-11-02 16:20:50 +0100

    Localtype declaration manage in progress

--- Dashboard/src/main/java/eu/apenet/dashboard/utils/FileDownloader.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/utils/FileDownloader.java
@@ -5,8 +5,13 @@
  */
 package eu.apenet.dashboard.utils;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -37,9 +42,52 @@ public class FileDownloader {
         return Optional.empty();
     }
     
+    public static Optional<String> getURLFileName(final URL url) {
+
+        Objects.requireNonNull(url, "URL is null");
+
+        final String file = url.getFile();
+
+        if (file.contains("/")) {
+            final String sub = file.substring(file.lastIndexOf('/') + 1);
+
+            if (sub.length() == 0) {
+                return Optional.empty();
+            }
+
+            if (sub.contains("?")) {
+                return Optional.of(sub.substring(0, sub.indexOf('?')));
+            }
+            return Optional.of(sub);
+        }
+        return Optional.empty();
+    }
+    
+    public static Path downloadFile(String fileURL, String targetDirectory) throws MalformedURLException, IOException, FileAlreadyExistsException {
+        URL url = new URL(fileURL);
+        Optional fileName = FileDownloader.getURLFileName(url);
+        if (!fileName.isPresent()) {
+            throw new IOException("Url is not a file: "+fileURL);
+        }
+        Path targetPath = new File(targetDirectory + File.separator + fileName).toPath();
+        Files.copy(url.openStream(), targetPath);
+
+        return targetPath;
+    }
+    
     public static void main(String[] args) throws MalformedURLException {
         Optional fileName = FileDownloader.getURLFileExtension(new URL("https://localhost:8443/docs/RUNNING.txt?gsdg"));
-        System.out.println("Filename: "+fileName.get());
+        System.out.println("File ext: "+fileName.get());
+        fileName = FileDownloader.getURLFileName(new URL("https://localhost:8443/RUNNING.txt?gsdg"));
+        fileName.ifPresent(value -> {System.out.println("File name: "+value);});
+        
+        try {
+            Optional fileName2 = FileDownloader.getURLFileExtension(null);
+            System.out.println("Filename: "+fileName.get());
+        } catch (Exception ex) {
+            System.out.println("Ex");
+        }
+        
         
     }
 }
