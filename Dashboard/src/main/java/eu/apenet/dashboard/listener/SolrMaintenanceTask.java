4b8ea210433310bf776bedfdf28a8a13ee67c526
Stefan Papp <stefanpapp-ape>
2020-04-23 11:05:45 +0200

    Merge commit '0058842ea00af695def38e2e3b9d3c3525faa682'

--- Dashboard/src/main/java/eu/apenet/dashboard/listener/SolrMaintenanceTask.java
+++ Dashboard/src/main/java/eu/apenet/dashboard/listener/SolrMaintenanceTask.java
@@ -9,44 +9,53 @@ import eu.apenet.commons.solr.SolrUtil;
 import eu.apenet.commons.utils.APEnetUtilities;
 
 public class SolrMaintenanceTask implements Runnable {
-	private static final Logger LOGGER = Logger.getLogger(SolrMaintenanceTask.class);
-    private static final int WAIT_FOR_START = 30*60;
-	private final ScheduledExecutorService scheduler;
 
-	public SolrMaintenanceTask(ScheduledExecutorService scheduler) {
-		this.scheduler = scheduler;
-	}
+    private static final Logger LOGGER = Logger.getLogger(SolrMaintenanceTask.class);
+    private static final int WAIT_FOR_START = 30 * 60;
+    private final ScheduledExecutorService scheduler;
 
-	@Override
-	public void run() {
-		LOGGER.info("Maintenance process started");
-		try {
-			executeMaintenanceTasks();
-		} catch (Throwable e) {
-			LOGGER.error("Stopping processing for a while: " + APEnetUtilities.generateThrowableLog(e));
-		}
+    public SolrMaintenanceTask(ScheduledExecutorService scheduler) {
+        this.scheduler = scheduler;
+    }
 
-		LOGGER.info("Maintenance process stopped");
-		if (!scheduler.isShutdown()) {
-			int delaySeconds = MaintenanceDaemon.calculateSeconds();
-			LOGGER.info("Next maintenance task after " + MaintenanceDaemon.convertNumberToDuration(delaySeconds));
-			scheduler.schedule(new SolrMaintenanceTask(scheduler), delaySeconds,
-					TimeUnit.SECONDS);
-		}
-	}
-
-	public void executeMaintenanceTasks() throws Exception {
-		APEnetUtilities.getDashboardConfig().setMaintenanceMode(true);
-		QueueDaemon.stop();
+    @Override
+    public void run() {
+        LOGGER.info("Maintenance process started");
         try {
-        	LOGGER.info("Maintenance task will start after: " + MaintenanceDaemon.convertNumberToDuration(WAIT_FOR_START) );
-            Thread.sleep(WAIT_FOR_START*1000);
+            executeMaintenanceTasks();
+        } catch (Throwable e) {
+            LOGGER.error("Stopping processing for a while: " + APEnetUtilities.generateThrowableLog(e));
+        }
+
+        LOGGER.info("Maintenance process stopped");
+        if (!scheduler.isShutdown()) {
+            int delaySeconds = MaintenanceDaemon.calculateSeconds();
+            LOGGER.info("Next maintenance task after " + MaintenanceDaemon.convertNumberToDuration(delaySeconds));
+            scheduler.schedule(new SolrMaintenanceTask(scheduler), delaySeconds,
+                    TimeUnit.SECONDS);
+        }
+    }
+
+    public void executeMaintenanceTasks() throws Exception {
+        boolean maintenanceActive = APEnetUtilities.getDashboardConfig().isMaintenanceMode();
+        boolean queueActive = APEnetUtilities.getDashboardConfig().isDefaultQueueProcessing();
+        APEnetUtilities.getDashboardConfig().setMaintenanceMode(true);
+        QueueDaemon.stop();
+        try {
+            LOGGER.info("Maintenance task will start after: " + MaintenanceDaemon.convertNumberToDuration(WAIT_FOR_START));
+            Thread.sleep(WAIT_FOR_START * 1000);
         } catch (InterruptedException e) {
         }
-		SolrUtil.forceSolrCommit();
-		SolrUtil.solrOptimize();
-		SolrUtil.rebuildAutosuggestion();
-		QueueDaemon.start();
-		APEnetUtilities.getDashboardConfig().setMaintenanceMode(false);
-	}
+        SolrUtil.forceSolrCommit();
+        SolrUtil.solrOptimize();
+        SolrUtil.rebuildAutosuggestion();
+        // restart queue only if it was exclusively stopped for the maintenance task
+        if (!queueActive) {
+            QueueDaemon.start();
+        }
+        // lift maintenance mode only if it had not been in place before
+        if (!maintenanceActive) {
+            APEnetUtilities.getDashboardConfig().setMaintenanceMode(false);
+        }
+    }
 }
