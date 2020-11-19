e0dff281206aee9b42259fd295dbb9c61474dc60
Stefan Papp <stefanpapp-ape>
2020-11-19 15:09:26 +0100

    SQL script from 2.8.0 version

---
+++ persistence/src/main/resources/dbScripts/2020-10-30-update-database.sql
@@ -0,0 +1,4 @@
+ALTER TABLE ese ALTER COLUMN path TYPE text;
+ALTER TABLE ese ALTER COLUMN oai_identifier TYPE text;
+ALTER TABLE resumption_token ALTER COLUMN set TYPE text;
+ALTER TABLE resumption_token ALTER COLUMN last_record_harvested TYPE text;
